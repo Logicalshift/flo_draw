e54bf589257aa82cc502df564b56f1f3664cf5c6
Andrew Hunter <andrew@logicalshift.co.uk>
2021-02-19 09:56:30 +0000

    Calculate the scale factor

--- render_canvas/src/renderer_layer.rs
+++ render_canvas/src/renderer_layer.rs
@@ -24,7 +24,14 @@ impl Layer {
     pub fn update_transform(&mut self, active_transform: &canvas::Transform2D) {
         if &self.state.current_matrix != active_transform && !self.state.is_sprite {
             // Update the current matrix
-            self.state.current_matrix = *active_transform;
+            self.state.current_matrix   = *active_transform;
+
+            // Work out the scale factor from the matrix (skewed matrices won't produce accurate values here)
+            let canvas::Transform2D([[a, b, _], [d, e, _], [_, _, _]]) = active_transform;
+            let scale_x                 = a*a + b*b;
+            let scale_y                 = d*d + e*e;
+
+            self.state.scale_factor     = f32::max(scale_x, scale_y).sqrt();
 
             // Add a 'set transform' to the rendering for this layer
             self.render_order.push(RenderEntity::SetTransform(*active_transform));
