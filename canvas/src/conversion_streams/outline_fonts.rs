62d414b48fb44d31060dc9325f2707a0df0bdd36
Andrew Hunter <andrew@logicalshift.co.uk>
2021-02-23 22:10:13 +0000

    Start work on a basic outline generator for the outline renderer

--- canvas/src/conversion_streams/outline_fonts.rs
+++ canvas/src/conversion_streams/outline_fonts.rs
@@ -17,9 +17,85 @@ use allsorts::tag;
 
 use font_kit::handle::{Handle};
 use font_kit::loaders::default::{Font};
+use font_kit::outline::{OutlineSink};
+
+use pathfinder_geometry::vector::{Vector2F};
+use pathfinder_geometry::line_segment::{LineSegment2F};
 
 use std::sync::*;
 use std::collections::{HashMap};
+
+///
+/// Structure used to receive outlining instructions from FontKit
+///
+struct FontOutliner<'a> {
+    drawing:        &'a mut Vec<Draw>,
+    scale_factor:   f32,
+    x_pos:          f32,
+    y_pos:          f32,
+    last:           (f32, f32)
+}
+
+impl<'a> OutlineSink for FontOutliner<'a> {
+    fn move_to(&mut self, to: Vector2F) {
+        let (x, y)  = (to.x(), to.y());
+        let (x, y)  = (x * self.scale_factor, y * self.scale_factor);
+
+        self.last   = (x, y);
+
+        self.drawing.push(Draw::Move(self.x_pos + x, self.y_pos + y));
+    }
+
+    fn line_to(&mut self, to: Vector2F) {
+        let (x, y) = (to.x(), to.y());
+        let (x, y) = (x * self.scale_factor, y * self.scale_factor);
+
+        self.last   = (x, y);
+
+        self.drawing.push(Draw::Line(self.x_pos + x, self.y_pos + y));
+    }
+
+    fn quadratic_curve_to(&mut self, ctrl: Vector2F, to: Vector2F) {
+        let (x0q, y0q)  = self.last;
+
+        let (x1q, y1q)  = (to.x(), to.y());
+        let (x1q, y1q)  = (x1q * self.scale_factor, y1q * self.scale_factor);
+
+        let (x2q, y2q)  = (ctrl.x(), ctrl.y());
+        let (x2q, y2q)  = (x2q * self.scale_factor, y2q * self.scale_factor);
+
+        self.last       = (x1q, y1q);
+
+        let (x2, y2)    = (x0q + (2.0/3.0) * (x2q-x0q), y0q + (2.0/3.0) * (y2q-y0q));
+        let (x3, y3)    = (x1q + (2.0/3.0) * (x2q-x1q), y1q + (2.0/3.0) * (y2q-y1q));
+
+        self.drawing.push(Draw::BezierCurve(
+            (self.x_pos + x1q, self.y_pos + y1q), 
+            (self.x_pos + x2, self.y_pos + y2), 
+            (self.x_pos + x3, self.y_pos + y3)));
+    }
+
+    fn cubic_curve_to(&mut self, ctrl: LineSegment2F, to: Vector2F) {
+        let (x1, y1)    = (to.x(), to.y());
+        let (x1, y1)    = (x1 * self.scale_factor, y1 * self.scale_factor);
+
+        let (x2, y2)    = (ctrl.from_x(), ctrl.from_y());
+        let (x2, y2)    = (x2 * self.scale_factor, y2 * self.scale_factor);
+        let (x3, y3)    = (ctrl.to_x(), ctrl.to_y());
+        let (x3, y3)    = (x3 * self.scale_factor, y3 * self.scale_factor);
+
+        self.last       = (x1, y1);
+
+        self.drawing.push(Draw::BezierCurve(
+            (self.x_pos + x1, self.y_pos + y1), 
+            (self.x_pos + x2, self.y_pos + y2), 
+            (self.x_pos + x3, self.y_pos + y3)));
+    }
+
+    fn close(&mut self) {
+        self.drawing.push(Draw::ClosePath);
+    }
+}
 
 ///
 /// State of the outline font system (and target for )
@@ -61,16 +137,25 @@ impl FontState {
     }
 
     ///
+    /// Updates the size of a particular font
+    ///
+    pub fn set_font_size(&mut self, id: FontId, new_size: f32) {
+        if let Some(size) = self.font_size.get_mut(&id) {
+            *size = new_size;
+        }
+    }
+
+    ///
     /// Retrieves the allsorts font object for a particular font ID
     ///
     /// Returns None if the font is not available or could not be loaded
     ///
     pub fn shape_text<'a>(&'a self, id: FontId, text: String) -> Option<Vec<gpos::Info>> {
         // Fetch the font-kit font
-        let font    = if let Some(font) = self.loaded_fonts.get(&id) { font } else { return None; };
+        let font        = if let Some(font) = self.loaded_fonts.get(&id) { font } else { return None; };
 
         // The font handle contains the font data
-        let handle  = if let Some(handle) = font.handle() { handle } else { return None; };
+        let handle      = if let Some(handle) = font.handle() { handle } else { return None; };
 
         // Retrieve the font data
         let (data, font_index) = match handle {
@@ -92,6 +177,22 @@ impl FontState {
         let shape       = font.shape(glyphs, tag::LATN, Some(tag::DFLT), &gsub::Features::Mask(gsub::GsubFeatureMask::default()), true).ok()?;
 
         Some(shape)
+    }
+
+    ///
+    /// Generates the outlines for some text that has been rendered into glyphs
+    ///
+    pub fn generate_outlines<'a>(&'a self, id: FontId, glyphs: Vec<gpos::Info>, x: f32, y: f32) -> Option<Vec<Draw>> {
+        // Fetch the font-kit font and its size
+        let font            = if let Some(font) = self.loaded_fonts.get(&id)    { font } else { return None; };
+        let font_size       = if let Some(font_size) = self.font_size.get(&id)  { *font_size } else { return None; };
+
+        // Fetch some information about this font
+        let metrics         = font.metrics();
+        let scale_factor    = font_size / (metrics.units_per_em as f32);
+
+        // TODO: generate the outlines
+        Some(vec![])
     }
 }
 
@@ -120,9 +221,19 @@ pub fn stream_outline_fonts<InStream: 'static+Send+Unpin+Stream<Item=Draw>>(draw
                 }
 
                 Draw::DrawText(font_id, text, x, y) => {
-                    let shape = state.shape_text(font_id, text);
-                    if let Some(shape) = shape {
-                        println!("OK {:?}", shape);
+                    // Call the shaper to generate the glyphs
+                    let glyphs = state.shape_text(font_id, text);
+
+                    // Render them as outlines
+                    if let Some(glyphs) = glyphs {
+                        yield_value(Draw::NewPath).await;
+
+                        for draw in state.generate_outlines(font_id, glyphs, x, y).into_iter().flatten() {
+                            yield_value(draw).await;
+                        }
+
+                        yield_value(Draw::Fill).await;
+                        yield_value(Draw::NewPath).await;
                     }
                 }
 
