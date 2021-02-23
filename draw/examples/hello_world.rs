7a22b28943e34c78798ab647dafac1071adef0b2
Andrew Hunter <andrew@logicalshift.co.uk>
2021-02-23 22:34:09 +0000

    Add the Lato font to flo_draw and use it to draw a 'hello, world' message

---
+++ draw/examples/hello_world.rs
@@ -0,0 +1,32 @@
+use flo_draw::*;
+use flo_canvas::*;
+
+use std::sync::*;
+
+///
+/// Simple example that displays a canvas window and renders a triangle
+///
+pub fn main() {
+    // 'with_2d_graphics' is used to support operating systems that can't run event loops anywhere other than the main thread
+    with_2d_graphics(|| {
+        let lato    = Arc::new(Vec::from(include_bytes!("Lato-Regular.ttf").clone()));
+
+        // Create a window
+        let canvas  = create_canvas_window("Hello");
+
+        // Say 'hello, world'
+        canvas.draw(|gc| {
+            // Set up the canvas
+            gc.canvas_height(1000.0);
+            gc.center_region(0.0, 0.0, 1000.0, 1000.0);
+
+            // Load a font
+            gc.define_font_data(FontId(1), FontData::Ttf(Arc::clone(&lato)));
+            gc.set_font_size(FontId(1), 100.0);
+
+            // Draw some text in our font
+            gc.fill_color(Color::Rgba(0.0, 0.0, 0.6, 1.0));
+            gc.draw_text(FontId(1), "Hello, World".to_string(), 50.0, 500.0);
+        });
+    });
+}
