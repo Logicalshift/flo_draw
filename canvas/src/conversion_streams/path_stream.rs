34c69c930a5e04b99608666670c03ca91871cf72
Andrew Hunter <andrew@logicalshift.co.uk>
2021-02-24 10:00:12 +0000

    Rename to 'drawing_to_paths' and add a simple test

--- canvas/src/conversion_streams/path_stream.rs
+++ canvas/src/conversion_streams/path_stream.rs
@@ -14,7 +14,7 @@ use std::mem;
 /// This can generate any path structure that implements the `BezierPathFactory` trait as its result. Bezier paths can't
 /// contain 'move' instructions, so the result is a list of path segments that make up the overall path.
 ///
-pub fn to_paths<BezierPath, InStream>(draw_stream: InStream) -> impl Send+Unpin+Stream<Item=Vec<BezierPath>>
+pub fn drawing_to_paths<BezierPath, InStream>(draw_stream: InStream) -> impl Send+Unpin+Stream<Item=Vec<BezierPath>>
 where
 InStream:           'static+Send+Unpin+Stream<Item=Draw>,
 BezierPath:         'static+Send+BezierPathFactory,
@@ -23,6 +23,7 @@ BezierPath::Point:  Send+Coordinate2D {
         // The path that is currently being created by the path builder (or 'None' if no path has been generated yet)
         let mut current_path: Option<BezierPathBuilder<_>>  = None;
         let mut current_components                          = vec![];
+        let mut start_point                                 = None;
 
         // Read to the end of the stream
         let mut draw_stream                                 = draw_stream;
@@ -52,6 +53,7 @@ BezierPath::Point:  Send+Coordinate2D {
 
                     // Start a new path
                     current_path = Some(BezierPathBuilder::start(BezierPath::Point::from_components(&[x as _, y as _])));
+                    start_point  = Some(BezierPath::Point::from_components(&[x as _, y as _]));
                 }
 
                 Draw::Line(x, y)                                => {
@@ -69,6 +71,14 @@ BezierPath::Point:  Send+Coordinate2D {
                             BezierPath::Point::from_components(&[x1 as _, y1 as _])
                         )
                     });
+                }
+
+                Draw::ClosePath                                 => {
+                    if let Some(start_point) = &start_point {
+                        current_path = current_path.map(|current_path| {
+                            current_path.line_to(start_point.clone())
+                        })
+                    }
                 }
 
                 // Ignore other instructions
@@ -89,4 +99,47 @@ BezierPath::Point:  Send+Coordinate2D {
             yield_value(next_path).await;
         }
     })
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use futures::stream;
+    use futures::executor;
+
+    #[test]
+    pub fn square_path() {
+        executor::block_on(async {
+            // Describe a square
+            let square          = vec![
+                Draw::NewPath,
+                Draw::Move(0.0, 0.0), 
+                Draw::Line(100.0, 0.0), 
+                Draw::Line(100.0, 100.0), 
+                Draw::Line(0.0, 100.0), 
+                Draw::ClosePath
+            ];
+
+            // Stream it through drawing_to_paths
+            let square_stream   = stream::iter(square);
+            let path_stream     = drawing_to_paths::<SimpleBezierPath, _>(square_stream);
+            
+            // Collect the paths that result
+            let paths           = path_stream.collect::<Vec<_>>().await;
+
+            // Should contain our square
+            assert!(paths.len() == 1);
+            assert!(paths[0].len() == 1);
+
+            let (start, curves) = &paths[0][0];
+
+            assert!(start == &Coord2(0.0, 0.0));
+            assert!(curves[0].2 == Coord2(100.0, 0.0));
+            assert!(curves[1].2 == Coord2(100.0, 100.0));
+            assert!(curves[2].2 == Coord2(0.0, 100.0));
+            assert!(curves[3].2 == Coord2(0.0, 0.0));
+
+            assert!(curves.len() == 4);
+        });
+    }
 }
