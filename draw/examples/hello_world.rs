f27ef53d7c4c5b10397ebfa690ceda3ede6213e4
Andrew Hunter <andrew@logicalshift.co.uk>
2021-02-27 22:43:01 +0000

    Draw the bounding box around the 'hello, world' example

--- draw/examples/hello_world.rs
+++ draw/examples/hello_world.rs
@@ -1,5 +1,6 @@
 use flo_draw::*;
 use flo_canvas::*;
+use flo_curves::geo::*;
 
 use std::sync::*;
 
@@ -8,10 +9,13 @@ use std::sync::*;
 ///
 pub fn main() {
     with_2d_graphics(|| {
-        let lato    = CanvasFontFace::from_slice(include_bytes!("Lato-Regular.ttf"));
+        let lato        = CanvasFontFace::from_slice(include_bytes!("Lato-Regular.ttf"));
 
         // Create a window
-        let canvas  = create_canvas_window("Hello");
+        let canvas      = create_canvas_window("Hello");
+
+        let hello_size  = lato.measure("Hello, World", 100.0);
+        let (min, max)  = hello_size.inner_bounds;
 
         // Say 'hello, world'
         canvas.draw(|gc| {
@@ -26,6 +30,11 @@ pub fn main() {
             // Draw some text in our font
             gc.fill_color(Color::Rgba(0.0, 0.0, 0.6, 1.0));
             gc.draw_text(FontId(1), "Hello, World".to_string(), 50.0, 500.0);
+
+            gc.new_path();
+            gc.rect(50.0 + min.x() as f32, 500.0 + min.y() as f32, 
+                50.0 + max.x() as f32, 500.0 + max.y() as f32);
+            gc.stroke();
         });
     });
 }
