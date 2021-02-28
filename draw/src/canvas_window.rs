92df640244cb23914f55d5afc46a1f57159c9db0
Andrew Hunter <andrew@logicalshift.co.uk>
2021-02-28 12:33:01 +0000

    Use the new layout stream in canvas_window so text works again

--- draw/src/canvas_window.rs
+++ draw/src/canvas_window.rs
@@ -83,6 +83,7 @@ pub fn create_canvas_window_with_events<'a, TProperties: 'a+FloWindowProperties>
 
     // Get the stream of drawing instructions (and gather them into batches)
     let canvas_stream       = canvas.stream();
+    let canvas_stream       = drawing_with_laid_out_text(canvas_stream);
     let canvas_stream       = drawing_with_text_as_paths(canvas_stream);
     let canvas_stream       = BatchedStream { stream: Some(canvas_stream) };
 
