562d3bdcab02c5b46abc7a55fad9d84dc0328d2e
Andrew Hunter <andrew@logicalshift.co.uk>
2021-02-28 12:49:28 +0000

    Add a basic test for the new layout routine

--- canvas/src/conversion_streams/glyph_layout.rs
+++ canvas/src/conversion_streams/glyph_layout.rs
@@ -164,3 +164,77 @@ pub fn drawing_with_laid_out_text<InStream: 'static+Send+Unpin+Stream<Item=Draw>
         }
     })
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::font_face::*;
+    use futures::stream;
+    use futures::executor;
+
+    #[test]
+    fn layout_hello_world() {
+        executor::block_on(async {
+            // Set up loading a font from a byte stream
+            let lato            = CanvasFontFace::from_slice(include_bytes!("../../test_data/Lato-Regular.ttf"));
+
+            let instructions    = vec![
+                Draw::Font(FontId(1), FontOp::UseFontDefinition(lato)),
+                Draw::Font(FontId(1), FontOp::FontSize(100.0)),
+                Draw::BeginLineLayout(500.0, 500.0, TextAlignment::Left),
+                Draw::Font(FontId(1), FontOp::LayoutText("Hello, world".to_string())),
+                Draw::DrawLaidOutText
+            ];
+            let instructions    = stream::iter(instructions);
+            let instructions    = drawing_with_laid_out_text(instructions);
+
+            let instructions    = instructions.collect::<Vec<_>>().await;
+
+            // Should get the font definition, font size and glyph layouts
+            assert!(instructions.len() == 3);
+
+            if let Draw::Font(FontId(1), FontOp::DrawGlyphs(glyphs)) = &instructions[2] {
+                // Final instruction should be to draw the glyphs we just laid out
+                println!("{:?}", instructions[2]);
+
+                // 'Hello, world' has a simple shape so we should generate one glyph per character
+                assert!(glyphs.len() == "Hello, world".len());
+
+                // Glyph values and positions should be approximately these values
+                fn dist((x1, y1): (f32, f32), (x2, y2): (f32, f32)) -> f32 { let (x, y) = (x1-x2, y1-y2); (x*x + y*y).sqrt() }
+
+                assert!(glyphs[0].id == GlyphId(15));
+                assert!(glyphs[0].em_size == 100.0);
+                assert!(dist(glyphs[0].location, (500.0, 500.0)) < 1.0);
+
+                assert!(glyphs[1].id == GlyphId(59));
+                assert!(glyphs[2].id == GlyphId(1140));
+                assert!(glyphs[3].id == GlyphId(1140));
+                assert!(glyphs[4].id == GlyphId(111));
+                assert!(glyphs[5].id == GlyphId(311));
+                assert!(glyphs[6].id == GlyphId(2));
+                assert!(glyphs[7].id == GlyphId(137));
+                assert!(glyphs[8].id == GlyphId(111));
+                assert!(glyphs[9].id == GlyphId(117));
+                assert!(glyphs[10].id == GlyphId(1140));
+                assert!(glyphs[11].id == GlyphId(55));
+
+                assert!(dist(glyphs[1].location, (574.8, 500.0)) < 1.0);
+                assert!(dist(glyphs[2].location, (627.6, 500.0)) < 1.0);
+                assert!(dist(glyphs[3].location, (651.2, 500.0)) < 1.0);
+                assert!(dist(glyphs[4].location, (674.8, 500.0)) < 1.0);
+                assert!(dist(glyphs[5].location, (731.5, 500.0)) < 1.0);
+                assert!(dist(glyphs[6].location, (754.2, 500.0)) < 1.0);
+                assert!(dist(glyphs[7].location, (777.75, 500.0)) < 1.0);
+                assert!(dist(glyphs[8].location, (855.6, 500.0)) < 1.0);
+                assert!(dist(glyphs[9].location, (912.3, 500.0)) < 1.0);
+                assert!(dist(glyphs[10].location, (948.7, 500.0)) < 1.0);
+                assert!(dist(glyphs[11].location, (972.3, 500.0)) < 1.0);
+            } else {
+                // Not the expected layout instruction
+                println!("{:?}", instructions[2]);
+                assert!(false);
+            }
+        });
+    }
+}
