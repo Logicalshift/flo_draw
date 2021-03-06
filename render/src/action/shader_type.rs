864b5508c31db9d8747ecd487247adb06e3a615d
Andrew Hunter <andrew@logicalshift.co.uk>
2021-03-06 22:16:12 +0000

    Add some functions to help set the erase and clip texture for a shader

--- render/src/action/shader_type.rs
+++ render/src/action/shader_type.rs
@@ -12,3 +12,29 @@ pub enum ShaderType {
     /// Flat colour with 'dashed line' texturing using a 1D texture
     DashedLine { dash_texture: TextureId, erase_texture: Option<TextureId>, clip_texture: Option<TextureId> }
 }
+
+impl ShaderType {
+    ///
+    /// Adds an erase mask texture to the existing shader
+    ///
+    pub fn with_erase_mask(self, new_erase_mask_texture: Option<TextureId>) -> ShaderType {
+        use self::ShaderType::*;
+
+        match self {
+            Simple { erase_texture: _, clip_texture }                   => Simple       { erase_texture: new_erase_mask_texture, clip_texture: clip_texture },
+            DashedLine { dash_texture, erase_texture: _, clip_texture } => DashedLine   { dash_texture: dash_texture, erase_texture: new_erase_mask_texture, clip_texture: clip_texture }
+        }
+    }
+
+    ///
+    /// Adds a clip mask texture to the existing shader
+    ///
+    pub fn with_clip_mask(self, new_clip_mask_texture: Option<TextureId>) -> ShaderType {
+        use self::ShaderType::*;
+
+        match self {
+            Simple { erase_texture, clip_texture: _ }                   => Simple       { erase_texture: erase_texture, clip_texture: new_clip_mask_texture },
+            DashedLine { dash_texture, erase_texture, clip_texture: _ } => DashedLine   { dash_texture: dash_texture, erase_texture: erase_texture, clip_texture: new_clip_mask_texture }
+        }
+    }
+}
