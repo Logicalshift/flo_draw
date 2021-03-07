7a5d94979fef7b4563cd003a1d232f4d0ff443e8
Andrew Hunter <andrew@logicalshift.co.uk>
2021-03-07 19:42:41 +0000

    Assign a texture ID to use for the dash pattern

--- render_canvas/src/resource_ids.rs
+++ render_canvas/src/resource_ids.rs
@@ -7,3 +7,4 @@ pub (crate) const CLIP_RENDER_TARGET: RenderTargetId    = RenderTargetId(2);
 pub (crate) const MAIN_RENDER_TEXTURE: TextureId        = TextureId(0);
 pub (crate) const ERASE_RENDER_TEXTURE: TextureId       = TextureId(1);
 pub (crate) const CLIP_RENDER_TEXTURE: TextureId        = TextureId(2);
+pub (crate) const DASH_TEXTURE: TextureId               = TextureId(3);
