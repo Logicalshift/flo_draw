1163d489fa23d5369f63c202b67a6de4fcb53d94
Andrew Hunter <andrew@logicalshift.co.uk>
2021-03-07 19:35:26 +0000

    Add a simple dashed line example
    This doesn't work yet, but it should show the dashed line texture mapping when it's done

---
+++ draw/examples/dashed_line.rs
@@ -0,0 +1,30 @@
+use flo_draw::*;
+use flo_draw::canvas::*;
+
+///
+/// Draws a dashed line
+///
+pub fn main() {
+    with_2d_graphics(|| {
+        // Create a window
+        let canvas      = create_canvas_window("Dashed line");
+
+        canvas.draw(|gc| {
+            // Set up the canvas
+            gc.canvas_height(1000.0);
+            gc.center_region(0.0, 0.0, 1000.0, 1000.0);
+
+            gc.line_width(8.0);
+            gc.new_dash_pattern();
+            gc.dash_length(4.0);
+            gc.dash_length(4.0);
+            gc.dash_length(2.0);
+            gc.dash_length(2.0);
+            gc.stroke_color(Color::Rgba(0.0, 0.0, 0.6, 1.0));
+
+            gc.new_path();
+            gc.rect(100.0, 100.0, 900.0, 900.0);
+            gc.stroke();
+        });
+    });
+}
