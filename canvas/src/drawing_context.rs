ee1e08a214a3bb19748ba49b8f8044686cf216fe
Andrew Hunter <andrew@logicalshift.co.uk>
2021-05-05 09:19:13 +0100

    Add some tests
    Looks like we need to treat things like font sizes as separate resources

--- canvas/src/drawing_context.rs
+++ canvas/src/drawing_context.rs
@@ -57,11 +57,14 @@ impl DrawingContext {
     pub fn draw<FnAction>(&self, action: FnAction)
     where FnAction: Send+FnOnce(&mut DrawGraphicsContext) -> () {
         // Fill a buffer with the drawing actions
-        let mut actions = vec![];
-        action(&mut actions);
+        let mut draw_actions = vec![];
+        action(&mut draw_actions);
+
+        draw_actions.insert(0, Draw::StartFrame);
+        draw_actions.push(Draw::ShowFrame);
 
         // Send the actions to the core
-        self.write(actions);
+        self.write(draw_actions);
     }
 }
 
@@ -69,5 +72,119 @@ impl Drop for DrawingContext {
     fn drop(&mut self) {
         let waker = self.stream_core.sync(|core| { core.close(); core.take_waker() });
         waker.map(|waker| waker.wake());
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::font::*;
+    use crate::context::*;
+    use crate::font_face::*;
+
+    use futures::prelude::*;
+    use futures::executor;
+
+    use std::thread::*;
+    use std::time::*;
+
+    #[test]
+    fn follow_drawing_context_stream() {
+        let (context, stream) = DrawingContext::new();
+
+        // Thread to draw some stuff to the canvas
+        spawn(move || {
+            sleep(Duration::from_millis(50));
+
+            context.write(vec![
+                Draw::NewPath,
+                Draw::Move(0.0, 0.0),
+                Draw::Line(10.0, 0.0),
+                Draw::Line(10.0, 10.0),
+                Draw::Line(0.0, 10.0)
+            ]);
+        });
+
+        // Check we can get the results via the stream
+        executor::block_on(async {
+            let mut stream = stream;
+
+            assert!(stream.next().await == Some(Draw::NewPath));
+            assert!(stream.next().await == Some(Draw::Move(0.0, 0.0)));
+            assert!(stream.next().await == Some(Draw::Line(10.0, 0.0)));
+            assert!(stream.next().await == Some(Draw::Line(10.0, 10.0)));
+            assert!(stream.next().await == Some(Draw::Line(0.0, 10.0)));
+
+            // When the thread goes away, it'll drop the canvas, so we should get the 'None' request here too
+            assert!(stream.next().await == None);
+        })
+    }
+
+    #[test]
+    fn only_one_font_definition_survives_clear_layer() {
+        let (context, stream)   = DrawingContext::new();
+        let lato                = CanvasFontFace::from_slice(include_bytes!("../test_data/Lato-Regular.ttf"));
+
+        context.draw(|gc| {
+            gc.layer(LayerId(1));
+
+            gc.define_font_data(FontId(1), lato.clone());
+            gc.define_font_data(FontId(1), lato.clone());
+            gc.define_font_data(FontId(2), lato.clone());
+            gc.define_font_data(FontId(1), lato.clone());
+            gc.set_font_size(FontId(1), 12.0);
+            gc.draw_text(FontId(1), "Test".to_string(), 100.0, 100.0);
+
+            gc.clear_layer();
+            gc.fill();
+        });
+
+        let mut stream = stream;
+
+        executor::block_on(async {
+            assert!(stream.next().await == Some(Draw::StartFrame));
+
+            assert!(match stream.next().await { Some(Draw::Font(FontId(2), FontOp::UseFontDefinition(_))) => true, _ => false });
+            assert!(match stream.next().await { Some(Draw::Font(FontId(1), FontOp::UseFontDefinition(_))) => true, _ => false });
+            assert!(stream.next().await == Some(Draw::Font(FontId(1), FontOp::FontSize(12.0))));
+
+            assert!(stream.next().await == Some(Draw::Layer(LayerId(1))));
+            assert!(stream.next().await == Some(Draw::Fill));
+        });
+    }
+
+    #[test]
+    fn only_one_font_size_survives_clear_layer() {
+        let (context, stream)   = DrawingContext::new();
+        let lato                = CanvasFontFace::from_slice(include_bytes!("../test_data/Lato-Regular.ttf"));
+
+        context.draw(|gc| {
+            gc.layer(LayerId(1));
+
+            gc.define_font_data(FontId(1), lato.clone());
+            gc.set_font_size(FontId(1), 16.0);
+            gc.set_font_size(FontId(1), 15.0);
+            gc.set_font_size(FontId(2), 18.0);
+            gc.set_font_size(FontId(1), 14.0);
+            gc.set_font_size(FontId(1), 13.0);
+            gc.set_font_size(FontId(1), 12.0);
+            gc.draw_text(FontId(1), "Test".to_string(), 100.0, 100.0);
+
+            gc.clear_layer();
+            gc.fill();
+        });
+
+        executor::block_on(async {
+            let mut stream = stream;
+
+            assert!(stream.next().await == Some(Draw::StartFrame));
+
+            assert!(match stream.next().await { Some(Draw::Font(FontId(1), FontOp::UseFontDefinition(_))) => true, _ => false });
+            assert!(stream.next().await == Some(Draw::Font(FontId(2), FontOp::FontSize(18.0))));
+            assert!(stream.next().await == Some(Draw::Font(FontId(1), FontOp::FontSize(12.0))));
+
+            assert!(stream.next().await == Some(Draw::Layer(LayerId(1))));
+            assert!(stream.next().await == Some(Draw::Fill));
+        });
     }
 }
