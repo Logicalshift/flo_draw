0d5b0bff65126d75d45f7f477acdb45059f556ea
Andrew Hunter <andrew@logicalshift.co.uk>
2021-07-30 22:42:31 +0100

    Add some doc comments to the Context trait

--- canvas/src/context.rs
+++ canvas/src/context.rs
@@ -13,69 +13,211 @@ use std::sync::*;
 /// A graphics context provides the basic set of graphics actions that can be performed
 ///
 pub trait GraphicsContext {
+    /// Suspends rendering to the display until the next 'ShowFrame'
+    ///
+    /// The renderer may perform tessellation or rendering in the background after 'StartFrame' but won't
+    /// commit anything to the visible frame buffer until 'ShowFrame' is hit. If 'StartFrame' is nested,
+    /// then the frame won't be displayed until 'ShowFrame' has been requested at least that many times.
+    ///
+    /// The frame state persists across a 'ClearCanvas'
     fn start_frame(&mut self);
+
+    /// Displays any requested queued after 'StartFrame'
     fn show_frame(&mut self);
+
+    /// Resets the frame count back to 0 (for when regenerating the state of a canvas)
     fn reset_frame(&mut self);
 
+
+
+    /// Begins a new path
     fn new_path(&mut self);
+
+    /// Move to a new point in the current path (paths should always begin with a move instruction, and moves can define subpaths)
     fn move_to(&mut self, x: f32, y: f32);
+
+    /// Adds a line to the current path
     fn line_to(&mut self, x: f32, y: f32);
+
+    /// Adds a bezier curve to the current path
     fn bezier_curve_to(&mut self, x: f32, y: f32, cp1_x: f32, cp1_y: f32, cp2_x: f32, cp2_y: f32);
+
+    /// Closes the current path (adds a line to the last move point)
     fn close_path(&mut self);
+
+    /// Fills the currently defined path
     fn fill(&mut self);
+
+    /// Draws a line around the currently defined path
     fn stroke(&mut self);
+
+    /// Sets the line width for the next stroke() operation
     fn line_width(&mut self, width: f32);
+
+    /// Sets the line width for the next stroke() operation in device pixels
     fn line_width_pixels(&mut self, width: f32);
+
+    /// Sets the line join style for the next stroke() operation
     fn line_join(&mut self, join: LineJoin);
+
+    /// Sets the style of the start and end cap of the next line drawn by the stroke() operation
     fn line_cap(&mut self, cap: LineCap);
+
+    /// Sets the winding rule used to determine if an internal subpath should be filled or empty
     fn winding_rule(&mut self, winding_rule: WindingRule);
+
+    /// Starts defining a new dash pattern
     fn new_dash_pattern(&mut self);
+
+    /// Adds a dash of the specified length to the dash pattern (alternating between drawing and gap lengths)
     fn dash_length(&mut self, length: f32);
+
+    /// Sets the offset for where the dash pattern starts at the next stroke
     fn dash_offset(&mut self, offset: f32);
+
+    /// Sets the colour of the next fill() operation
     fn fill_color(&mut self, col: Color);
+
+    /// Sets the texture to use for the next fill() operation
     fn fill_texture(&mut self, texture_id: TextureId, x: f32, y: f32, width: f32, height: f32);
+
+    /// Sets the gradient to use for the next fill() operation
     fn fill_gradient(&mut self, gradient_id: GradientId, x1: f32, y1: f32, x2: f32, y2: f32);
+
+    /// Applies a transformation to the fill texture or gradient
     fn fill_transform(&mut self, transform: Transform2D);
+
+    /// Sets the colour to use for the next stroke() operation
     fn stroke_color(&mut self, col: Color);
+
+    /// Sets the blend mode of the next fill or stroke operation
     fn blend_mode(&mut self, mode: BlendMode);
+
+    /// Reset the canvas transformation to the identity transformation (so that the y axis goes from -1 to 1)
     fn identity_transform(&mut self);
+
+    /// Sets a transformation such that:
+    /// (0,0) is the center point of the canvas
+    /// (0,height/2) is the top of the canvas
+    /// Pixels are square
     fn canvas_height(&mut self, height: f32);
+
+    /// Moves a particular region to the center of the canvas (coordinates are minx, miny, maxx, maxy)
     fn center_region(&mut self, minx: f32, miny: f32, maxx: f32, maxy: f32);
+
+    /// Multiply a 2D transform by the current transformation
     fn transform(&mut self, transform: Transform2D);
+
+    /// Removes the current clipping path
     fn unclip(&mut self);
+
+    /// Sets the current path as the clipping path
     fn clip(&mut self);
+
+    /// Stores the current contents of the canvas in a background buffer
     fn store(&mut self);
+
+    /// Restores the contents of the canvas from the background buffer
     fn restore(&mut self);
+
+    /// Releases the memory allocated by the last store() operation
     fn free_stored_buffer(&mut self);
+
+    /// Stores the current state of the canvas (line width, fill colour, etc)
     fn push_state(&mut self);
+
+    /// Restore a state previously pushed
+    ///
+    /// This will restore the line width (and the other stroke settings), stroke colour, current path, fill colour,
+    /// winding rule, sprite settings and blend settings.
+    ///
+    /// The currently selected layer is not affected by this operation.
     fn pop_state(&mut self);
+
+    /// Clears the canvas entirely to a background colour, and removes any stored resources (layers, sprites, fonts, textures)
     fn clear_canvas(&mut self, color: Color);
 
+
+
+    /// Selects a particular layer for drawing
+    /// Layer 0 is selected initially. Layers are drawn in order starting from 0.
+    /// Layer IDs don't have to be sequential.
     fn layer(&mut self, layer_id: LayerId);
+
+    /// Sets how a particular layer is blended with the underlying layer
     fn layer_blend(&mut self, layer_id: LayerId, blend_mode: BlendMode);
+
+    /// Clears the current layer
     fn clear_layer(&mut self);
 
+
+    /// Selects a particular sprite for drawing
+    ///
+    /// Future drawing actions are sent to this sprite: use something like `Layer(0)` to start drawing
+    /// to a layer again.
+    ///
+    /// Sprites can be repeatedly re-rendered with a single command and their appearance may be
+    /// cached for efficiency. Actions that affect the whole canvas or layers are not permitted in
+    /// sprites.
     fn sprite(&mut self, sprite_id: SpriteId);
+
+    /// Releases the resources used by the current sprite
     fn clear_sprite(&mut self);
+
+    /// Adds a sprite transform to the next sprite drawing operation
     fn sprite_transform(&mut self, transform: SpriteTransform);
+
+    /// Renders a sprite with a set of transformations
     fn draw_sprite(&mut self, sprite_id: SpriteId);
 
+
+
+    /// Loads font data into the canvas for a particular font ID
     fn define_font_data(&mut self, font_id: FontId, font_data: Arc<CanvasFontFace>);
+
+    /// Sets the size that text in the specified font will be rendered at
     fn set_font_size(&mut self, font_id: FontId, size: f32);
+
+    /// Draws a text string using a font
     fn draw_text(&mut self, font_id: FontId, text: String, baseline_x: f32, baseline_y: f32);
+
+    /// Draws specific glyphs from a font
     fn draw_glyphs(&mut self, font_id: FontId, glyphs: Vec<GlyphPosition>);
+
+    /// Starts laying out a line of text
     fn begin_line_layout(&mut self, x: f32, y: f32, align: TextAlignment);
+
+    /// Adds text to the current line layout
     fn layout_text(&mut self, font_id: FontId, text: String);
+
+    /// Finishes laying out text and renders the result
     fn draw_text_layout(&mut self);
 
+
+
+    /// Creates a new texture that can be used with fill_texture of the specified width and height
     fn create_texture(&mut self, texture_id: TextureId, width: u32, height: u32, format: TextureFormat);
+
+    /// Releases the memory allocated to a particular texture
     fn free_texture(&mut self, texture_id: TextureId);
+
+    /// Sets the bitmap data for a texture, in the format specified by the call to create_texture()
     fn set_texture_bytes(&mut self, texture_id: TextureId, x: u32, y: u32, width: u32, height: u32, bytes: Arc<Vec<u8>>);
+
+    /// Applies an alpha value to a texture
     fn set_texture_fill_alpha(&mut self, texture_id: TextureId, alpha: f32);
 
+
+
+    /// Defines a new gradient with a colour at stop position 0.0. Gradients can be used via fill_gradient()
     fn create_gradient(&mut self, gradient_id: GradientId, initial_color: Color);
+
+    /// Adds a new colour stop to a texture
     fn gradient_stop(&mut self, gradient_id: GradientId, pos: f32, color: Color);
 
+
+
+    /// Sends a single drawing instruction to this graphics context
     fn draw(&mut self, d: Draw) {
         use self::Draw::*;
         use self::PathOp::*;
