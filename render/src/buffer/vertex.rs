7377bf27d993828bc335503b32173d7846cfbd73
Andrew Hunter <andrew@logicalshift.co.uk>
2022-01-09 16:22:39 +0000

    Add a convenience method for creating Vertex2Ds with just the position set

--- render/src/buffer/vertex.rs
+++ render/src/buffer/vertex.rs
@@ -8,3 +8,16 @@ pub struct Vertex2D {
     pub tex_coord:  [f32; 2],
     pub color:      [u8; 4]
 }
+
+impl Vertex2D {
+    ///
+    /// Creates a 2D vertex with the position set and the other values zeroed out
+    ///
+    pub fn with_pos(x: f32, y: f32) -> Vertex2D {
+        Vertex2D {
+            pos:        [x, y],
+            tex_coord:  [0.0, 0.0],
+            color:      [0, 0, 0, 0]
+        }
+    }
+}
