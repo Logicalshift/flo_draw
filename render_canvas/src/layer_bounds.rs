82128323bd8ed6c881d1cce6a91ae217e1b0bc7e
Andrew Hunter <andrew@logicalshift.co.uk>
2022-01-15 17:03:32 +0000

    Add a routine to apply transformations to the layer bounds

--- render_canvas/src/layer_bounds.rs
+++ render_canvas/src/layer_bounds.rs
@@ -1,4 +1,6 @@
-use crate::render_entity_details::RenderEntityDetails;
+use crate::render_entity_details::*;
+
+use flo_canvas as canvas;
 
 ///
 /// Represents the bounds of a particular layer on the canvas
@@ -52,5 +54,27 @@ impl LayerBounds {
         self.min_y = f32::min(self.min_y, details.min.1);
         self.max_x = f32::max(self.max_x, details.max.0);
         self.max_y = f32::max(self.max_y, details.max.1);
+    }
+
+    ///
+    /// Returns the effect of transforming these bounds by some transformation
+    ///
+    pub fn transform(&self, transform: canvas::Transform2D) -> LayerBounds {
+        // Transforming has no effect on undefined layer bounds
+        if self.is_undefined() { return LayerBounds::default(); }
+
+        // Transform the x and y coordinates of the four corners of the bounding box
+        let (x1, y1) = transform.transform_point(self.min_x, self.min_y);
+        let (x2, y2) = transform.transform_point(self.max_x, self.max_y);
+        let (x3, y3) = transform.transform_point(self.min_x, self.max_y);
+        let (x4, y4) = transform.transform_point(self.min_x, self.max_y);
+
+        // Use the min/max values of each coordinate
+        LayerBounds {
+            min_x: f32::min(f32::min(f32::min(x1, x2), x3), x4),
+            min_y: f32::min(f32::min(f32::min(y1, y2), y3), y4),
+            max_x: f32::max(f32::max(f32::max(x1, x2), x3), x4),
+            max_y: f32::max(f32::max(f32::max(y1, y2), y3), y4),
+        }
     }
 }
