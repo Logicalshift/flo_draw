e4c5842b6a98147aaf1adbe52342442f4ddc9e71
Andrew Hunter <andrew@logicalshift.co.uk>
2022-06-30 20:10:48 +0100

    Start on the pipeline configuration, with the fragment shader format

--- render/src/wgpu_renderer/pipeline_configuration.rs
+++ render/src/wgpu_renderer/pipeline_configuration.rs
@@ -1,7 +1,22 @@
+use super::render_target::*;
+
+use wgpu;
+
 ///
 /// Description of a WGPU pipeline configuration (used to create the configuration and as a hash key)
 ///
 #[derive(Clone, PartialEq, Hash)]
 pub (crate) struct PipelineConfiguration {
-    
+    pub (crate) texture_format: wgpu::TextureFormat,
+}
+
+impl PipelineConfiguration {
+    ///
+    /// Creates a render pipeline descriptor from this configuration
+    ///
+    pub fn to_render_pipeline_description(&self, adapter: &wgpu::Adapter, surface: &wgpu::Surface) -> wgpu::RenderPipelineDescriptor {
+        let fragment_format = wgpu::ColorTargetState::from(self.texture_format);
+
+        unimplemented!()
+    }
 }
