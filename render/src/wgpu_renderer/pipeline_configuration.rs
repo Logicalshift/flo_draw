24346018a8a06d97853524286b264afe04c98e99
Andrew Hunter <andrew@logicalshift.co.uk>
2022-06-30 22:48:23 +0100

    I think we can entirely construct a pipeline state with the texture format and the shader

--- render/src/wgpu_renderer/pipeline_configuration.rs
+++ render/src/wgpu_renderer/pipeline_configuration.rs
@@ -1,3 +1,4 @@
+use super::wgpu_shader::*;
 use super::render_target::*;
 
 use wgpu;
@@ -7,16 +8,9 @@ use wgpu;
 ///
 #[derive(Clone, PartialEq, Hash)]
 pub (crate) struct PipelineConfiguration {
+    /// Format of the texture that this will render against
     pub (crate) texture_format: wgpu::TextureFormat,
-}
 
-impl PipelineConfiguration {
-    ///
-    /// Creates a render pipeline descriptor from this configuration
-    ///
-    pub fn to_render_pipeline_description(&self, adapter: &wgpu::Adapter, surface: &wgpu::Surface) -> wgpu::RenderPipelineDescriptor {
-        let fragment_format = wgpu::ColorTargetState::from(self.texture_format);
-
-        unimplemented!()
-    }
+    /// The identifier of the shader module to use (this defines both the vertex and the fragment shader, as well as the pipeline layout to use)
+    pub (crate) shader_module: WgpuShader,
 }
