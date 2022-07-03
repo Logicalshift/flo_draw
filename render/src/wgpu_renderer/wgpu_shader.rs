c641ac0fc48b91c089cb7ee5af7dc7b07a63df36
Andrew Hunter <andrew@logicalshift.co.uk>
2022-07-03 11:52:02 +0100

    Add the shader variants from the OpenGL implementation, and the simple shader definition to WgpuShader

--- render/src/wgpu_renderer/wgpu_shader.rs
+++ render/src/wgpu_renderer/wgpu_shader.rs
@@ -1,7 +1,65 @@
 ///
+/// The variants that every shader must have
+///
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub enum StandardShaderVariant {
+    /// No clipping texture is applied to the shader
+    NoClipping,
+
+    /// A clipping texture is used to mask the rendering
+    ClippingMask
+}
+
+///
+/// The post-processing step to apply to the colour output of a shader
+///
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub enum ColorPostProcessingStep {
+    /// The shader should not change the colour outputs
+    NoPostProcessing,
+
+    /// The shader should multiply its outputs by the alpha value
+    MultiplyAlpha,
+
+    /// The colour is blended so that at alpha (0), the RGB values are (1,1,1) - the inverse of pre-multiplications
+    InvertColorAlpha,
+}
+
+///
+/// Describes what to do when applying an alpha value to a pixel
+///
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub enum AlphaBlendStep {
+    /// Input colours are not pre-multiplied
+    NoPremultiply,
+
+    /// Input colours are pre-mulitplied
+    Premultiply,
+}
+
+///
+/// The format of the source texture for a filter step
+///
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub enum FilterSourceFormat {
+    /// Alpha is pre-multiplied
+    PremultipliedAlpha,
+
+    /// Alpha is not pre-multiplied
+    NotPremultiplied,
+}
+
+///
 /// Enumeration of the shaders loaded for the WGPU renderer
 ///
-#[derive(Clone, PartialEq, Hash)]
+#[derive(Clone, PartialEq, Eq, Hash)]
 pub enum WgpuShader {
+    /// Flat colour shader
+    Simple(StandardShaderVariant, ColorPostProcessingStep),
+}
 
+impl Default for WgpuShader {
+    fn default() -> WgpuShader {
+        WgpuShader::Simple(StandardShaderVariant::NoClipping, ColorPostProcessingStep::NoPostProcessing)
+    }
 }
