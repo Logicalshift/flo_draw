1aa27407cb0677bd65bd593c42ab03685d65a04d
Andrew Hunter <andrew@logicalshift.co.uk>
2022-07-14 23:32:55 +0100

    Use the OpenGL or the WGPU render entities in the rendering scene

--- draw/src/draw_scene/render_window_entity.rs
+++ draw/src/draw_scene/render_window_entity.rs
@@ -1,6 +1,9 @@
 #[cfg(feature="render-opengl")]
 use super::glutin_render_window_entity::*;
 
+#[cfg(feature="render-wgpu")]
+use super::wgpu_render_window_entity::*;
+
 use flo_scene::*;
 use flo_canvas_events::*;
 
@@ -9,7 +12,23 @@ use std::sync::*;
 ///
 /// Creates a render window in a scene with the specified entity ID
 ///
+#[cfg(feature="render-opengl")]
 pub fn create_render_window_entity(context: &Arc<SceneContext>, entity_id: EntityId, initial_size: (u64, u64)) -> Result<SimpleEntityChannel<RenderWindowRequest, ()>, CreateEntityError> {
     create_glutin_render_window_entity(context, entity_id, initial_size)
 }
 
+///
+/// Retrieves or creates a scene context for flo_draw
+///
+#[cfg(all(feature="render-wgpu", not(feature="render-opengl")))]
+pub fn create_render_window_entity(context: &Arc<SceneContext>, entity_id: EntityId, initial_size: (u64, u64)) -> Result<SimpleEntityChannel<RenderWindowRequest, ()>, CreateEntityError> {
+    create_wgpu_render_window_entity(context, entity_id, initial_size)
+}
+
+///
+/// Retrieves or creates a scene context for flo_draw
+///
+#[cfg(all(not(feature="render-wgpu"), not(feature="render-opengl")))]
+pub fn create_render_window_entity(context: &Arc<SceneContext>, entity_id: EntityId, initial_size: (u64, u64)) -> Result<SimpleEntityChannel<RenderWindowRequest, ()>, CreateEntityError> {
+    panic!("No default renderer was specified when flo_draw was compiled (use `render-wgpu` or `render-opengl`)")
+}
--- draw/src/draw_scene/scene.rs
+++ draw/src/draw_scene/scene.rs
@@ -1,12 +1,32 @@
 #[cfg(feature="render-opengl")]
 use super::glutin_scene::*;
 
+#[cfg(feature="render-wgpu")]
+use super::wgpu_scene::*;
+
 use flo_scene::*;
 use std::sync::*;
 
 ///
 /// Retrieves or creates a scene context for flo_draw
 ///
+#[cfg(feature="render-opengl")]
 pub fn flo_draw_scene_context() -> Arc<SceneContext> {
     flo_draw_glutin_scene_context()
+}
+
+///
+/// Retrieves or creates a scene context for flo_draw
+///
+#[cfg(all(feature="render-wgpu", not(feature="render-opengl")))]
+pub fn flo_draw_scene_context() -> Arc<SceneContext> {
+    flo_draw_wgpu_scene_context()
+}
+
+///
+/// Retrieves or creates a scene context for flo_draw
+///
+#[cfg(all(not(feature="render-wgpu"), not(feature="render-opengl")))]
+pub fn flo_draw_scene_context() -> Arc<SceneContext> {
+    panic!("No default renderer was specified when flo_draw was compiled (use `render-wgpu` or `render-opengl`)")
 }
