992d95d4ebf24cc9c8f5e7269effccece6be3bcf
Andrew Hunter <andrew@logicalshift.co.uk>
2022-07-16 22:43:37 +0100

    Fix lifetime of the alpha buffer

--- render/src/wgpu_renderer/renderer_state.rs
+++ render/src/wgpu_renderer/renderer_state.rs
@@ -134,7 +134,8 @@ impl RendererState {
     ///
     #[inline]
     pub fn f32_buffer(&self, value: f32) -> Arc<wgpu::Buffer> {
-        let f32_void    = [value].as_ptr() as *const c_void;
+        let f32_buf     = [value];
+        let f32_void    = f32_buf.as_ptr() as *const c_void;
         let f32_len     = mem::size_of::<f32>();
         let f32_u8      = unsafe { slice::from_raw_parts(f32_void as *const u8, f32_len) };
 
