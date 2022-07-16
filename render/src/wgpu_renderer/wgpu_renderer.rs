f6442e3c5226b2bbda51e02dce2bddff831c8341
Andrew Hunter <andrew@logicalshift.co.uk>
2022-07-16 19:25:55 +0100

    Use the 'no vsync' presentation mode

--- render/src/wgpu_renderer/wgpu_renderer.rs
+++ render/src/wgpu_renderer/wgpu_renderer.rs
@@ -131,7 +131,7 @@ impl WgpuRenderer {
             format:         actual_format,
             width:          width,
             height:         height,
-            present_mode:   wgpu::PresentMode::AutoVsync,
+            present_mode:   wgpu::PresentMode::AutoNoVsync,
         };
 
         self.target_surface.configure(&*self.device, &surface_config);
