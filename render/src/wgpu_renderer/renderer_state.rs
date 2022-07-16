3fe9272a81f0d4e6489c2bfed5634a3d41e5006e
Andrew Hunter <andrew@logicalshift.co.uk>
2022-07-16 22:45:15 +0100

    Use correct size too

--- render/src/wgpu_renderer/renderer_state.rs
+++ render/src/wgpu_renderer/renderer_state.rs
@@ -136,7 +136,7 @@ impl RendererState {
     pub fn f32_buffer(&self, value: f32) -> Arc<wgpu::Buffer> {
         let f32_buf     = [value];
         let f32_void    = f32_buf.as_ptr() as *const c_void;
-        let f32_len     = mem::size_of::<f32>();
+        let f32_len     = mem::size_of::<[f32; 1]>();
         let f32_u8      = unsafe { slice::from_raw_parts(f32_void as *const u8, f32_len) };
 
         let f32_buffer  = self.device.create_buffer_init(&util::BufferInitDescriptor {
