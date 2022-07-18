6a9680c8ee418d4f06154dc90900aa2498e35ae3
Andrew Hunter <andrew@logicalshift.co.uk>
2022-07-18 22:49:57 +0100

    If the active pipeline has a flipped matrix, then write that as the binding

--- render/src/wgpu_renderer/renderer_state.rs
+++ render/src/wgpu_renderer/renderer_state.rs
@@ -128,7 +128,17 @@ impl RendererState {
             // Add the matrix to the buffer
             let matrix_buffer_index     = self.render_pass_resources.matrices.len();
             let matrix_group            = pipeline.matrix_group_index();
-            self.render_pass_resources.matrices.push(self.active_matrix.0);
+
+            let mut active_matrix       = self.active_matrix.0;
+            if pipeline.flip_vertical {
+                active_matrix = [
+                    [active_matrix[0][0], -active_matrix[0][1], active_matrix[0][2], active_matrix[0][3]],
+                    [active_matrix[1][0], -active_matrix[1][1], active_matrix[1][2], active_matrix[1][3]],
+                    [active_matrix[2][0], -active_matrix[2][1], active_matrix[2][2], active_matrix[2][3]],
+                    [active_matrix[3][0], -active_matrix[3][1], active_matrix[3][2], active_matrix[3][3]],
+                ];
+            }
+            self.render_pass_resources.matrices.push(active_matrix);
 
             // Bind the matrix as the next step in the pending render pass
             self.render_pass.push(Box::new(move |resources, render_pass| {
