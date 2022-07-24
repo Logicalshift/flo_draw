a96757734a5e850383c5fb6f40b364b9debb7943
Andrew Hunter <andrew@logicalshift.co.uk>
2022-07-24 18:23:20 +0100

    Add a way to convert a Vec<f32> to a buffer

--- render/src/wgpu_renderer/to_buffer.rs
+++ render/src/wgpu_renderer/to_buffer.rs
@@ -45,6 +45,21 @@ impl ToWgpuBuffer for Vec<u16> {
     }
 }
 
+impl ToWgpuBuffer for Vec<f32> {
+    #[inline]
+    fn to_buffer(&self, device: &wgpu::Device, usage: wgpu::BufferUsages) -> wgpu::Buffer {
+        let contents_void   = self.as_ptr() as *const f32;
+        let contents_len    = self.len() * mem::size_of::<f32>();
+        let contents_u8     = unsafe { slice::from_raw_parts(contents_void as *const u8, contents_len) };
+
+        device.create_buffer_init(&util::BufferInitDescriptor {
+            label:      Some("Vec<f32>::to_buffer"),
+            contents:   contents_u8,
+            usage:      usage,
+        })
+    }
+}
+
 impl ToWgpuBuffer for f32 {
     #[inline]
     fn to_buffer(&self, device: &wgpu::Device, usage: wgpu::BufferUsages) -> wgpu::Buffer {
