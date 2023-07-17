fbb122194bbef82f434b56562f9de2530eb726b2
Andrew Hunter <andrew@logicalshift.co.uk>
2023-07-17 22:09:32 +0100

    Add a couple of modules to describe two of the lower levels of the rendering alogrithm (the curve plan and the scan plan)

---
+++ render_software/src/lib.rs
@@ -0,0 +1,2 @@
+mod scanplan;
+mod curveplan;
---
+++ render_software/src/scanplan/mod.rs
@@ -0,0 +1,13 @@
+//!
+//! # ScanPlan
+//!
+//! The 'scan-plan' is a low-level model of a rasterized scene. It represents each line of the final result as a 'plan'
+//! of programs to apply to ranges of pixels. This plan can be executed to generate the final scene, or combined with
+//! other plans to create more complex renders.
+//!
+//! There are a few advantages of making this plan: notably it avoids overdraw (where individual pixels are rendered
+//! multiple times) and it makes it easy to efficiently mix colours using f32 precision. This can make rendering faster
+//! for complex scenes as work can be avoided rendering pixels that will be obscured later on, and it makes it easy to
+//! parallize both the rendering and the generation tasks. Less complex scenes may render more slowly due to the extra
+//! work involved, however.
+//!
