8a91b853bac889f103eba3170a67b5015d3e8a21
Andrew Hunter <andrew@logicalshift.co.uk>
2023-07-18 09:50:15 +0100

    Add a 'create scanline data' routine to the program

--- render_software/src/pixel_program.rs
+++ render_software/src/pixel_program.rs
@@ -13,5 +13,15 @@ pub trait PixelProgram {
     ///
     /// Draws a series of pixels to a frame buffer
     ///
-    fn draw_pixels(target: &[[f32; 4]], x_range: Range<i32>, ypos: i32, program_data: &Self::ProgramData, scanline_data: &Self::ScanlineData);
+    /// The target points to the start of the range of values to be written. `x_range` provides the range of X values to 
+    ///
+    fn draw_pixels(&self, target: &[[f32; 4]], x_range: Range<i32>, ypos: i32, program_data: &Self::ProgramData, scanline_data: &Self::ScanlineData);
+
+    ///
+    /// Returns the data for a specific scanline
+    ///
+    /// The x-range here is the range of values intercepted by this program: note that `draw_pixels` may be called with a narrower range if part of
+    /// the scan line is not visible, or it is clipped to the edges of the rendering area.
+    ///
+    fn create_scanline_data(&self, target: &[[f32; 4]], x_range: Range<f32>, ypos: i32, program_data: &Self::ProgramData) -> Self::ScanlineData;
 }
