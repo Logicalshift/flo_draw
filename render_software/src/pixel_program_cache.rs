3a180602e378eb209a6769c2acc4bce9f5f30797
Andrew Hunter <andrew@logicalshift.co.uk>
2023-07-18 20:46:20 +0100

    Add an outline of what the pixel program cache will do

--- render_software/src/pixel_program_cache.rs
+++ render_software/src/pixel_program_cache.rs
@@ -1,7 +1,45 @@
+use crate::pixel_program::*;
+
+use std::marker::{PhantomData};
+
 ///
 /// The pixel program cache provides a way to assign IDs to pixel programs and support initialising them
 /// with a data cache.
 ///
 pub struct PixelProgramCache {
 
+}
+
+///
+/// The pixel program data cache stores the program data for the pixel programs
+///
+pub struct PixelProgramDataCache {
+
+}
+
+///
+/// A data manager is used to store data associated with a program into a data cache
+///
+pub struct PixelProgramDataManager<TProgramData> {
+    data: PhantomData<TProgramData>
+}
+
+impl PixelProgramCache {
+    ///
+    /// Creates an empty pixel program cache
+    ///
+    pub fn empty() -> PixelProgramCache {
+        PixelProgramCache {
+        }
+    }
+
+    ///
+    /// Caches a pixel program, returns its ID and a data manager to store data relating to the program
+    ///
+    pub fn add_program<TProgram>(&mut self, program: TProgram) -> (PixelProgramId, PixelProgramDataManager<TProgram::ProgramData>) 
+    where
+        TProgram: PixelProgram,
+    {
+        todo!()
+    }
 }
