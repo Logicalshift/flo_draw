bf4beb1429729ca2e89e768e87b5daa78395215e
Andrew Hunter <andrew@logicalshift.co.uk>
2023-07-18 20:37:13 +0100

    Start putting together the pixel program cache

--- render_software/src/lib.rs
+++ render_software/src/lib.rs
@@ -1,3 +1,9 @@
 mod pixel_program;
+mod pixel_program_cache;
 mod scanplan;
 mod curveplan;
+
+pub use pixel_program;
+pub use pixel_program_cache;
+pub use scanplan;
+pub use curveplan;
---
+++ render_software/src/pixel_program_cache.rs
@@ -0,0 +1,7 @@
+///
+/// The pixel program cache provides a way to assign IDs to pixel programs and support initialising them
+/// with a data cache.
+///
+pub struct PixelProgramCache {
+
+}
