de4acac3fdcf7af9a3cf8dafd7cfc8db3ed61a57
Andrew Hunter <andrew@logicalshift.co.uk>
2023-07-18 23:16:45 +0100

    Add a 'PixelProgramFn' type to make it possible to create pixel programs that take program data
    (This is really just needed because we need a PhantomData to make the program data work in rust)

--- render_software/src/pixel_program.rs
+++ render_software/src/pixel_program.rs
@@ -1,4 +1,4 @@
-use std::ops::{Range};
+use std::{ops::{Range}, marker::PhantomData};
 
 ///
 /// A pixel program descibes how to draw pixels along a scan line
@@ -26,6 +26,23 @@ pub trait PixelProgram {
     fn create_scanline_data(&self, x_range: Range<f32>, ypos: i32, program_data: &Self::ProgramData) -> Self::ScanlineData;
 }
 
+///
+/// Pixel program that calls a function to fill the pixels, with program data
+///
+pub struct PixelProgramFn<TFn, TData>
+where 
+    TFn: Fn(&mut [[f32; 4]], Range<i32>, i32, &TData) -> (),
+{
+    /// The function to call to fill in the pixels
+    function: TFn,
+
+    /// Placeholder for the TData type (Rust doesn't see a function parameter as a constraint)
+    phantom_data: PhantomData<TData>,
+}
+
+///
+/// Simple functions can be pixel programs that take no program data
+///
 impl<TFn> PixelProgram for TFn
 where
     TFn: Fn(&mut [[f32; 4]], Range<i32>, i32, &()) -> (),
@@ -34,8 +51,38 @@ where
     type ScanlineData   = ();
 
     #[inline]
-    fn draw_pixels(&self, target: &mut [[f32; 4]], x_range: Range<i32>, ypos: i32, program_data: &Self::ProgramData, _scanline_data: &()) {
+    fn draw_pixels(&self, target: &mut [[f32; 4]], x_range: Range<i32>, ypos: i32, program_data: &(), _scanline_data: &()) {
         (*self)(target, x_range, ypos, program_data)
+    }
+
+    #[inline]
+    fn create_scanline_data(&self, _x_range: Range<f32>, _ypos: i32, _program_data: &Self::ProgramData) -> () {
+        ()
+    }
+}
+
+impl<TFn, TData> From<TFn> for PixelProgramFn<TFn, TData> 
+where 
+    TFn: Fn(&mut [[f32; 4]], Range<i32>, i32, &TData) -> (),
+{
+    fn from(function: TFn) -> Self {
+        PixelProgramFn {
+            function:       function,
+            phantom_data:   PhantomData,
+        }
+    }
+}
+
+impl<TFn, TData> PixelProgram for PixelProgramFn<TFn, TData> 
+where 
+    TFn: Fn(&mut [[f32; 4]], Range<i32>, i32, &TData) -> (),
+{
+    type ProgramData    = TData;
+    type ScanlineData   = ();
+
+    #[inline]
+    fn draw_pixels(&self, target: &mut [[f32; 4]], x_range: Range<i32>, ypos: i32, program_data: &TData, _scanline_data: &()) {
+        (self.function)(target, x_range, ypos, program_data)
     }
 
     #[inline]
