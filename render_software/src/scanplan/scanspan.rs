4e200c1783b7bf8f3b8fff0bb6985428768ffe99
Andrew Hunter <andrew@logicalshift.co.uk>
2023-07-21 09:44:45 +0100

    Add functions to create and split a ScanSpan
    Was originally thinking of having multiple programs here but I believe it's more efficient to stack up the programs in the scanline itself (which has some problems with how we split up existing programs...)

--- render_software/src/scanplan/scanspan.rs
+++ render_software/src/scanplan/scanspan.rs
@@ -10,6 +10,42 @@ pub struct ScanSpan {
     /// The pixels to draw on the scanline
     x_range: Range<i32>,
 
-    /// The data ID for the program to run over this scanline
-    program: PixelScanlineDataId,
+    /// The ID of the scanline data for the program to run over this range
+    program: PixelScanlineDataId
+}
+
+impl ScanSpan {
+    ///
+    /// Creates a scanspan that will run a single program
+    ///
+    #[inline]
+    pub fn new(range: Range<i32>, program: PixelScanlineDataId) -> ScanSpan {
+        ScanSpan {
+            x_range: range,
+            program: program
+        }
+    }
+
+    ///
+    /// Splits this span at the specified position
+    ///
+    /// Returns the same span if the split would result in a 0-length span
+    ///
+    #[inline]
+    pub fn split(self, pos: i32) -> Result<(ScanSpan, ScanSpan), ScanSpan> {
+        if pos >= self.x_range.start && pos < self.x_range.end {
+            Ok((
+                ScanSpan {
+                    x_range: (self.x_range.start)..pos,
+                    program: self.program,
+                },
+                ScanSpan {
+                    x_range: pos..(self.x_range.end),
+                    program: self.program,
+                }
+            ))
+        } else {
+            Err(self)
+        }
+    }
 }
