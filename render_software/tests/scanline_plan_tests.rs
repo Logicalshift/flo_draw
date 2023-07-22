9ac55e24273432d2282f167197d20c02ece9d59c
Andrew Hunter <andrew@logicalshift.co.uk>
2023-07-22 22:08:22 +0100

    Add tests for neighboring and nearby spans

--- render_software/tests/scanline_plan_tests.rs
+++ render_software/tests/scanline_plan_tests.rs
@@ -59,6 +59,26 @@ fn add_two_spans_reverse() {
 }
 
 #[test]
+fn add_in_between_span() {
+    // Create a data token for the scanline we're generating
+    let mut program_cache = PixelProgramCache::empty();
+    let program_id          = program_cache.add_program(PerPixelProgramFn::from(|_x, _y, _data: &()| 12.0f64));
+    let mut data_cache      = program_cache.create_data_cache();
+    let program_data_id     = program_cache.store_program_data(&program_id, &mut data_cache, ());
+    let scanline_data_id    = program_cache.create_scanline_data(&mut data_cache, 0, &vec![], program_data_id);
+
+    // Set up a plan for a scanline using this program (two spans, reverse order of above)
+    let mut plan = ScanlinePlan::new();
+    plan.add_span(ScanSpan::opaque(0..100, scanline_data_id));
+    plan.add_span(ScanSpan::opaque(200..300, scanline_data_id));
+    plan.add_span(ScanSpan::opaque(125..175, scanline_data_id));
+
+    // Read the span back again
+    let spans = plan.iter_as_spans().collect::<Vec<_>>();
+    assert!(spans == vec![ScanSpan::opaque(0..100, scanline_data_id), ScanSpan::opaque(125..175, scanline_data_id), ScanSpan::opaque(200..300, scanline_data_id)], "Unexpected spans: {:?}", spans);
+}
+
+#[test]
 fn add_opaque_spans_overlap() {
     // Create a data token for the scanline we're generating
     let mut program_cache = PixelProgramCache::empty();
@@ -76,6 +96,86 @@ fn add_opaque_spans_overlap() {
     // Read the span back again
     let spans = plan.iter_as_spans().collect::<Vec<_>>();
     assert!(spans == vec![ScanSpan::opaque(0..100, scanline_data_id_2)], "Unexpected spans: {:?}", spans);
+}
+
+#[test]
+fn add_two_neighboring_spans() {
+    // Create a data token for the scanline we're generating
+    let mut program_cache = PixelProgramCache::empty();
+    let program_id          = program_cache.add_program(PerPixelProgramFn::from(|_x, _y, _data: &()| 12.0f64));
+    let mut data_cache      = program_cache.create_data_cache();
+    let program_data_id     = program_cache.store_program_data(&program_id, &mut data_cache, ());
+    let scanline_data_id_1  = program_cache.create_scanline_data(&mut data_cache, 0, &vec![], program_data_id);
+    let scanline_data_id_2  = program_cache.create_scanline_data(&mut data_cache, 0, &vec![], program_data_id);
+
+    // Set up a plan for a scanline using this program (two spans)
+    let mut plan = ScanlinePlan::new();
+    plan.add_span(ScanSpan::opaque(0..100, scanline_data_id_1));
+    plan.add_span(ScanSpan::opaque(100..200, scanline_data_id_2));
+
+    // Read the span back again
+    let spans = plan.iter_as_spans().collect::<Vec<_>>();
+    assert!(spans == vec![ScanSpan::opaque(0..100, scanline_data_id_1), ScanSpan::opaque(100..200, scanline_data_id_2)], "Unexpected spans: {:?}", spans);
+}
+
+#[test]
+fn add_two_neighboring_spans_reverse_order() {
+    // Create a data token for the scanline we're generating
+    let mut program_cache = PixelProgramCache::empty();
+    let program_id          = program_cache.add_program(PerPixelProgramFn::from(|_x, _y, _data: &()| 12.0f64));
+    let mut data_cache      = program_cache.create_data_cache();
+    let program_data_id     = program_cache.store_program_data(&program_id, &mut data_cache, ());
+    let scanline_data_id_1  = program_cache.create_scanline_data(&mut data_cache, 0, &vec![], program_data_id);
+    let scanline_data_id_2  = program_cache.create_scanline_data(&mut data_cache, 0, &vec![], program_data_id);
+
+    // Set up a plan for a scanline using this program (two spans)
+    let mut plan = ScanlinePlan::new();
+    plan.add_span(ScanSpan::opaque(100..200, scanline_data_id_2));
+    plan.add_span(ScanSpan::opaque(0..100, scanline_data_id_1));
+
+    // Read the span back again
+    let spans = plan.iter_as_spans().collect::<Vec<_>>();
+    assert!(spans == vec![ScanSpan::opaque(0..100, scanline_data_id_1), ScanSpan::opaque(100..200, scanline_data_id_2)], "Unexpected spans: {:?}", spans);
+}
+
+#[test]
+fn add_closely_overlapping_spans() {
+    // Create a data token for the scanline we're generating
+    let mut program_cache = PixelProgramCache::empty();
+    let program_id          = program_cache.add_program(PerPixelProgramFn::from(|_x, _y, _data: &()| 12.0f64));
+    let mut data_cache      = program_cache.create_data_cache();
+    let program_data_id     = program_cache.store_program_data(&program_id, &mut data_cache, ());
+    let scanline_data_id_1  = program_cache.create_scanline_data(&mut data_cache, 0, &vec![], program_data_id);
+    let scanline_data_id_2  = program_cache.create_scanline_data(&mut data_cache, 0, &vec![], program_data_id);
+
+    // Set up a plan for a scanline using this program (two spans)
+    let mut plan = ScanlinePlan::new();
+    plan.add_span(ScanSpan::opaque(0..100, scanline_data_id_1));
+    plan.add_span(ScanSpan::opaque(99..200, scanline_data_id_2));
+
+    // Read the span back again
+    let spans = plan.iter_as_spans().collect::<Vec<_>>();
+    assert!(spans == vec![ScanSpan::opaque(0..99, scanline_data_id_1), ScanSpan::opaque(99..100, scanline_data_id_2), ScanSpan::opaque(100..200, scanline_data_id_2)], "Unexpected spans: {:?}", spans);
+}
+
+#[test]
+fn add_closely_overlapping_spans_reverse_order() {
+    // Create a data token for the scanline we're generating
+    let mut program_cache = PixelProgramCache::empty();
+    let program_id          = program_cache.add_program(PerPixelProgramFn::from(|_x, _y, _data: &()| 12.0f64));
+    let mut data_cache      = program_cache.create_data_cache();
+    let program_data_id     = program_cache.store_program_data(&program_id, &mut data_cache, ());
+    let scanline_data_id_1  = program_cache.create_scanline_data(&mut data_cache, 0, &vec![], program_data_id);
+    let scanline_data_id_2  = program_cache.create_scanline_data(&mut data_cache, 0, &vec![], program_data_id);
+
+    // Set up a plan for a scanline using this program (two spans)
+    let mut plan = ScanlinePlan::new();
+    plan.add_span(ScanSpan::opaque(99..200, scanline_data_id_2));
+    plan.add_span(ScanSpan::opaque(0..100, scanline_data_id_1));
+
+    // Read the span back again
+    let spans = plan.iter_as_spans().collect::<Vec<_>>();
+    assert!(spans == vec![ScanSpan::opaque(0..99, scanline_data_id_1), ScanSpan::opaque(99..100, scanline_data_id_1), ScanSpan::opaque(100..200, scanline_data_id_2)], "Unexpected spans: {:?}", spans);
 }
 
 #[test]
