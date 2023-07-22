1415205d01517c364c00b3c8d8ce53cf3a830135
Andrew Hunter <andrew@logicalshift.co.uk>
2023-07-22 17:26:27 +0100

    Think the plans should be in separate modules

--- render_software/src/lib.rs
+++ render_software/src/lib.rs
@@ -1,9 +1,7 @@
 mod pixel_program;
 mod pixel_program_cache;
-mod scanplan;
-mod edgeplan;
+pub mod scanplan;
+pub mod edgeplan;
 
 pub use pixel_program::*;
 pub use pixel_program_cache::*;
-pub use scanplan::*;
-pub use edgeplan::*;
