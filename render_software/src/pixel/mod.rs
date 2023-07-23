6bfd21ff3f2977ea206102bc6e532260a4dfdcce
Andrew Hunter <andrew@logicalshift.co.uk>
2023-07-23 17:18:05 +0100

    Add a type to represent a standard 8bpp pixel, as a pre-multiplied, gamma-corrected value
    (Not implementing pixel on this for now as these won't need any operations on them yet)

--- render_software/src/pixel/mod.rs
+++ render_software/src/pixel/mod.rs
@@ -1,5 +1,7 @@
 mod pixel_trait;
 mod alpha_blend_trait;
+mod u8_rgba;
 
 pub use pixel_trait::*;
 pub use alpha_blend_trait::*;
+pub use u8_rgba::*;
---
+++ render_software/src/pixel/u8_rgba.rs
@@ -0,0 +1,6 @@
+///
+/// An RGBA pixel as a set of u8 values
+///
+/// The alpha value is pre-multiplied into the RGB values, and the colour space is gamma-corrected
+///
+pub struct U8RgbaPremultiplied(pub [u8; 4]);
