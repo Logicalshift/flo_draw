8085f109f9b132cb9c6d1259cc2d2619ff80a24d
Andrew Hunter <andrew@logicalshift.co.uk>
2023-07-23 17:10:42 +0100

    Add a basic 'alpha blend' trait we can use (we'll use this when generating alpha blending operations)

--- render_software/src/pixel/alpha_blend_trait.rs
+++ render_software/src/pixel/alpha_blend_trait.rs
@@ -1,6 +1,109 @@
+use std::ops::*;
+
+///
+/// The alpha blending functions that can be applied to a 
+///
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum AlphaFunction {
+    Zero,
+    One,
+    SourceAlpha,
+    DestAlpha,
+    OneMinusSourceAlpha,
+    OneMinusDestAlpha,
+}
+
+///
+/// An operation applied to an alpha function
+///
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum AlphaOperation {
+    Clear,
+    Source,
+    Target,
+    SourceOver,
+    DestOver,
+    SourceIn,
+    DestIn,
+    SourceHeldOut,
+    DestHeldOut,
+    SourceAtop,
+    DestAtop,
+    Xor,
+}
+
 ///
 /// Trait implemented by types that support alpha blending
 ///
-pub trait AlphaBlend {
+pub trait AlphaBlend : Sized {
+    /// Performs alpha blending with a chosen source and target functions (for non-premultiplied alphas)
+    fn alpha_blend_with_function(self, dest: Self, source_alpha: AlphaFunction, dest_alpha: AlphaFunction) -> Self;
 
+    /// Performs the specified alpha blending operation
+    #[inline]
+    fn alpha_blend(self, dest: Self, operation: AlphaOperation) -> Self {
+        let (src, dst) = operation.functions();
+        self.alpha_blend_with_function(dest, src, dst)
+    }
+
+    #[inline] fn source_over(self, dest: Self) -> Self        { self.alpha_blend(dest, AlphaOperation::SourceOver) }
+    #[inline] fn dest_over(self, dest: Self) -> Self          { self.alpha_blend(dest, AlphaOperation::DestOver) }
+    #[inline] fn source_in(self, dest: Self) -> Self          { self.alpha_blend(dest, AlphaOperation::SourceIn) }
+    #[inline] fn dest_in(self, dest: Self) -> Self            { self.alpha_blend(dest, AlphaOperation::DestIn) }
+    #[inline] fn source_held_out(self, dest: Self) -> Self    { self.alpha_blend(dest, AlphaOperation::SourceHeldOut) }
+    #[inline] fn dest_held_out(self, dest: Self) -> Self      { self.alpha_blend(dest, AlphaOperation::DestHeldOut) }
+    #[inline] fn source_atop(self, dest: Self) -> Self        { self.alpha_blend(dest, AlphaOperation::SourceAtop) }
+    #[inline] fn dest_atop(self, dest: Self) -> Self          { self.alpha_blend(dest, AlphaOperation::DestAtop) }
+}
+
+impl AlphaOperation {
+    ///
+    /// Returns the alpha functions to use for the source and target for this alpha operation
+    ///
+    #[inline]
+    pub const fn functions(&self) -> (AlphaFunction, AlphaFunction) {
+        match self {
+            AlphaOperation::Clear           => (AlphaFunction::Zero,                AlphaFunction::Zero),
+            AlphaOperation::Source          => (AlphaFunction::One,                 AlphaFunction::Zero),
+            AlphaOperation::Target          => (AlphaFunction::Zero,                AlphaFunction::One),
+            AlphaOperation::SourceOver      => (AlphaFunction::One,                 AlphaFunction::OneMinusSourceAlpha),
+            AlphaOperation::DestOver        => (AlphaFunction::OneMinusDestAlpha,   AlphaFunction::One),
+            AlphaOperation::SourceIn        => (AlphaFunction::DestAlpha,           AlphaFunction::Zero),
+            AlphaOperation::DestIn          => (AlphaFunction::Zero,                AlphaFunction::SourceAlpha),
+            AlphaOperation::SourceHeldOut   => (AlphaFunction::OneMinusDestAlpha,   AlphaFunction::Zero),
+            AlphaOperation::DestHeldOut     => (AlphaFunction::Zero,                AlphaFunction::OneMinusSourceAlpha),
+            AlphaOperation::SourceAtop      => (AlphaFunction::DestAlpha,           AlphaFunction::OneMinusSourceAlpha),
+            AlphaOperation::DestAtop        => (AlphaFunction::OneMinusDestAlpha,   AlphaFunction::SourceAlpha),
+            AlphaOperation::Xor             => (AlphaFunction::OneMinusDestAlpha,   AlphaFunction::OneMinusSourceAlpha),
+        }
+    }
+}
+
+///
+/// Returns the 0 and 1 values for an alpha component
+///
+pub trait AlphaValue {
+    fn zero() -> Self;
+    fn one() -> Self;
+}
+
+impl AlphaFunction {
+    ///
+    /// Applies this alpha function to a pixel
+    ///
+    #[inline]
+    pub fn apply<TPixel, TComponent>(&self, pixel: TPixel, src_alpha: TComponent, dst_alpha: TComponent) -> TPixel
+    where
+        TPixel:         Copy + Mul<TComponent, Output=TPixel>,
+        TComponent:     Copy + AlphaValue + Sub<TComponent, Output=TComponent>,
+    {
+        match self {
+            AlphaFunction::Zero                     => pixel * TComponent::zero(),
+            AlphaFunction::One                      => pixel * TComponent::one(),
+            AlphaFunction::SourceAlpha              => pixel * src_alpha,
+            AlphaFunction::DestAlpha                => pixel * dst_alpha,
+            AlphaFunction::OneMinusSourceAlpha      => pixel * (TComponent::one() - src_alpha),
+            AlphaFunction::OneMinusDestAlpha        => pixel * (TComponent::one() - dst_alpha),
+        }
+    }
 }
--- render_software/src/pixel/pixel_trait.rs
+++ render_software/src/pixel/pixel_trait.rs
@@ -9,13 +9,17 @@ pub trait Pixel<const N: usize>
 where
     Self: Sized + Copy + Clone,
     Self: Neg<Output=Self> + Add<Output=Self> + Sub<Output=Self> + Mul<Output=Self> + Div<Output=Self>,
-    Self: AlphaBlend
+    Self: Add<Self::Component, Output=Self> + Sub<Self::Component, Output=Self> + Mul<Self::Component, Output=Self> + Div<Self::Component, Output=Self>,
+    Self: AlphaBlend,
 {
-    type Component: Sized + Copy + Clone + Neg<Output=Self::Component> + Add<Output=Self::Component> + Sub<Output=Self::Component> + Mul<Output=Self::Component> + Div<Output=Self::Component>;
+    type Component: Sized + Copy + Clone + AlphaValue + Neg<Output=Self::Component> + Add<Output=Self::Component> + Sub<Output=Self::Component> + Mul<Output=Self::Component> + Div<Output=Self::Component>;
 
     /// Returns the components that make up this pixel
     fn to_components(&self) -> [Self::Component; N];
 
     /// Retrieves an individual component from this 
     fn get(&self, component: usize) -> Self::Component { self.to_components()[component] }
+
+    /// Returns the alpha component of this pixel
+    fn alpha_component(&self) -> Self::Component;
 }
