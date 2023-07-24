708c003c28a03b11ec0f8c77d40b42394350432f
Andrew Hunter <andrew@logicalshift.co.uk>
2023-07-24 22:14:10 +0100

    Add doc comments for the modules

--- render_software/src/lib.rs
+++ render_software/src/lib.rs
@@ -1,7 +1,13 @@
 mod pixel_program;
 mod pixel_program_cache;
-pub mod scanplan;
+
+/// An edge plan divides a 2D spaces into regions using arbitrary edge definitions, and can be rendered down into a scan plan
 pub mod edgeplan;
+
+/// A scan plan describes the actions required to draw a single scanline (modelling a 1 dimensional space)
+pub mod scanplan;
+
+/// A pixel models a single colour sample (thematically it could be considered 0 dimensional, though really a pixel is better modelled as aggregation of the light passing through a particular region)
 pub mod pixel;
 
 pub use pixel_program::*;
