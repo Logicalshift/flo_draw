692cdae0d47675dd4f8c954fe3cdbc3ea674a7b2
Andrew Hunter <andrew@logicalshift.co.uk>
2023-07-26 22:38:13 +0100

    Add an edge ID we can use with the edge plan

--- render_software/src/edgeplan/mod.rs
+++ render_software/src/edgeplan/mod.rs
@@ -11,11 +11,13 @@
 //!
 
 mod edge_descriptor;
+mod edge_id;
 mod shape_descriptor;
 mod shape_id;
 mod edge_plan;
 
 pub use edge_descriptor::*;
+pub use edge_id::*;
 pub use shape_descriptor::*;
 pub use shape_id::*;
 pub use edge_plan::*;
