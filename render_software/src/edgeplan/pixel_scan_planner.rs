f295c596f8f82527047092452feb1e817e8cf0f7
Andrew Hunter <andrew@logicalshift.co.uk>
2023-07-29 11:26:00 +0100

    Scan the intercepts that occur outside of the bounds of the rendering region first

--- render_software/src/edgeplan/pixel_scan_planner.rs
+++ render_software/src/edgeplan/pixel_scan_planner.rs
@@ -1,5 +1,6 @@
 use super::edge_descriptor::*;
 use super::edge_plan::*;
+use super::scanline_intercept::*;
 
 use crate::scanplan::*;
 
@@ -15,5 +16,26 @@ pub fn plan_pixel_scanline<TEdge>(edge_plan: &EdgePlan<TEdge>, y_pos: f64, x_ran
 where
     TEdge: EdgeDescriptor,
 {
+    // Ask the edge plan to compute the intercepts on the current scanline
+    let mut ordered_intercepts = edge_plan.intercepts_on_scanline(y_pos);
+
+    // Initial program/position comes from the earliest intercept position
+    let mut current_intercept = if let Some(intercept) = ordered_intercepts.next() { intercept } else { return ScanlinePlan::new(); };
+
+    // Trace programs but don't generate fragments until we get an intercept
+    let mut active_shapes = ScanlineInterceptState::new();
+
+    while (current_intercept.2.ceil() as i64) < x_range.start {
+        // Add or remove this intercept's programs to the active list
+        let (shape_id, direction, x_pos) = &current_intercept;
+        // active_shapes.add_intercept(*direction, z_index, *shape_id, *x_pos); -- z-index
+
+        // Move to the next intercept (or stop if no intercepts actually fall within the x-range)
+        if let Some(intercept) = ordered_intercepts.next() { intercept } else { return ScanlinePlan::new(); };
+    }
+
+    // Update all of the existing shapes to have a start position at the left-hand side of the screen
+    active_shapes.clip_start_x(x_range.start as _);
+
     todo!()
 }
