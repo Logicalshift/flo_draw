8f50f79d6c4d7deca71b4181f1a2ce4b7ca72dab
Andrew Hunter <andrew@logicalshift.co.uk>
2023-08-07 00:19:26 +0100

    Add some tests for clipping intercepts to the left and right of the image

--- render_software/tests/pixel_planner_tests.rs
+++ render_software/tests/pixel_planner_tests.rs
@@ -121,3 +121,72 @@ fn transparent_rectangle_on_rectangle() {
     assert!(pixel_plan[1].iter_as_spans().collect::<Vec<_>>() == vec![ScanSpan::opaque(100..140, program_data_id_1), ScanSpan::opaque(140..160, program_data_id_1), ScanSpan::transparent(140..160, program_data_id_2), ScanSpan::opaque(160..200, program_data_id_1)], "[1, y == 140.0] {:?}", pixel_plan[1].iter_as_spans().collect::<Vec<_>>());
     assert!(pixel_plan[2].iter_as_spans().collect::<Vec<_>>() == vec![ScanSpan::opaque(100..140, program_data_id_1), ScanSpan::opaque(140..160, program_data_id_1), ScanSpan::transparent(140..160, program_data_id_2), ScanSpan::opaque(160..200, program_data_id_1)], "[2, y == 141.0] {:?}", pixel_plan[2].iter_as_spans().collect::<Vec<_>>());
 }
+
+#[test]
+fn clip_left() {
+    // Declare a program cache for the rectangle
+    let mut program_cache   = PixelProgramCache::empty();
+    let program_id          = program_cache.add_program(PerPixelProgramFn::from(|_x, _y, _data: &()| 12.0f64));
+    let mut data_cache      = program_cache.create_data_cache();
+    let program_data_id     = program_cache.store_program_data(&program_id, &mut data_cache, ());
+
+    let rectangle_shape = ShapeId::new();
+    let rectangle_edge  = RectangleEdge::new(rectangle_shape, -100.0..200.0, 125.0..175.0);
+    let edge_plan       = EdgePlan::new().with_shape_description(rectangle_shape, ShapeDescriptor::opaque(program_data_id)).with_edge(rectangle_edge);
+
+    let pixel_plan      = plan_pixel_scanlines(&edge_plan, &[124.0, 125.0, 126.0], 0..1000);
+    assert!(pixel_plan.len() == 3);
+
+    assert!(pixel_plan[0].iter_as_spans().count() == 0, "[0, y == 124.0] {} != 0", pixel_plan[0].iter_as_spans().count());
+    assert!(pixel_plan[1].iter_as_spans().count() == 1, "[1, y == 125.0] {} != 1", pixel_plan[1].iter_as_spans().count());
+    assert!(pixel_plan[2].iter_as_spans().count() == 1, "[2, y == 126.0] {} != 1", pixel_plan[2].iter_as_spans().count());
+
+    assert!(pixel_plan[1].iter_as_spans().collect::<Vec<_>>() == vec![ScanSpan::opaque(0..200, program_data_id)], "[1, y == 125.0] {:?}", pixel_plan[1].iter_as_spans().collect::<Vec<_>>());
+    assert!(pixel_plan[2].iter_as_spans().collect::<Vec<_>>() == vec![ScanSpan::opaque(0..200, program_data_id)], "[2, y == 126.0] {:?}", pixel_plan[1].iter_as_spans().collect::<Vec<_>>());
+}
+
+#[test]
+fn clip_right() {
+    // Declare a program cache for the rectangle
+    let mut program_cache   = PixelProgramCache::empty();
+    let program_id          = program_cache.add_program(PerPixelProgramFn::from(|_x, _y, _data: &()| 12.0f64));
+    let mut data_cache      = program_cache.create_data_cache();
+    let program_data_id     = program_cache.store_program_data(&program_id, &mut data_cache, ());
+
+    let rectangle_shape = ShapeId::new();
+    let rectangle_edge  = RectangleEdge::new(rectangle_shape, 100.0..1200.0, 125.0..175.0);
+    let edge_plan       = EdgePlan::new().with_shape_description(rectangle_shape, ShapeDescriptor::opaque(program_data_id)).with_edge(rectangle_edge);
+
+    let pixel_plan      = plan_pixel_scanlines(&edge_plan, &[124.0, 125.0, 126.0], 0..1000);
+    assert!(pixel_plan.len() == 3);
+
+    assert!(pixel_plan[0].iter_as_spans().count() == 0, "[0, y == 124.0] {} != 0", pixel_plan[0].iter_as_spans().count());
+    assert!(pixel_plan[1].iter_as_spans().count() == 1, "[1, y == 125.0] {} != 1", pixel_plan[1].iter_as_spans().count());
+    assert!(pixel_plan[2].iter_as_spans().count() == 1, "[2, y == 126.0] {} != 1", pixel_plan[2].iter_as_spans().count());
+
+    assert!(pixel_plan[1].iter_as_spans().collect::<Vec<_>>() == vec![ScanSpan::opaque(100..1000, program_data_id)], "[1, y == 125.0] {:?}", pixel_plan[1].iter_as_spans().collect::<Vec<_>>());
+    assert!(pixel_plan[2].iter_as_spans().collect::<Vec<_>>() == vec![ScanSpan::opaque(100..1000, program_data_id)], "[2, y == 126.0] {:?}", pixel_plan[1].iter_as_spans().collect::<Vec<_>>());
+}
+
+#[test]
+fn clip_both() {
+    // Declare a program cache for the rectangle
+    let mut program_cache   = PixelProgramCache::empty();
+    let program_id          = program_cache.add_program(PerPixelProgramFn::from(|_x, _y, _data: &()| 12.0f64));
+    let mut data_cache      = program_cache.create_data_cache();
+    let program_data_id     = program_cache.store_program_data(&program_id, &mut data_cache, ());
+
+    let rectangle_shape = ShapeId::new();
+    let rectangle_edge  = RectangleEdge::new(rectangle_shape, -100.0..1200.0, 125.0..175.0);
+    let edge_plan       = EdgePlan::new().with_shape_description(rectangle_shape, ShapeDescriptor::opaque(program_data_id)).with_edge(rectangle_edge);
+
+    let pixel_plan      = plan_pixel_scanlines(&edge_plan, &[124.0, 125.0, 126.0], 0..1000);
+    assert!(pixel_plan.len() == 3);
+
+    assert!(pixel_plan[0].iter_as_spans().count() == 0, "[0, y == 124.0] {} != 0", pixel_plan[0].iter_as_spans().count());
+    assert!(pixel_plan[1].iter_as_spans().count() == 1, "[1, y == 125.0] {} != 1", pixel_plan[1].iter_as_spans().count());
+    assert!(pixel_plan[2].iter_as_spans().count() == 1, "[2, y == 126.0] {} != 1", pixel_plan[2].iter_as_spans().count());
+
+    assert!(pixel_plan[1].iter_as_spans().collect::<Vec<_>>() == vec![ScanSpan::opaque(0..1000, program_data_id)], "[1, y == 125.0] {:?}", pixel_plan[1].iter_as_spans().collect::<Vec<_>>());
+    assert!(pixel_plan[2].iter_as_spans().collect::<Vec<_>>() == vec![ScanSpan::opaque(0..1000, program_data_id)], "[2, y == 126.0] {:?}", pixel_plan[1].iter_as_spans().collect::<Vec<_>>());
+}
