64ed88af196baf38cc25fb98626875d68202ab4f
Andrew Hunter <andrew@logicalshift.co.uk>
2023-08-21 21:32:59 +0100

    Add some TODOs

--- render_software/src/render/edge_plan.rs
+++ render_software/src/render/edge_plan.rs
@@ -19,6 +19,15 @@ where
     where
         TPixel: 'static + Default + Send + Pixel<N>,
     {
+        // TODO:
+        //      * Add a way to choose the scan planner to use
+        //      * Make it so that the width, height and gamma are all inputs to the appropriate stages instead of initialisation things (much more useful for streaming requests if they can vary)
+        //      * Add a trait to make the frame renderer from a target type and a source region renderer
+        //      * Add a trait for creating the region renderer from a type (eg, EdgePlan in this case) and a scan planner
+        //      * Add a trait for running a program (so we can just pass in that instead of the data type)
+        //      * Simplified 'to U8 pixel' type so we don't need to make TPixel a Pixel (and remove the 'N' from everything)
+        //      * Some way to do away with the `for<'a> &'a ...` constraints on the region planners
+
         let scanline_renderer       = ScanlineRenderer::new(program_cache, data);
         let scanline_planner        = PixelScanPlanner::<TEdge>::default();
         let edge_region_renderer    = EdgePlanRegionRenderer::<TEdge, _, _>::new(width, scanline_planner, scanline_renderer);
