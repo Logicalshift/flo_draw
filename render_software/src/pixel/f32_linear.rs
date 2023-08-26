a1cc0e906f77ab0bf89e505946a97fb3651bb086
Andrew Hunter <andrew@logicalshift.co.uk>
2023-08-26 23:10:00 +0100

    'splat' is quite slow, so we get better perf if we do that early on
    The 'pow' is dominating everything here so this isn't very noticable at the moment... but the gamma correction seems to be by far the slowest part of the rendering at the moment

--- render_software/src/pixel/f32_linear.rs
+++ render_software/src/pixel/f32_linear.rs
@@ -6,6 +6,7 @@ use super::u8_rgba::*;
 use flo_canvas as canvas;
 
 use wide::*;
+use once_cell::sync::{Lazy};
 
 use std::ops::*;
 
@@ -85,13 +86,15 @@ impl Pixel<4> for F32LinearPixel {
 impl ToGammaColorSpace<U8RgbaPremultipliedPixel> for F32LinearPixel {
     #[inline]
     fn to_gamma_colorspace(&self, gamma: f64) -> U8RgbaPremultipliedPixel {
+        static I32X4_255: Lazy<i32x4> = Lazy::new(|| i32x4::splat(255));
+
         // Remove gamma correction
         let gamma   = (1.0/gamma) as f32;
         let rgba    = self.0;
         let rgba    = rgba.powf(gamma);
         let rgba    = rgba * 255.0;
         let rgba    = rgba.fast_trunc_int();
-        let rgba    = rgba.min(i32x4::splat(255)).max(i32x4::splat(0));
+        let rgba    = rgba.min(*I32X4_255).max(i32x4::ZERO);
 
         let [r, g, b, a] = rgba.to_array();
         U8RgbaPremultipliedPixel::from_components([r as _, g as _, b as _, a as _])
