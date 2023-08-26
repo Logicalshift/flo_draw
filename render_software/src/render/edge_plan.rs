b12c64d4a7b1ab07157ce9f23a67750ab9f3dc1a
Andrew Hunter <andrew@logicalshift.co.uk>
2023-08-26 16:39:32 +0100

    Remove the old 'render whole frame' routine

--- render_software/src/render/edge_plan.rs
+++ render_software/src/render/edge_plan.rs
@@ -1,39 +1,10 @@
-use super::renderer::*;
 use super::render_source_trait::*;
 use super::edgeplan_region_renderer::*;
-use super::frame_size::*;
 use super::scanline_renderer::*;
-use super::u8_frame_renderer::*;
 
 use crate::edgeplan::*;
 use crate::pixel::*;
 use crate::scanplan::*;
-
-impl<TEdge> EdgePlan<TEdge>
-where
-    TEdge: EdgeDescriptor,
-{
-    ///
-    /// Renders an edge plan to an 8-bit RGBA buffer (must contain width*height pixels)
-    ///
-    pub fn render_whole_frame<TPixel>(&self, data: &PixelProgramDataCache<TPixel>, width: usize, height: usize, gamma: f64, target: &mut [U8RgbaPremultipliedPixel])
-    where
-        TPixel: 'static + Default + Send + Copy + AlphaBlend + ToGammaColorSpace<U8RgbaPremultipliedPixel>,
-    {
-        // TODO:
-        //      * Add a way to choose the scan planner to use
-        //      * Add a trait to make the frame renderer from a target type and a source region renderer
-        //      * Add a trait for creating the region renderer from a type (eg, EdgePlan in this case) and a scan planner
-        //      * Some way to do away with the `for<'a> &'a ...` constraints on the region planners
-
-        let scanline_renderer       = ScanlineRenderer::new(data);
-        let scanline_planner        = PixelScanPlanner::<TEdge>::default();
-        let edge_region_renderer    = EdgePlanRegionRenderer::new(scanline_planner, scanline_renderer);
-        let frame_renderer          = U8FrameRenderer::new(edge_region_renderer);
-
-        (&frame_renderer).render(&GammaFrameSize { width, height, gamma }, self, target);
-    }
-}
 
 impl<'a, TEdge, TScanPlanner, TProgramRunner, TPixel> RenderSource<'a, TScanPlanner, TProgramRunner, TPixel> for EdgePlan<TEdge>
 where
