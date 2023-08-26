dc6d205bf13a08a1ebfabca849e9d6490775bb90
Andrew Hunter <andrew@logicalshift.co.uk>
2023-08-26 17:34:44 +0100

    Add a basic pixel program runner that uses a function directly (for cases where we don't want to use the full program cache)

--- render_software/src/pixel/pixel_program_runner.rs
+++ render_software/src/pixel/pixel_program_runner.rs
@@ -1,5 +1,6 @@
 use super::pixel_program_cache::*;
 
+use std::marker::{PhantomData};
 use std::ops::{Range};
 
 ///
@@ -16,4 +17,38 @@ pub trait PixelProgramRunner {
     /// eventually be rendered at the specified y position in the frame.
     ///
     fn run_program(&self, program_data: PixelProgramDataId, target: &mut [Self::TPixel], x_range: Range<i32>, y_pos: f64);
+}
+
+///
+/// A pixel program runner that is implemented as a basic function
+///
+pub struct BasicPixelProgramRunner<TFn, TPixel> 
+where
+    TFn: Fn(PixelProgramDataId, &mut [TPixel], Range<i32>, f64),
+{
+    pixel_fn:   TFn,
+    pixel:      PhantomData<TPixel>,
+}
+
+impl<TFn, TPixel> From<TFn> for BasicPixelProgramRunner<TFn, TPixel>
+where
+    TFn: Fn(PixelProgramDataId, &mut [TPixel], Range<i32>, f64),
+{
+    fn from(func: TFn) -> Self {
+        BasicPixelProgramRunner { 
+            pixel_fn: func, 
+            pixel: PhantomData
+        }
+    }
+}
+
+impl<TFn, TPixel> PixelProgramRunner for BasicPixelProgramRunner<TFn, TPixel>
+where
+    TFn: Fn(PixelProgramDataId, &mut [TPixel], Range<i32>, f64),
+{
+    type TPixel = TPixel;
+
+    fn run_program(&self, program_data: PixelProgramDataId, target: &mut [Self::TPixel], x_range: Range<i32>, y_pos: f64) {
+        (self.pixel_fn)(program_data, target, x_range, y_pos)
+    }
 }
