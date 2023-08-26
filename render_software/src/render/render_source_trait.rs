d18727571845dd025dde858c2bd897ca02e09da3
Andrew Hunter <andrew@logicalshift.co.uk>
2023-08-26 16:38:39 +0100

    Add a where clause

--- render_software/src/render/render_source_trait.rs
+++ render_software/src/render/render_source_trait.rs
@@ -7,7 +7,12 @@ use crate::scanplan::*;
 ///
 /// A render source can create an edge region renderer to be used with a render target
 ///
-pub trait RenderSource<'a, TScanPlanner: ScanPlanner, TProgramRunner: PixelProgramRunner<TPixel>, TPixel: 'a> {
+pub trait RenderSource<'a, TScanPlanner, TProgramRunner, TPixel>
+where
+    TPixel:         'a,
+    TScanPlanner:   ScanPlanner,
+    TProgramRunner: PixelProgramRunner<TPixel>,
+{
     /// The region renderer takes instances of this type and uses them to generate pixel values in a region
     type RegionRenderer: Renderer<Region=RenderSlice, Source=Self, Dest=[TPixel]>;
 
