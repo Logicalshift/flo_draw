fb8dabb8f8e7e2ed91af7a02da373dace04989cf
Andrew Hunter <andrew@logicalshift.co.uk>
2023-08-28 12:55:27 +0100

    Use the pixel program cache to define the foreground and background colours for the image

--- render_software/examples/circle_to_png.rs
+++ render_software/examples/circle_to_png.rs
@@ -1,6 +1,7 @@
 use flo_render_software::edgeplan::*;
 use flo_render_software::edges::*;
 use flo_render_software::pixel::*;
+use flo_render_software::pixel_programs::*;
 use flo_render_software::render::*;
 use flo_render_software::curves::bezier::vectorize::{CircularDistanceField};
 
@@ -13,6 +14,11 @@ use std::time::{Instant};
 /// Renders a circle to circle.png using an edge plan and a straightforward pixel program
 ///
 pub fn main() {
+    // Create a program cache and data cache
+    let mut program_cache   = PixelProgramCache::empty();
+    let mut data_cache      = program_cache.create_data_cache();
+    let solid_color         = program_cache.add_program(SolidColorProgram);
+
     // Create a circular edge (using the circle distance field from flo_curves)
     // This is a more low-level way to represent a 2D scene than describing the rendering instructions using flo_canvas::Draw
     let radius              = 500.0;
@@ -21,24 +27,15 @@ pub fn main() {
     let circle              = ContourEdge::new((960.0-radius, 540.0-radius), circle_shape, CircularDistanceField::with_radius(radius));
     let background          = RectangleEdge::new(background_shape, 0.0..1920.0, 0.0..1080.0);
 
+    let background_color    = program_cache.store_program_data(&solid_color, &mut data_cache, SolidColorData(F32LinearPixel::from_components([0.0, 0.0, 0.0, 0.0])));
+    let foreground_color    = program_cache.store_program_data(&solid_color, &mut data_cache, SolidColorData(F32LinearPixel::from_components([0.0, 0.0, 1.0, 1.0])));
+
     // Create an edge plan that renders this circle
     let edge_plan       = EdgePlan::<Box<dyn EdgeDescriptor>>::new()
-        .with_shape_description(background_shape, ShapeDescriptor { programs: smallvec![PixelProgramDataId(0)], is_opaque: true, z_index: 0 })
-        .with_shape_description(circle_shape, ShapeDescriptor { programs: smallvec![PixelProgramDataId(1)], is_opaque: true, z_index: 1 })
+        .with_shape_description(background_shape, ShapeDescriptor { programs: smallvec![background_color], is_opaque: true, z_index: 0 })
+        .with_shape_description(circle_shape, ShapeDescriptor { programs: smallvec![foreground_color], is_opaque: true, z_index: 1 })
         .with_edge(Box::new(circle))
         .with_edge(Box::new(background));
-
-    // Pixel program that renders everything in blue
-    let pixel_programs = BasicPixelProgramRunner::from(|program_id, data: &mut [F32LinearPixel], range, _ypos| {
-        let col = if program_id == PixelProgramDataId(1) {
-            F32LinearPixel::from_components([0.0, 0.0, 1.0, 1.0])
-        } else {
-            F32LinearPixel::from_components([0.0, 0.0, 0.0, 0.0])
-        };
-        for x in range {
-            data[x as usize] = col;
-        }
-    });
 
     // Render to a buffer as a perf test
     let mut frame   = vec![0u8; 1920*1080*4];
@@ -46,7 +43,7 @@ pub fn main() {
 
     let render_start = Instant::now();
     for _ in 0..10 {
-        render_frame_with_planner(PixelScanPlanner::default(), &pixel_programs, &edge_plan, &mut rgba);
+        render_frame_with_planner(PixelScanPlanner::default(), &data_cache, &edge_plan, &mut rgba);
     }
     let render_time = Instant::now().duration_since(render_start);
     let avg_micros  = render_time.as_micros() / 10;
@@ -56,7 +53,7 @@ pub fn main() {
     let mut term_renderer = TerminalRenderTarget::new(1920, 1080);
 
     let render_start = Instant::now();
-    render_frame_with_planner(PixelScanPlanner::default(), &pixel_programs, &edge_plan, &mut term_renderer);
+    render_frame_with_planner(PixelScanPlanner::default(), &data_cache, &edge_plan, &mut term_renderer);
     let render_time = Instant::now().duration_since(render_start);
 
     println!("PNG render time: {}.{}ms", render_time.as_micros()/1000, render_time.as_micros()%1000);
