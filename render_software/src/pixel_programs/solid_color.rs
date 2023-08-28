d4d635ff27158f2fee451f7d8ab198998d6c0cfb
Andrew Hunter <andrew@logicalshift.co.uk>
2023-08-28 13:33:36 +0100

    Change the SolidColorProgram to use any pixel type

--- render_software/src/pixel_programs/solid_color.rs
+++ render_software/src/pixel_programs/solid_color.rs
@@ -1,18 +1,28 @@
 use crate::pixel::*;
 
+use std::marker::{PhantomData};
 use std::ops::{Range};
+use std::sync::*;
 
 /// Data for a solid colour pixel
-pub struct SolidColorData(pub F32LinearPixel);
+pub struct SolidColorData<TPixel: Send + Sync>(pub TPixel);
 
 ///
 /// Pixel program that writes out a solid colour according to the solid colour data it's supplied
 ///
-pub struct SolidColorProgram;
+pub struct SolidColorProgram<TPixel: Copy + Send + Sync> { 
+    pixel: PhantomData<Mutex<TPixel>>
+}
 
-impl PixelProgram for SolidColorProgram {
-    type Pixel          = F32LinearPixel;
-    type ProgramData    = SolidColorData;
+impl<TPixel: Copy + Send + Sync> Default for SolidColorProgram<TPixel> {
+    fn default() -> Self {
+        SolidColorProgram { pixel: PhantomData }
+    }
+}
+
+impl<TPixel: Copy + Send + Sync> PixelProgram for SolidColorProgram<TPixel> {
+    type Pixel          = TPixel;
+    type ProgramData    = SolidColorData<TPixel>;
 
     #[inline]
     fn draw_pixels(&self, _data_cache: &PixelProgramDataCache<Self::Pixel>, target: &mut [Self::Pixel], x_range: Range<i32>, _y_pos: f64, program_data: &Self::ProgramData) {
