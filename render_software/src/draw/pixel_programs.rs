900180b59dcc2a77c66fdbdac400e3155aa558fc
Andrew Hunter <andrew@logicalshift.co.uk>
2023-08-29 12:47:37 +0100

    Add the 'source over' program to the pixel programs list

--- render_software/src/draw/pixel_programs.rs
+++ render_software/src/draw/pixel_programs.rs
@@ -13,6 +13,9 @@ where
 
     /// The basic solid colour pixel program
     pub (super) solid_color: StoredPixelProgram<SolidColorProgram<TPixel>>,
+
+    /// The 'source over' alpha blending pixel program
+    pub (super) source_over_color: StoredPixelProgram<SourceOverColorProgram<TPixel>>
 }
 
 impl<TPixel, const N: usize> Default for CanvasPixelPrograms<TPixel, N> 
@@ -22,10 +25,12 @@ where
     fn default() -> Self {
         let mut cache   = PixelProgramCache::empty();
         let solid_color = cache.add_program(SolidColorProgram::default());
+        let source_over = cache.add_program(SourceOverColorProgram::default());
 
         CanvasPixelPrograms { 
-            program_cache:  cache, 
-            solid_color:    solid_color,
+            program_cache:      cache, 
+            solid_color:        solid_color,
+            source_over_color:  source_over,
         }
     }
 }
