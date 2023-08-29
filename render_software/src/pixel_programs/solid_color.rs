61427a0e5a78aef020466b940f391b97a332d7a7
Andrew Hunter <andrew@logicalshift.co.uk>
2023-08-29 12:45:02 +0100

    Add a basic 'source over' alpha blending program for when the alpha value is non-zero for a colour

--- render_software/src/pixel_programs/solid_color.rs
+++ render_software/src/pixel_programs/solid_color.rs
@@ -31,3 +31,34 @@ impl<TPixel: Copy + Send + Sync> PixelProgram for SolidColorProgram<TPixel> {
         }
     }
 }
+
+///
+/// Pixel program that applies an alpha-blended colour using the source over algorithm
+///
+pub struct SourceOverColorProgram<TPixel: Copy + Send + Sync> { 
+    pixel: PhantomData<Mutex<TPixel>>
+}
+
+impl<TPixel: Copy + Send + Sync> Default for SourceOverColorProgram<TPixel> 
+where
+    TPixel: AlphaBlend
+{
+    fn default() -> Self {
+        SourceOverColorProgram { pixel: PhantomData }
+    }
+}
+
+impl<TPixel: Copy + Send + Sync> PixelProgram for SourceOverColorProgram<TPixel>
+where
+    TPixel: AlphaBlend
+{
+    type Pixel          = TPixel;
+    type ProgramData    = SolidColorData<TPixel>;
+
+    #[inline]
+    fn draw_pixels(&self, _data_cache: &PixelProgramDataCache<Self::Pixel>, target: &mut [Self::Pixel], x_range: Range<i32>, _y_pos: f64, program_data: &Self::ProgramData) {
+        for pixel in target[(x_range.start as usize)..(x_range.end as usize)].iter_mut() {
+            *pixel = pixel.source_over(program_data.0);
+        }
+    }
+}
