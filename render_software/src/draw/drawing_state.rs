c0027703bdd3957342f169b9d5944fa86aa22380
Andrew Hunter <andrew@logicalshift.co.uk>
2023-08-29 11:49:33 +0100

    Add the fill and stroke brush and program to the drawing state

--- render_software/src/draw/drawing_state.rs
+++ render_software/src/draw/drawing_state.rs
@@ -1,13 +1,77 @@
+use crate::pixel::*;
+use crate::edgeplan::*;
+
+use flo_canvas as canvas;
+
+///
+/// A brush represents what will be used to fill in the next region 
+///
+#[derive(Clone)]
+pub enum Brush {
+    /// Basic solid colour brush (will be drawn opaque so the image behind will be hidden)
+    OpaqueSolidColor(canvas::Color),
+
+    /// Transparent solid colour brush (will be blended with the image behind)
+    TransparentSolidColor(canvas::Color),
+}
+
 ///
 /// Represents the active drawing state for a canvas drawing
 ///
 #[derive(Clone)]
 pub struct DrawingState {
+    /// The shape descriptor that will be used for filling the next shape (or None if we haven't allocated data for it yet)
+    pub (super) fill_program: Option<ShapeDescriptor>,
 
+    /// The shape descriptor that will be used for filling the stroke of the next shape (or None if we haven't allocated data for it yet)
+    pub (super) stroke_program: Option<ShapeDescriptor>,
+
+    /// The brush to select next time fill_program is None
+    pub (super) next_fill_brush: Brush,
+
+    /// The brush to select next time stroke_program is None
+    pub (super) next_stroke_brush: Brush,
 }
 
 impl Default for DrawingState {
     fn default() -> Self {
-        DrawingState { }
+        DrawingState { 
+            fill_program:       None,
+            stroke_program:     None,
+            next_fill_brush:    Brush::OpaqueSolidColor(canvas::Color::Rgba(0.0, 0.0, 0.0, 1.0)),
+            next_stroke_brush:  Brush::OpaqueSolidColor(canvas::Color::Rgba(0.0, 0.0, 0.0, 1.0)),
+        }
+    }
+}
+
+impl DrawingState {
+    ///
+    /// Updates the state so that the next shape added will use a solid fill colour 
+    ///
+    pub fn fill_solid_color(&mut self, colour: canvas::Color) {
+        // This clears the fill program so we allocate data for it next time
+        self.fill_program = None;
+
+        // Choose opaque or transparent for the brush based on the alpha component
+        if colour.alpha_component() >= 1.0 {
+            self.next_fill_brush = Brush::OpaqueSolidColor(colour);
+        } else {
+            self.next_fill_brush = Brush::TransparentSolidColor(colour);
+        }
+    }
+
+    ///
+    /// Updates the state so that the next shape added will use a solid fill colour 
+    ///
+    pub fn stroke_solid_color(&mut self, colour: canvas::Color) {
+        // This clears the fill program so we allocate data for it next time
+        self.stroke_program = None;
+
+        // Choose opaque or transparent for the brush based on the alpha component
+        if colour.alpha_component() >= 1.0 {
+            self.next_stroke_brush = Brush::OpaqueSolidColor(colour);
+        } else {
+            self.next_stroke_brush = Brush::TransparentSolidColor(colour);
+        }
     }
 }
