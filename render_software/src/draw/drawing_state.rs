a907feb7500cac045d850f5f7c648a6f6e65f59b
Andrew Hunter <andrew@logicalshift.co.uk>
2023-08-30 10:36:39 +0100

    Add a transform to the drawing state, and specify how coordinates end up

--- render_software/src/draw/drawing_state.rs
+++ render_software/src/draw/drawing_state.rs
@@ -22,6 +22,11 @@ pub enum Brush {
 ///
 #[derive(Clone)]
 pub struct DrawingState {
+    /// The transform to apply to points added to a path
+    ///
+    /// The internal coordinates should range from 1 to -1 along the y axis (with the x scaling determined by whatever is needed to make the pixels square)
+    pub (super) transform: canvas::Transform2D,
+
     /// The shape descriptor that will be used for filling the next shape (or None if we haven't allocated data for it yet)
     pub (super) fill_program: Option<ShapeDescriptor>,
 
@@ -50,6 +55,7 @@ pub struct DrawingState {
 impl Default for DrawingState {
     fn default() -> Self {
         DrawingState { 
+            transform:          canvas::Transform2D::identity(),
             fill_program:       None,
             stroke_program:     None,
             next_fill_brush:    Brush::OpaqueSolidColor(canvas::Color::Rgba(0.0, 0.0, 0.0, 1.0)),
@@ -136,9 +142,9 @@ impl DrawingState {
 
         match path_op {
             NewPath                                             => self.path_new(),
-            Move(x, y)                                          => self.path_move(x as _, y as _),
-            Line(x, y)                                          => self.path_line(x as _, y as _),
-            BezierCurve(((cp1x, cp1y), (cp2x, cp2y)), (dx, dy)) => self.path_bezier_curve((cp1x as _, cp1y as _), (cp2x as _, cp2y as _), (dx as _, dy as _)),
+            Move(x, y)                                          => self.path_move(x, y),
+            Line(x, y)                                          => self.path_line(x, y),
+            BezierCurve(((cp1x, cp1y), (cp2x, cp2y)), (dx, dy)) => self.path_bezier_curve((cp1x, cp1y), (cp2x, cp2y), (dx, dy)),
             ClosePath                                           => self.path_close(),
         }
     }
@@ -155,7 +161,11 @@ impl DrawingState {
     ///
     /// Moves to start a new subpath
     ///
-    pub fn path_move(&mut self, x: f64, y: f64) {
+    pub fn path_move(&mut self, x: f32, y: f32) {
+        let (x, y) = self.transform.transform_point(x, y);
+        let x = x as f64;
+        let y = y as f64;
+
         // Start a new subpath if we've generated any new edges
         if self.subpaths.pop() != Some(self.path_edges.len()) {
             self.subpaths.push(self.path_edges.len());
@@ -168,7 +178,11 @@ impl DrawingState {
     ///
     /// Draws a line to a position
     ///
-    pub fn path_line(&mut self, x: f64, y: f64) {
+    pub fn path_line(&mut self, x: f32, y: f32) {
+        let (x, y) = self.transform.transform_point(x, y);
+        let x = x as f64;
+        let y = y as f64;
+
         // Create a line from the last position
         let next_pos    = Coord2(x, y);
         let line        = (self.path_position, next_pos);
@@ -183,11 +197,15 @@ impl DrawingState {
     ///
     /// Draws a bezier curve to a position
     ///
-    pub fn path_bezier_curve(&mut self, cp1: (f64, f64), cp2: (f64, f64), end: (f64, f64)) {
+    pub fn path_bezier_curve(&mut self, cp1: (f32, f32), cp2: (f32, f32), end: (f32, f32)) {
+        let cp1 = self.transform.transform_point(cp1.0, cp1.1);
+        let cp2 = self.transform.transform_point(cp2.0, cp2.1);
+        let end = self.transform.transform_point(end.0, end.1);
+
         // Convert the points
-        let cp1 = Coord2(cp1.0, cp1.1);
-        let cp2 = Coord2(cp2.0, cp2.1);
-        let end = Coord2(end.0, end.1);
+        let cp1 = Coord2(cp1.0 as _, cp1.1 as _);
+        let cp2 = Coord2(cp2.0 as _, cp2.1 as _);
+        let end = Coord2(end.0 as _, end.1 as _);
 
         // Create a curve
         let curve = Curve::from_points(self.path_position, (cp1, cp2), end);
@@ -210,7 +228,9 @@ impl DrawingState {
 
                 // Want to close by drawing a line from the end of last_curve to the subpath start
                 if start_point != self.path_position {
-                    self.path_line(start_point.x(), start_point.y());
+                    let line = (self.path_position, start_point);
+                    self.path_edges.push(line_to_bezier(&line));
+                    self.path_position = start_point;
                 }
             }
         }
