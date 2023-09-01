4742caabfd24f22776fbfdc9eba253df243cb6cf
Andrew Hunter <andrew@logicalshift.co.uk>
2023-09-01 17:22:40 +0100

    Add a transform for mapping between edge plan x coordinates and pixel coordinates in the final result

--- render_software/src/scanplan/scan_planner.rs
+++ render_software/src/scanplan/scan_planner.rs
@@ -4,6 +4,35 @@ use crate::edgeplan::*;
 use std::ops::{Range};
 
 ///
+/// Describes how to transform the x positions in the edge plan to the viewport x positions
+///
+#[derive(Clone, Copy)]
+pub struct ScanlineTransform {
+    /// Value to add to the x coordinates before transforming
+    offset: f64,
+
+    /// The scale to apply to the 
+    scale: f64,
+
+    /// The reciprocal of the scale
+    scale_recip: f64,
+}
+
+impl ScanlineTransform {
+    ///
+    /// Creates a scanline transform that maps from the specified source x range to pixel values of 0..pixel_width
+    ///
+    #[inline]
+    pub fn for_region(source_x_range: Range<f64>, pixel_width: usize) -> Self {
+        ScanlineTransform {
+            offset:         -source_x_range.start,
+            scale:          (pixel_width as f64) / (source_x_range.start-source_x_range.end),
+            scale_recip:    (source_x_range.start-source_x_range.end) / (pixel_width as f64),
+        }
+    }
+}
+
+///
 /// A scan planner is an algorithm that discovers where along a scanline to render pixels using pixel programs
 ///
 pub trait ScanPlanner : Send + Sync {
