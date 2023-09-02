96f286f27fc31e6bc0697a88a715ad9da19d7f75
Andrew Hunter <andrew@logicalshift.co.uk>
2023-09-02 16:14:29 +0100

    Add a test that mirrors the behaviour of the canvas rendering we were trying to do
    (So there are a couple of issues: one is that the edges aren't rendering properly, and the other is that we're not using the right program after clearing the canvas, this test only ensures there's no panicking)

--- render_software/tests/canvas_render_tests.rs
+++ render_software/tests/canvas_render_tests.rs
@@ -5,12 +5,9 @@ use flo_render_software::scanplan::*;
 
 use flo_render_software::canvas::*;
 
-use std::time::{Instant};
-
-///
-/// Draw some canvas commands to the terminal
-///
-pub fn main() {
+#[test]
+pub fn render_simple_circle() {
+    // Render a basic circle
     let mut drawing = Vec::<Draw>::new();
 
     drawing.clear_canvas(Color::Rgba(0.0, 0.0, 0.0, 0.0));
@@ -27,18 +24,6 @@ pub fn main() {
     let mut frame   = vec![0u8; 1920*1080*4];
     let mut rgba    = RgbaFrame::from_bytes(1920, 1080, 2.2, &mut frame).unwrap();
 
-    let render_start = Instant::now();
-    for _ in 0..100 {
-        let renderer = CanvasDrawingRegionRenderer::new(PixelScanPlanner::default(), ScanlineRenderer::new(canvas_drawing.program_runner()), 1080);
-        rgba.render(renderer, &canvas_drawing);
-    }
-    let render_time = Instant::now().duration_since(render_start);
-    let avg_micros  = render_time.as_micros() / 100;
-    println!("Frame render time: {}.{}ms", avg_micros/1000, avg_micros%1000);
-
-    // Render the mascot to the terminal
-    let mut term_renderer = TerminalRenderTarget::new(500, 400);
-
     let renderer = CanvasDrawingRegionRenderer::new(PixelScanPlanner::default(), ScanlineRenderer::new(canvas_drawing.program_runner()), 1080);
-    term_renderer.render(renderer, &canvas_drawing);
+    rgba.render(renderer, &canvas_drawing);
 }
