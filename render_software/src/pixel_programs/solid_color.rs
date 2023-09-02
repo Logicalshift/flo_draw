60e1972fe2aa05b69c81beff3622fa3d4b694f8d
Andrew Hunter <andrew@logicalshift.co.uk>
2023-09-02 21:27:00 +0100

    Fix source_over so that the source pixel is the solid colour, not the target colour.
    (This reveals some more glitches, unfortunately, but does also improve the rendering a lot as the alpha-blended items weren't showing before)

--- render_software/src/pixel_programs/solid_color.rs
+++ render_software/src/pixel_programs/solid_color.rs
@@ -59,7 +59,7 @@ where
     #[inline]
     fn draw_pixels(&self, _data_cache: &PixelProgramDataCache<Self::Pixel>, target: &mut [Self::Pixel], x_range: Range<i32>, _: &ScanlineTransform, _y_pos: f64, program_data: &Self::ProgramData) {
         for pixel in target[(x_range.start as usize)..(x_range.end as usize)].iter_mut() {
-            *pixel = pixel.source_over(program_data.0);
+            *pixel = program_data.0.source_over(*pixel);
         }
     }
 }
