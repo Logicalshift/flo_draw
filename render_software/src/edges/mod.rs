8326badcf9afe9f9027bfa2912b0ba98e3c333b8
Andrew Hunter <andrew@logicalshift.co.uk>
2023-09-03 22:53:21 +0100

    Delete the bezier curve edge type (should use the subpath type instead)

--- render_software/src/edges/mod.rs
+++ render_software/src/edges/mod.rs
@@ -1,9 +1,7 @@
 mod contour_edge;
-mod bezier_curve_edge;
 mod bezier_subpath_edge;
 mod rectangle_edge;
 
 pub use contour_edge::*;
-pub use bezier_curve_edge::*;
 pub use bezier_subpath_edge::*;
 pub use rectangle_edge::*;
