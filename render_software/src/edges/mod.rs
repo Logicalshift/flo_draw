18838c04929f1ff9dc64213332827562d3feea10
Andrew Hunter <andrew@logicalshift.co.uk>
2023-09-25 22:22:42 +0100

    Add a line stroke edge that can be used to defer the 'stroke' operation we need to do to render lines

--- render_software/src/edges/mod.rs
+++ render_software/src/edges/mod.rs
@@ -1,7 +1,9 @@
 mod contour_edge;
 mod bezier_subpath_edge;
+mod line_stroke_edge;
 mod rectangle_edge;
 
 pub use contour_edge::*;
 pub use bezier_subpath_edge::*;
+pub use line_stroke_edge::*;
 pub use rectangle_edge::*;
