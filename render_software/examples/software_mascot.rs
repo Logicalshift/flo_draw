1f8a0a79eaf1ae67376ae802ab1af9a87c20322c
Andrew Hunter <andrew@logicalshift.co.uk>
2023-10-08 16:40:49 +0100

    Add in a layer blend as another useful thing to test
    ... given that this isn't working yet either

--- render_software/examples/software_mascot.rs
+++ render_software/examples/software_mascot.rs
@@ -14,6 +14,15 @@ pub fn main() {
     // Decode
     let mascot = decode_drawing(MASCOT.chars()).collect::<Result<Vec<Draw>, _>>().unwrap();
 
+    // Splice in a layer blend
+    let mut mascot      = mascot;
+    let mut layer_blend = vec![];
+
+    layer_blend.layer(LayerId(1));
+    layer_blend.layer_alpha(LayerId(1), 0.25);
+
+    mascot.splice(1..1, layer_blend);
+
     /*
     // Splice in setting a clip path
     let mut mascot  = mascot;
