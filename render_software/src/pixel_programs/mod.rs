7b743acde49967a060954d1a7fc498254b1c9452
Andrew Hunter <andrew@logicalshift.co.uk>
2023-10-10 09:38:23 +0100

    Put the 'source over' pixel program in its own file

--- render_software/src/pixel_programs/mod.rs
+++ render_software/src/pixel_programs/mod.rs
@@ -1,3 +1,5 @@
 mod solid_color;
+mod source_over;
 
 pub use solid_color::*;
+pub use source_over::*;
--- render_software/src/pixel_programs/source_over.rs
+++ render_software/src/pixel_programs/source_over.rs
@@ -1,3 +1,5 @@
+use super::solid_color::*;
+
 use crate::pixel::*;
 use crate::scanplan::*;
 
@@ -5,30 +7,33 @@ use std::marker::{PhantomData};
 use std::ops::{Range};
 use std::sync::*;
 
-/// Data for a solid colour pixel
-pub struct SolidColorData<TPixel: Send + Sync>(pub TPixel);
-
 ///
-/// Pixel program that writes out a solid colour according to the solid colour data it's supplied
+/// Pixel program that applies an alpha-blended colour using the source over algorithm
 ///
-pub struct SolidColorProgram<TPixel: Copy + Send + Sync> { 
+pub struct SourceOverColorProgram<TPixel: Copy + Send + Sync> { 
     pixel: PhantomData<Mutex<TPixel>>
 }
 
-impl<TPixel: Copy + Send + Sync> Default for SolidColorProgram<TPixel> {
+impl<TPixel: Copy + Send + Sync> Default for SourceOverColorProgram<TPixel> 
+where
+    TPixel: AlphaBlend
+{
     fn default() -> Self {
-        SolidColorProgram { pixel: PhantomData }
+        SourceOverColorProgram { pixel: PhantomData }
     }
 }
 
-impl<TPixel: Copy + Send + Sync> PixelProgram for SolidColorProgram<TPixel> {
+impl<TPixel: Copy + Send + Sync> PixelProgram for SourceOverColorProgram<TPixel>
+where
+    TPixel: AlphaBlend
+{
     type Pixel          = TPixel;
     type ProgramData    = SolidColorData<TPixel>;
 
     #[inline]
     fn draw_pixels(&self, _data_cache: &PixelProgramDataCache<Self::Pixel>, target: &mut [Self::Pixel], x_range: Range<i32>, _: &ScanlineTransform, _y_pos: f64, program_data: &Self::ProgramData) {
         for pixel in target[(x_range.start as usize)..(x_range.end as usize)].iter_mut() {
-            *pixel = program_data.0;
+            *pixel = program_data.0.source_over(*pixel);
         }
     }
 }
