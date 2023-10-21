3fd53b4811913d99f805e789db3d7c5eb5ab389f
Andrew Hunter <andrew@logicalshift.co.uk>
2023-10-21 23:30:53 +0100

    Add a prototype for a texture renderer pixel program
    (This isn't doing any blending yet)

---
+++ render_software/src/pixel_programs/basic_texture.rs
@@ -0,0 +1,98 @@
+use crate::pixel::*;
+use crate::scanplan::*;
+
+use std::marker::{PhantomData};
+use std::ops::{Range};
+use std::sync::*;
+
+///
+/// Trait implemented by types that can be read from a texture type
+///
+pub trait TextureReader<TTexture> : Send + Sync 
+where
+    TTexture: Send + Sync,
+{
+    /// Reads the pixel at the specified position in the texture
+    ///
+    /// The coordinates are fractions of pixels
+    fn read_pixel(texture: &TTexture, x: f64, y: f64) -> Self;
+}
+
+pub struct TextureData<TTexture>
+where
+    TTexture:       Send + Sync,
+{
+    /// The texture that this program will read from
+    texture: Arc<TTexture>,
+
+    // The top two rows of the transformation matrix between source coordinates and texture coordinates
+    transform: [[f64; 3]; 2],
+}
+
+///
+/// Program that pixels from a texture, without doing any filtering or mipmapping
+///
+pub struct BasicTextureProgram<TTextureReader, TTexture>
+where
+    TTexture:       Send + Sync,
+    TTextureReader: TextureReader<TTexture>,
+{
+    /// Placeholder for the texture data
+    texture: PhantomData<Arc<TTexture>>,
+
+    /// Placeholder for the texture reader type
+    texture_reader: PhantomData<TTextureReader>
+}
+
+impl<TTextureReader, TTexture> Default for BasicTextureProgram<TTextureReader, TTexture>
+where
+    TTexture:       Send + Sync,
+    TTextureReader: TextureReader<TTexture>,
+{
+    ///
+    /// Creates a basic texture program that will read from the specified texture
+    ///
+    fn default() -> BasicTextureProgram<TTextureReader, TTexture> {
+        BasicTextureProgram {
+            texture:        PhantomData,
+            texture_reader: PhantomData,
+        }
+    }
+}
+
+impl<TTextureReader, TTexture> PixelProgram for BasicTextureProgram<TTextureReader, TTexture>
+where
+    TTexture:       Send + Sync,
+    TTextureReader: TextureReader<TTexture>,
+{
+    type Pixel          = TTextureReader;
+    type ProgramData    = TextureData<TTexture>;
+
+    #[inline]
+    fn draw_pixels(&self, _data_cache: &PixelProgramDataCache<Self::Pixel>, target: &mut [Self::Pixel], pixel_range: Range<i32>, x_transform: &ScanlineTransform, y_pos: f64, data: &Self::ProgramData) {
+        // Read the data
+        let texture                 = &*data.texture;
+        let [[a, b, c], [d, e, f]]  = data.transform;
+
+        // Convert the start x position to source pixels
+        let x_pos   = x_transform.pixel_x_to_source_x(pixel_range.start);
+
+        // Partially calculate the transform and get the pixel size
+        let byc     = b * y_pos + c;
+        let eyf     = e * y_pos + f;
+        let dx      = x_transform.pixel_size();
+
+        // Calculate the position of teh 
+        let mut x_pos = x_pos;
+        for pixel in target[(pixel_range.start as usize)..(pixel_range.end as usize)].iter_mut() {
+            // Calculate the texture position
+            let tx = a * x_pos + byc;
+            let ty = d * x_pos + eyf;
+
+            *pixel = TTextureReader::read_pixel(texture, tx, ty);
+
+            // Move the x position along
+            x_pos += dx;
+        }
+    }
+}
--- render_software/src/pixel_programs/mod.rs
+++ render_software/src/pixel_programs/mod.rs
@@ -1,7 +1,9 @@
 mod solid_color;
 mod source_over;
 mod blend;
+mod basic_texture;
 
 pub use solid_color::*;
 pub use source_over::*;
 pub use blend::*;
+pub use basic_texture::*;
