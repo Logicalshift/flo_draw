c04debe4c7e42c4027cfaf3d8bcdee770dddc0cb
Andrew Hunter <andrew@logicalshift.co.uk>
2023-10-22 15:59:23 +0100

    Add functions for reading RGBA textures, make the internal values hidden

--- render_software/src/draw/texture.rs
+++ render_software/src/draw/texture.rs
@@ -15,53 +15,6 @@ pub enum Texture {
     Rgba(RgbaTexture),
 }
 
-impl RgbaTexture {
-    ///
-    /// Sets the bytes for a region of this image
-    ///
-    #[inline]
-    pub fn set_bytes(&mut self, x: usize, y: usize, width: usize, height: usize, bytes: &Vec<u8>) {
-        // Number of bytes to read/write at a time
-        let mut read_bytes = width * 4;
-
-        // Number of bytes to skip after every read_bytes
-        let mut read_skip_bytes = 0;
-
-        // Clip to the size of the image
-        let x       = x.min(self.width);
-        let y       = y.min(self.height);
-        let width   = if x + width > self.width {
-            let clip        = (x + width) - self.width;
-            read_bytes      = (width - clip) * 4;
-            read_skip_bytes = clip * 4;
-            width - clip
-        } else {
-            width 
-        };
-        let height  = height.min(self.height - y);
-
-        // After writing read_bytes, skip this many bytes to write
-        let write_skip_bytes = (self.width - width) * 4;
-
-        // Pointers for reading/writing
-        let mut write_idx   = (x*4) + (y*self.width*4);
-        let mut read_idx    = 0;
-
-        for _ in 0..height {
-            // Write a row
-            for _ in 0..read_bytes {
-                self.pixels[write_idx] = bytes[read_idx];
-                write_idx   += 1;
-                read_idx    += 1;
-            }
-
-            // Skip to the next row
-            read_idx    += read_skip_bytes;
-            write_idx   += write_skip_bytes;
-        }
-    }
-}
-
 impl<TPixel, const N: usize> CanvasDrawing<TPixel, N>
 where
     TPixel: 'static + Send + Sync + Pixel<N>,
@@ -103,7 +56,7 @@ where
 
         // Build the texture structure
         let pixels  = vec![0u8; width * height * 4];
-        let texture = RgbaTexture { width, height, pixels };
+        let texture = RgbaTexture::new(width, height, pixels);
         let texture = Texture::Rgba(texture);
 
         // Store it, replacing any existing texture with this ID
--- render_software/src/pixel/rgba_texture.rs
+++ render_software/src/pixel/rgba_texture.rs
@@ -6,11 +6,125 @@
 #[derive(Clone)]
 pub struct RgbaTexture {
     /// The width of the texture in pixels (a row is 4x this value)
-    pub width: usize,
+    width: i64,
 
     /// The height of the texture in pixels
-    pub height: usize,
+    height: i64,
 
     /// The pixels stored for this texture
-    pub pixels: Vec<u8>,
+    pixels: Vec<u8>,
+}
+
+impl RgbaTexture {
+    ///
+    /// Creates a new RGBA texture
+    ///
+    pub fn new(width: usize, height: usize, pixels: Vec<u8>) -> RgbaTexture {
+        // SAFETY: we later rely on this to use get_unchecked
+        assert!(width * height * 4 == pixels.len());
+
+        RgbaTexture { 
+            width:  width as i64, 
+            height: height as i64, 
+            pixels: pixels
+        }
+    }
+
+    ///
+    /// The width of this texture in pixels
+    ///
+    #[inline]
+    pub fn width(&self) -> usize {
+        self.width as usize
+    }
+
+    ///
+    /// The height of this texture in pixels
+    ///
+    #[inline]
+    pub fn height(&self) -> usize {
+        self.height as usize
+    }
+
+    ///
+    /// The pixels for this texture
+    ///
+    #[inline]
+    pub fn pixels(&self) -> &Vec<u8> {
+        &self.pixels
+    }
+
+    ///
+    /// The pixels for this texture
+    ///
+    #[inline]
+    pub fn pixels_mut(&mut self) -> &mut Vec<u8> {
+        &mut self.pixels
+    }
+
+    ///
+    /// Sets the bytes for a region of this image
+    ///
+    #[inline]
+    pub fn set_bytes(&mut self, x: usize, y: usize, width: usize, height: usize, bytes: &Vec<u8>) {
+        // Number of bytes to read/write at a time
+        let mut read_bytes = width * 4;
+
+        // Number of bytes to skip after every read_bytes
+        let mut read_skip_bytes = 0;
+
+        // Clip to the size of the image
+        let x       = x.min(self.width as _);
+        let y       = y.min(self.height as _);
+        let width   = if x + width > self.width as _ {
+            let clip        = (x + width) - self.width as usize;
+            read_bytes      = (width - clip) * 4;
+            read_skip_bytes = clip * 4;
+            width - clip
+        } else {
+            width 
+        };
+        let height  = height.min(self.height as usize - y);
+
+        // After writing read_bytes, skip this many bytes to write
+        let write_skip_bytes = (self.width as usize - width) * 4;
+
+        // Pointers for reading/writing
+        let mut write_idx   = (x*4) + (y*(self.width as usize)*4);
+        let mut read_idx    = 0;
+
+        for _ in 0..height {
+            // Write a row
+            for _ in 0..read_bytes {
+                self.pixels[write_idx] = bytes[read_idx];
+                write_idx   += 1;
+                read_idx    += 1;
+            }
+
+            // Skip to the next row
+            read_idx    += read_skip_bytes;
+            write_idx   += write_skip_bytes;
+        }
+    }
+
+    ///
+    /// Finds a pixel at the specified coordinate in this texture
+    ///
+    #[inline]
+    pub fn read_pixel(&self, x: i64, y: i64) -> [u8; 4] {
+        // The texture is treated as repeating infinitely
+        let x   = if x >= 0 { x%self.width } else { (x%self.width) + self.width };
+        let y   = if y >= 0 { y%self.height } else { (y%self.height) + self.height };
+
+        // Calculate the index where this pixel is
+        let idx     = (x + y*self.width) * 4;
+        let idx     = idx as usize;
+        let pixels  = &self.pixels;
+
+        // Because of the assertion in new() we know that 'idx' must be in the range covered by this texture
+        debug_assert!(idx + 4 <= pixels.len());
+        unsafe {
+            [*pixels.get_unchecked(idx), *pixels.get_unchecked(idx+1), *pixels.get_unchecked(idx+2), *pixels.get_unchecked(idx+3)]
+        }
+    }
 }
