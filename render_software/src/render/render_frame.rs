d7d38a50520c3b2bf7e193c90cac1c724f49e519
Andrew Hunter <andrew@logicalshift.co.uk>
2023-10-22 10:39:46 +0100

    Add a 'render_drawing' function to do the simplest possible render of a drawing to a target

--- render_software/src/render/render_frame.rs
+++ render_software/src/render/render_frame.rs
@@ -1,8 +1,12 @@
+use crate::draw::*;
 use crate::pixel::*;
 use crate::scanplan::*;
 
 use super::render_source_trait::*;
 use super::render_target_trait::*;
+use super::scanline_renderer::*;
+
+use flo_canvas as canvas;
 
 ///
 /// Renders and entire frame to a render target from a render source
@@ -17,4 +21,19 @@ where
 {
     let region_renderer = TSource::create_region_renderer(scan_planner, program_runner);
     target.render(region_renderer, source);
+}
+
+///
+/// Renders a set of drawing instructions to a target using the default settings
+///
+pub fn render_drawing<TTarget>(target: &mut TTarget, drawing: impl IntoIterator<Item=canvas::Draw>) 
+where
+    TTarget: RenderTarget<F32LinearPixel>,
+{
+    // Prepare a canvas drawing
+    let mut canvas_drawing = CanvasDrawing::<F32LinearPixel, 4>::empty();
+    canvas_drawing.draw(drawing);
+
+    let renderer = CanvasDrawingRegionRenderer::new(PixelScanPlanner::default(), ScanlineRenderer::new(canvas_drawing.program_runner()), target.height());
+    target.render(renderer, &canvas_drawing);
 }
