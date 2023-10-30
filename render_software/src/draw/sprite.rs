653761e3ded03b5ff5440983632c443ff33a02ba
Andrew Hunter <andrew@logicalshift.co.uk>
2023-10-30 20:07:04 +0000

    Finish up the transformations for the sprite matrix
    ... this really needs some tests to make sure it's working OK, but for now we just want to get to where we're drawing a sprite

--- render_software/src/draw/sprite.rs
+++ render_software/src/draw/sprite.rs
@@ -6,6 +6,21 @@ use flo_canvas as canvas;
 
 use crate::pixel::*;
 
+impl SpriteTransform {
+    ///
+    /// Returns this transform as a transformation matrix indicating how the points should be transformed
+    ///
+    #[inline]
+    pub fn matrix(&self) -> canvas::Transform2D {
+        match self {
+            SpriteTransform::ScaleTransform { scale, translate } =>
+                canvas::Transform2D::scale(scale.0 as _, scale.1 as _) * canvas::Transform2D::translate(translate.0 as _, translate.0 as _),
+
+            SpriteTransform::Matrix(matrix) => *matrix
+        }
+    }
+}
+
 impl<TPixel, const N: usize> CanvasDrawing<TPixel, N>
 where
     TPixel: 'static + Send + Sync + Pixel<N>,
@@ -68,13 +83,11 @@ impl DrawingState {
             (Translate(x, y), SpriteTransform::ScaleTransform { translate, scale }) => { translate.0 -= x as f64 * scale.0; translate.1 -= y as f64 * scale.0; }
             (Scale(x, y), SpriteTransform::ScaleTransform { scale, .. })            => { scale.0 /= x as f64; scale.1 /= y as f64; }
 
-            (Rotate(theta), SpriteTransform::ScaleTransform { .. })                 => { }
-            (Transform2D(matrix), SpriteTransform::ScaleTransform { .. })           => { }
+            (Rotate(theta), sprite_transform)                                       => { *sprite_transform = SpriteTransform::Matrix(sprite_transform.matrix() * canvas::Transform2D::rotate_degrees(theta)); }
+            (Transform2D(matrix), sprite_transform)                                 => { *sprite_transform = SpriteTransform::Matrix(sprite_transform.matrix() * matrix); }
         
-            (Translate(x, y), SpriteTransform::Matrix(t))                           => { }
-            (Scale(x, y), SpriteTransform::Matrix(t))                               => { }
-            (Rotate(theta), SpriteTransform::Matrix(t))                             => { }
-            (Transform2D(matrix), SpriteTransform::Matrix(t))                       => { }
+            (Translate(x, y), SpriteTransform::Matrix(t))                           => { *t = *t * canvas::Transform2D::translate(x, y); }
+            (Scale(x, y), SpriteTransform::Matrix(t))                               => { *t = *t * canvas::Transform2D::scale(x, y); }
         }
     }
 }
