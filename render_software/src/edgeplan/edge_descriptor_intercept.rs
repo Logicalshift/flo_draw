dc2bad1ebf39fb2b5aff10635ce425ceec2899e1
Andrew Hunter <andrew@logicalshift.co.uk>
2023-11-22 11:53:36 +0000

    Add a structure to provide an edge intercept position
    This is useful for shard intercepts in fairly specific circumstances but might not be useful otherwise, so I'm not sure if we want an extra 'intercepts' implementation that generates it only when needed or to always generate it.

--- render_software/src/edgeplan/edge_descriptor_intercept.rs
+++ render_software/src/edgeplan/edge_descriptor_intercept.rs
@@ -1,6 +1,15 @@
 use super::edge_intercept_direction::*;
 
 ///
+/// Describes a position within an edge descriptor
+///
+/// These are ordered, and have two parts: the 'edge ID' for where there are multiple edges and the edge position which can 
+/// distinguish multiple intercepts along the same edge (it's usually the 't' value for the intercept)
+///
+#[derive(Clone, Copy, PartialEq, PartialOrd)]
+pub struct EdgePosition(pub usize, pub f64);
+
+///
 /// Describes an intercept from an edge descriptor
 ///
 #[derive(Clone, Copy, PartialEq, Debug)]
