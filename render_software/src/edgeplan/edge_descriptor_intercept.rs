5e6c37bc846e7858fe866255b5effbdf1ec7bc23
Andrew Hunter <andrew@logicalshift.co.uk>
2023-11-23 21:20:51 +0000

    Add ordering for the EdgePosition

--- render_software/src/edgeplan/edge_descriptor_intercept.rs
+++ render_software/src/edgeplan/edge_descriptor_intercept.rs
@@ -1,12 +1,14 @@
 use super::edge_intercept_direction::*;
 
+use std::cmp::{Ordering};
+
 ///
 /// Describes a position within an edge descriptor
 ///
 /// These are ordered, and have two parts: the 'edge ID' for where there are multiple edges and the edge position which can 
 /// distinguish multiple intercepts along the same edge (it's usually the 't' value for the intercept)
 ///
-#[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct EdgePosition(pub usize, pub f64);
 
 ///
@@ -28,4 +30,28 @@ pub struct EdgeDescriptorIntercept {
     /// The position of this intercept
     ///
     pub position: EdgePosition,
+}
+
+impl PartialOrd for EdgePosition {
+    #[inline]
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Eq for EdgePosition {
+
+}
+
+impl Ord for EdgePosition {
+    #[inline]
+    fn cmp(&self, other: &Self) -> Ordering {
+        if self.0 < other.0 {
+            Ordering::Less
+        } else if self.0 > other.0 {
+            Ordering::Greater
+        } else {
+            self.1.total_cmp(&other.1)
+        }
+    }
 }
