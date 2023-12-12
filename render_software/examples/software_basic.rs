4181581fb6f3a63c3c7f5ad0301171293960bd3d
Andrew Hunter <andrew@logicalshift.co.uk>
2023-12-12 09:17:46 +0000

    Add a timer to the 'basic' drawing render
    (This is useful for comparison to the more complicated renderings)

--- render_software/examples/software_basic.rs
+++ render_software/examples/software_basic.rs
@@ -5,6 +5,8 @@ use flo_render_software::scanplan::*;
 
 use flo_render_software::canvas::*;
 
+use std::time::{Instant};
+
 ///
 /// Draws a triangle to the terminal
 ///
@@ -26,6 +28,24 @@ pub fn main() {
     // Create a canvas from the drawing
     let mut canvas_drawing = CanvasDrawing::<F32LinearPixel, 4>::empty();
     canvas_drawing.draw(drawing);
+
+    // Time how long it takes to draw the mascot to the canvas (full frames will often involve both of these steps)
+    let mut frame   = vec![0u8; 1920*1080*4];
+    let mut rgba    = RgbaFrame::from_bytes(1920, 1080, 2.2, &mut frame).unwrap();
+
+    for _ in 0..10 {
+        let renderer = CanvasDrawingRegionRenderer::new(ShardScanPlanner::default(), ScanlineRenderer::new(canvas_drawing.program_runner(1080.0)), 1080);
+        rgba.render(renderer, &canvas_drawing);
+    }
+
+    let render_start = Instant::now();
+    for _ in 0..100 {
+        let renderer = CanvasDrawingRegionRenderer::new(ShardScanPlanner::default(), ScanlineRenderer::new(canvas_drawing.program_runner(1080.0)), 1080);
+        rgba.render(renderer, &canvas_drawing);
+    }
+    let render_time = Instant::now().duration_since(render_start);
+    let avg_micros  = render_time.as_micros() / 100;
+    println!("F32 frame render time: {}.{}ms", avg_micros/1000, avg_micros%1000);
 
     // Render the drawing to the terminal
     let mut term_renderer = TerminalRenderTarget::new(1920, 1080);
