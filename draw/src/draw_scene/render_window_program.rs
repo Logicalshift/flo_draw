a04563040a3c98bc8b5532195ee6e2fdb144b861
Andrew Hunter <andrew@logicalshift.co.uk>
2024-01-06 21:17:36 +0000

    In the new scene design, these are called subprograms rather than entities

---
+++ draw/src/draw_scene/render_window_program.rs
@@ -0,0 +1,34 @@
+#[cfg(all(feature="render-opengl", not(feature="render-wgpu")))]
+use super::glutin_render_window_entity::*;
+
+#[cfg(feature="render-wgpu")]
+use super::wgpu_render_window_entity::*;
+
+use flo_scene::*;
+use flo_canvas_events::*;
+
+use std::sync::*;
+
+///
+/// Creates a render window in a scene with the specified program ID
+///
+#[cfg(all(feature="render-opengl", not(feature="render-wgpu")))]
+pub fn create_render_window_sub_program(scene: &Arc<Scene>, program_id: SubProgramId, initial_size: (u64, u64)) -> Result<SimpleEntityChannel<RenderWindowRequest>, CreateEntityError> {
+    create_glutin_render_window_program(scene, program_id, initial_size)
+}
+
+///
+/// Retrieves or creates a scene context for flo_draw
+///
+#[cfg(all(feature="render-wgpu"))]
+pub fn create_render_window_sub_program(scene: &Arc<Scene>, program_id: SubProgramId, initial_size: (u64, u64)) -> Result<SimpleEntityChannel<RenderWindowRequest>, CreateEntityError> {
+    create_wgpu_render_window_program(context, program_id, initial_size)
+}
+
+///
+/// Retrieves or creates a scene context for flo_draw
+///
+#[cfg(all(not(feature="render-wgpu"), not(feature="render-opengl")))]
+pub fn create_render_window_sub_program(context: &Arc<SceneContext>, entity_id: EntityId, initial_size: (u64, u64)) -> Result<SimpleEntityChannel<RenderWindowRequest>, CreateEntityError> {
+    panic!("No default renderer was specified when flo_draw was compiled (use `render-wgpu` or `render-opengl`)")
+}
