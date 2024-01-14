3af764145ac0d63d85dda9a5a2135ecbf110a2a6
Andrew Hunter <andrew@logicalshift.co.uk>
2024-01-14 12:48:07 +0000

    When creating a render window, do so outside of the future
    This stops the window from closing immediately if the future is not polled before the 'stop when windows closed' event is fired.

--- draw/src/draw_scene/glutin_render_window_program.rs
+++ draw/src/draw_scene/glutin_render_window_program.rs
@@ -25,7 +25,7 @@ pub fn create_glutin_render_window_program(scene: &Arc<Scene>, program_id: SubPr
     // Create the window in the scene
     scene.add_subprogram(
         program_id,
-        move |render_window_requests, context| async move {
+        move |render_window_requests, context| {
             // Create the publisher to send the render actions to the stream
             let title               = bind("flo_draw".to_string());
             let fullscreen          = bind(false);
@@ -45,70 +45,73 @@ pub fn create_glutin_render_window_program(scene: &Arc<Scene>, program_id: SubPr
             // Create a stream for publishing render requests
             let (render_sender, render_receiver) = mpsc::channel(5);
 
-            // Create a window that subscribes to the publisher
+            // Create a window that subscribes to the publisher (we do this outside of the main 'async' loop so this has happened on return)
+            // If the window is not created immediately, there may be a race condition if `StopWhenAllWindowsClosed` is sent 
             let glutin_thread   = glutin_thread();
             glutin_thread.send_event(GlutinThreadEvent::CreateRenderWindow(render_receiver.boxed(), event_publisher.republish(), window_properties.into()));
 
-            // Run the main event loop
-            let mut render_window_requests  = render_window_requests;
-            let mut render_sender           = render_sender;
+            async move {
+                // Run the main event loop
+                let mut render_window_requests  = render_window_requests;
+                let mut render_sender           = render_sender;
 
-            while let Some(request) = render_window_requests.next().await {
-                let request: RenderWindowRequest = request;
+                while let Some(request) = render_window_requests.next().await {
+                    let request: RenderWindowRequest = request;
 
-                match request {
-                    RenderWindowRequest::Render(RenderRequest::Render(render)) => {
-                        // Just pass render requests on to the render window
-                        if render_sender.send(render).await.is_err() {
-                            // This entity is finished if the window finishes
-                            break;
+                    match request {
+                        RenderWindowRequest::Render(RenderRequest::Render(render)) => {
+                            // Just pass render requests on to the render window
+                            if render_sender.send(render).await.is_err() {
+                                // This entity is finished if the window finishes
+                                break;
+                            }
                         }
-                    }
 
-                    RenderWindowRequest::SendEvents(channel_target) => {
-                        // Run a subprogram to send the events to the target program
-                        let subscriber = Mutex::new(Some(event_publisher.subscribe()));
+                        RenderWindowRequest::SendEvents(channel_target) => {
+                            // Run a subprogram to send the events to the target program
+                            let subscriber = Mutex::new(Some(event_publisher.subscribe()));
 
-                        context.send_message(SceneControl::start_program(SubProgramId::new(), move |_: InputStream<()>, context| {
-                            let subscriber = subscriber.lock().unwrap().take();
+                            context.send_message(SceneControl::start_program(SubProgramId::new(), move |_: InputStream<()>, context| {
+                                let subscriber = subscriber.lock().unwrap().take();
 
-                            async move {
-                                if let Some(mut subscriber) = subscriber {
-                                    let events_target = context.send(channel_target).ok();
+                                async move {
+                                    if let Some(mut subscriber) = subscriber {
+                                        let events_target = context.send(channel_target).ok();
 
-                                    if let Some(mut events_target) = events_target {
-                                        // Pass on events to everything that's listening, until the channel starts generating errors
-                                        while let Some(event) = subscriber.next().await {
-                                            let result = events_target.send(event).await;
+                                        if let Some(mut events_target) = events_target {
+                                            // Pass on events to everything that's listening, until the channel starts generating errors
+                                            while let Some(event) = subscriber.next().await {
+                                                let result = events_target.send(event).await;
 
-                                            if result.is_err() {
-                                                break;
+                                                if result.is_err() {
+                                                    break;
+                                                }
                                             }
                                         }
                                     }
                                 }
-                            }
-                        }, 20)).await.ok();
+                            }, 20)).await.ok();
+                        }
+
+                        RenderWindowRequest::CloseWindow => {
+                            // The window will close its publisher in response to the events stream being closed
+                            render_sender.close().await.ok();
+
+                            // Shut down the event publisher
+                            use std::mem;
+                            let when_closed = event_publisher.when_closed();
+                            mem::drop(event_publisher);
+
+                            // Finally, wait for the publisher to finish up
+                            when_closed.await;
+                            return;
+                        }
+
+                        RenderWindowRequest::SetTitle(new_title)                => { title.set(new_title); },
+                        RenderWindowRequest::SetFullScreen(new_fullscreen)      => { fullscreen.set(new_fullscreen); },
+                        RenderWindowRequest::SetHasDecorations(new_decorations) => { has_decorations.set(new_decorations); },
+                        RenderWindowRequest::SetMousePointer(new_mouse_pointer) => { mouse_pointer.set(new_mouse_pointer); },
                     }
-
-                    RenderWindowRequest::CloseWindow => {
-                        // The window will close its publisher in response to the events stream being closed
-                        render_sender.close().await.ok();
-
-                        // Shut down the event publisher
-                        use std::mem;
-                        let when_closed = event_publisher.when_closed();
-                        mem::drop(event_publisher);
-
-                        // Finally, wait for the publisher to finish up
-                        when_closed.await;
-                        return;
-                    }
-
-                    RenderWindowRequest::SetTitle(new_title)                => { title.set(new_title); },
-                    RenderWindowRequest::SetFullScreen(new_fullscreen)      => { fullscreen.set(new_fullscreen); },
-                    RenderWindowRequest::SetHasDecorations(new_decorations) => { has_decorations.set(new_decorations); },
-                    RenderWindowRequest::SetMousePointer(new_mouse_pointer) => { mouse_pointer.set(new_mouse_pointer); },
                 }
             }
         },
