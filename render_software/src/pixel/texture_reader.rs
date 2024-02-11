3e846387f1b8a9cf96402da7831f5e984cd4a065
Andrew Hunter <andrew@logicalshift.co.uk>
2024-02-11 16:10:38 +0000

    Want to be able to tell a texture to read a sequence of pixels instead of one pixel at a time
    (This makes it easier to do things like have multiple passes to process pixels)

--- render_software/src/pixel/texture_reader.rs
+++ render_software/src/pixel/texture_reader.rs
@@ -1,7 +1,7 @@
 ///
 /// Trait implemented by types that can be read from a texture type
 ///
-pub trait TextureReader<TTexture> : Send + Sync 
+pub trait TextureReader<TTexture> : Send + Sync + Sized
 where
     TTexture: Send + Sync,
 {
@@ -9,4 +9,14 @@ where
     ///
     /// The coordinates are fractions of pixels
     fn read_pixel(texture: &TTexture, x: f64, y: f64) -> Self;
+
+    ///
+    /// Reads a sequence of pixels from this texture into a target array
+    ///
+    #[inline]
+    fn read_pixels(texture: &TTexture, pixels: &mut [Self], positions: &[(f64, f64)]) {
+        for ((x, y), pixel) in positions.iter().zip(pixels.iter_mut()) {
+            *pixel = Self::read_pixel(texture, *x, *y);
+        }
+    }
 }
