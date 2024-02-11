22271301ba16e17e82c02501c43e7aab265a0ca5
Andrew Hunter <andrew@logicalshift.co.uk>
2024-02-11 21:50:31 +0000

    It makes sense to push the 'linear' pixel reader downwards (as we can potentially precalculate a bunch of stuff this way), so add a `read_pixels_linear()` function to TextureReader

--- render_software/src/pixel/texture_reader.rs
+++ render_software/src/pixel/texture_reader.rs
@@ -11,4 +11,31 @@ where
     /// Coordinates are in fractions of pixels to allow for a texture reader to support bilinear interpolation or mipmapping
     ///
     fn read_pixels(texture: &TTexture, positions: &[(f64, f64)]) -> Vec<Self>;
+
+    ///
+    /// Reads a set of pixels across a linear gradient
+    ///
+    /// This is a common way that pixels are read out from a texture, so this function can be overridden to optimise this
+    /// for different types of texture storage if needed.
+    ///
+    /// This reads `count` pixels at locations `t = 0, 1, 2, ...` such that `u = dx * t` and `x = x_gradient.0 * u + x_gradient.1`,
+    /// `y = y_gradient.0 * u + y_gradient.1`.
+    ///
+    #[inline]
+    fn read_pixels_linear(texture: &TTexture, dx: f64, x_gradient: (f64, f64), y_gradient: (f64, f64), count: usize) -> Vec<Self> {
+        // Allocate enough space to store the pixels
+        let mut positions = Vec::with_capacity(count);
+
+        // Calculate the positions for the pixels
+        positions.extend((0..count).map(|t| {
+            let t = t as f64;
+            let u = dx * t;
+            let x = x_gradient.0 * u + x_gradient.1;
+            let y = y_gradient.0 * u + y_gradient.1;
+
+            (x, y)
+        }));
+
+        Self::read_pixels(texture, &positions)
+    }
 }
