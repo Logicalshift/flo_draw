df7c7a83d23e83b699286d38343c77864b31f1ef
Andrew Hunter <andrew@logicalshift.co.uk>
2024-02-20 09:48:09 +0000

    Add an implementation of a basic mip-map texture program
    This isn't really doing a good job with the scale of the mip-map

--- render_software/src/pixel_programs/mip_map_texture.rs
+++ render_software/src/pixel_programs/mip_map_texture.rs
@@ -23,9 +23,31 @@ where
     TTextureReader: Copy + Pixel<N> + TextureReader<TTexture>,
 {
     type Program    = BilinearTextureProgram<TTextureReader, TTexture, N>;
-    type FrameData  = TextureData<MipMap<TTexture>>;
+    type FrameData  = TextureData<MipMap<Arc<TTexture>>>;
 
-    fn program_for_frame(&self, pixel_size: PixelSize, program_data: &Arc<TextureData<MipMap<TTexture>>>) -> (Self::Program, TextureData<TTexture>) {
-        todo!()
+    fn program_for_frame(&self, pixel_size: PixelSize, program_data: &Arc<TextureData<MipMap<Arc<TTexture>>>>) -> (Self::Program, TextureData<TTexture>) {
+        // Read the transform from the program_data
+        let mipmap                  = &*program_data.texture;
+        let [[a, b, c], [d, e, f]]  = program_data.transform;
+
+        // Calculate the transform parameters for the texture (we want to know how far we advance in the texture for every x position)
+        let dx = a * pixel_size.0;
+        let dy = d * pixel_size.0;
+
+        // Fetch the mip level that corresponds to this level
+        let mip_level = mipmap.level_for_pixel_step(dx, dy);
+
+        // Figure out the scaling for the mipmap
+        // TODO: use the actual texture size here
+        let scale = 1.0 / (2.0f64.powi(mip_level as _));
+
+        // Create texture data for reading from this mip-map level
+        let mipmap_texture = TextureData {
+            texture:    Arc::clone(mipmap.mip_level(mip_level)),
+            transform:  [[a*scale, b*scale, c*scale], [d*scale, e*scale, f*scale]]
+        };
+
+        // Result is a bilinear filter and the texture
+        (BilinearTextureProgram::default(), mipmap_texture)
     }
 }
