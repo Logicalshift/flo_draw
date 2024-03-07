67d13d8c1ce43c2fbe68ff2109e909958b284a50
Andrew Hunter <andrew@logicalshift.co.uk>
2024-03-07 09:57:23 +0000

    Add an 'extra_columns()' function to the filter so if we're running a filter as part of a pixel program we know how many extra pixels to generate at each point

--- render_software/src/filters/pixel_filter_trait.rs
+++ render_software/src/filters/pixel_filter_trait.rs
@@ -1,17 +1,28 @@
 ///
-/// A pixel filter implements a filter algorithm that can be applied to the result of 
+/// A pixel filter implements a filter algorithm that can be applied to pixels one line at a time
+///
+/// The design is such that it's not necessary to have the entire set of input pixels in one place for most filter types.
 ///
 pub trait PixelFilter {
     /// The type of the pixel that the filter accepts
     type Pixel : Send;
 
     ///
-    /// Retrieves the number of extra lines that are required to produce a single output line
+    /// Retrieves the number of extra lines that are required to produce a single output line (above and below)
     ///
     /// The result here is the number of lines above the current line and the number of lines below the current line that are required.
     /// The 'current' line is always supplied to the filter.
     ///
     fn input_lines(&self) -> (usize, usize);
+
+    ///
+    /// Retrieves the number of extra columns that are needed as input (left and right)
+    ///
+    /// The sum of these values will be added to the length of each input line. These extra pixels are useful when partially applying
+    /// a filter (eg, when rendering via a pixel program), as it provides a way to generate the extra pixels needed to fully apply
+    /// something like a gaussian filter.
+    ///
+    fn extra_columns(&self) -> (usize, usize);
 
     ///
     /// Filters a single line of pixels from an input set of pixels. For lines outside of the input range, the pixels are always returned as
