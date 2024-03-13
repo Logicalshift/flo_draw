c1cc2a66f2ec51a21066cbbf41dd73a98dfa029d
Andrew Hunter <andrew@logicalshift.co.uk>
2024-03-13 09:15:13 +0000

    Add files for each of the filters we need to implement

--- render_software/src/filters/mod.rs
+++ render_software/src/filters/mod.rs
@@ -1,5 +1,13 @@
 mod pixel_filter_trait;
 mod texture_filter;
+mod alpha_blend_filter;
+mod displacement_map_filter;
+mod gaussian_blur_filter;
+mod mask_filter;
 
 pub use pixel_filter_trait::*;
 pub use texture_filter::*;
+pub use alpha_blend_filter::*;
+pub use displacement_map_filter::*;
+pub use gaussian_blur_filter::*;
+pub use mask_filter::*;
