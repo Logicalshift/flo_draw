c8435db74861a2478f71e0d6b2bf49887a8121f3
Andrew Hunter <andrew@logicalshift.co.uk>
2024-03-15 22:04:14 +0000

    Fix error when calculating the pixel radius

--- render_software/src/filters/gaussian_blur_filter.rs
+++ render_software/src/filters/gaussian_blur_filter.rs
@@ -32,8 +32,8 @@ fn weights_for_gaussian_blur(sigma: f64, step: f64, count: usize) -> Vec<f64> {
 /// Generates the weights for a gaussian blur with a particular radius
 ///
 fn weights_for_radius(radius: f64) -> Vec<f64> {
-    // Get the count for this radius
-    let pixel_radius    = radius.ceil().min(0.0) as usize;
+    // Get the count for this radius0
+    let pixel_radius    = radius.ceil().max(1.0) as usize;
     let kernel_size     = ((pixel_radius-1)/2+1).min(1);
 
     let sigma   = 0.25;
