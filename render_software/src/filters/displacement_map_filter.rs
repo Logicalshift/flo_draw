4851ce35f22ca140e892b67ac93c362342e94a47
Andrew Hunter <andrew@logicalshift.co.uk>
2024-03-17 18:36:04 +0000

    Initial implementation of the whole displacement map filter

--- render_software/src/filters/displacement_map_filter.rs
+++ render_software/src/filters/displacement_map_filter.rs
@@ -43,15 +43,49 @@ where
     type Pixel = TPixel;
 
     fn input_lines(&self) -> (usize, usize) {
-        (self.offset_y.ceil() as usize, self.offset_y.ceil() as usize)
+        (self.offset_y.abs().ceil() as usize, self.offset_y.abs().ceil() as usize + 1)
     }
 
     fn extra_columns(&self) -> (usize, usize) {
-        (self.offset_x.ceil() as usize, self.offset_x.ceil() as usize)
+        (self.offset_x.abs().ceil() as usize, self.offset_x.abs().ceil() as usize + 1)
     }
 
     fn filter_line(&self, ypos: usize, input_lines: &[&[Self::Pixel]], output_line: &mut [Self::Pixel]) {
+        let mid_point_x = self.offset_x.abs().ceil();
+        let mid_point_y = self.offset_y.abs().ceil();
+
         // Read a line from the displacement map
-        todo!()
+        let line_pixels = self.displacement_map.pixel_line(ypos);
+        let num_extra   = (output_line.len() as isize - self.displacement_map.width() as isize).max(0);
+
+        if let Some(line_pixels) = line_pixels {
+            // Read from the input using the offsets from the displacement map
+            let line_pixels = U16LinearPixel::u16_slice_as_linear_pixels_immutable(line_pixels);
+
+            for (xpos, px) in line_pixels.iter().copied().chain((0..num_extra).map(|_| U16LinearPixel::from_components([32767, 32767, 32767, 32767]))).enumerate() {
+                // Read the x and y offsets from the texture
+                let x_off = ((px.r() as f64)/65535.0) * self.offset_x;
+                let y_off = ((px.g() as f64)/65535.0) * self.offset_y;
+
+                // The pixel we read is at a particular x, y position
+                let xpos = xpos + x_off as usize;
+                let ypos = y_off as usize;
+
+                // Read the 4 pixels for bilinear filtering
+                let pixels = [
+                    &input_lines[ypos][xpos],
+                    &input_lines[ypos][xpos+1],
+                    &input_lines[ypos+1][xpos],
+                    &input_lines[ypos+1][xpos+1],
+                ];
+
+                // Filter the result to generate the final pixel
+                output_line[xpos] = TPixel::filter_bilinear(pixels, TPixel::Component::with_value(x_off.fract()), TPixel::Component::with_value(y_off.fract()));
+            }
+        } else {
+            // Just copy the mid-point pixels to the output (outside of the displacement map)
+            let len = output_line.len();
+            output_line[0..len].copy_from_slice(&input_lines[mid_point_y as usize][(mid_point_x as usize)..(len + mid_point_x as usize)])
+        }
     }
 }
