128cb556780a57e434379cab1024f9c9025dd334
Andrew Hunter <andrew@logicalshift.co.uk>
2024-03-17 15:37:53 +0000

    Change the functions that apply filters so that they take iterators
    This makes it much easier to deal with applying multiple filters in a row though it is perhaps an issue with making this work in a multi-threaded way

--- render_software/src/filters/texture_filter.rs
+++ render_software/src/filters/texture_filter.rs
@@ -3,125 +3,181 @@ use super::pixel_filter_trait::*;
 
 use std::collections::*;
 use std::sync::*;
-use std::sync::atomic::{AtomicIsize, Ordering};
 
 ///
 /// Reads and filters the pixels from a texture
 ///
 /// The result here is an iterator of the generated rows, using the texture reader pixel format
 ///
-pub fn filter_texture<'a, TTexture, TTextureReader, const N: usize>(texture: &'a TTexture, filter: &'a impl PixelFilter<Pixel=TTextureReader>) -> impl 'a + Iterator<Item=(usize, Vec<TTextureReader>)>
+pub fn filter_texture<'a, TTexture, TPixel, const N: usize>(texture: &'a TTexture, filter: &'a impl PixelFilter<Pixel=TPixel>) -> impl 'a + Iterator<Item=Vec<TPixel>>
 where
-    TTextureReader: 'a + TextureReader<TTexture>,
-    TTextureReader: Pixel<N>,
-    TTexture:       Send + Sync,
+    TPixel:     'a + TextureReader<TTexture>,
+    TPixel:     Pixel<N>,
+    TTexture:   Send + Sync,
 {
     // Read about the texture
-    let (width, height) = TTextureReader::texture_size(texture);
+    let (width, height) = TPixel::texture_size(texture);
     let width_pixels    = width as usize;
     let height_pixels   = height as usize;
 
     // The read_row function reads a single row from the texture to be filtered
-    let read_row = move |row_num| {
-        TTextureReader::read_pixels_linear(texture, 0.0, 1.0, (1.0, 0.0), (0.0, row_num as f64), width_pixels)
-    };
+    let read_row = (0..height_pixels).map(move |row_num| {
+        TPixel::read_pixels_linear(texture, 0.0, 1.0, (1.0, 0.0), (0.0, row_num as f64), width_pixels)
+    });
 
-    apply_pixel_filter(width_pixels, read_row, height_pixels, filter)
+    apply_pixel_filter(width_pixels, read_row, filter)
+}
+
+///
+/// Iterator that returns a 'window' of pixels that 'rolls' down a source set of pixels
+///
+struct RollingPixelCache<TPixel, TIterator> 
+where
+    TPixel:     Default,
+    TIterator:  Iterator<Item=Vec<TPixel>>
+{
+    /// The number of pixels to define in a line
+    width: usize,
+
+    /// The number of blank pixels to add to the start of each line
+    padding_left: usize,
+
+    /// The number of blank pixels to add to the end of each line
+    padding_right: usize,
+
+    /// The pixels in the current cache (initially filled with blank lines)
+    cache: VecDeque<Arc<Vec<TPixel>>>,
+
+    /// None when finished, or the iterator for the pixel lines
+    iterator: Option<TIterator>,
+
+    /// The number of blank lines remaining after the iterator has completed
+    remaining_lines: usize,
+}
+
+impl<TPixel, TIterator> RollingPixelCache<TPixel, TIterator> 
+where
+    TPixel:     Default + Clone,
+    TIterator:  Iterator<Item=Vec<TPixel>>,
+{
+    ///
+    /// Creates a rolling pixel cache from an iterator
+    ///
+    pub fn from_iterator(iterator: TIterator, width: usize, add_above: usize, add_below: usize, add_left: usize, add_right: usize) -> Self {
+        // Start by creating blank lines in the initial cache (up to add_above, plus one extra to be overwritten by the first iteration)
+        let mut initial_cache = (0..=add_above).map(|_| Arc::new(vec![TPixel::default(); width+add_left+add_right])).collect::<VecDeque<_>>();
+
+        // Read extra lines to put 'underneath'
+        let mut iterator    = Some(iterator);
+        let mut remaining   = add_below;
+
+        for _ in 0..add_below {
+            if let Some(mut pixels) = iterator.as_mut().and_then(|iterator| iterator.next()) {
+                if add_left > 0 {
+                    pixels.splice(0..0, (0..add_left).map(|_| TPixel::default()));
+                }
+
+                if add_right > 0 {
+                    pixels.extend((0..add_right).map(|_| TPixel::default()));
+                }
+
+                initial_cache.push_back(Arc::new(pixels));
+            } else {
+                // Update the remaining count as we've run off the end of the iterator
+                iterator    = None;
+                remaining   = if remaining > 0 { remaining - 1 } else { 0 };
+
+                // Add default pixels
+                initial_cache.push_back(Arc::new(vec![TPixel::default(); width+add_left+add_right]));
+            }
+        }
+
+        // Fill up the final cache
+        RollingPixelCache { 
+            width:              width, 
+            cache:              initial_cache,
+            iterator:           iterator, 
+            remaining_lines:    remaining,
+            padding_left:       add_left,
+            padding_right:      add_right,
+        }
+    }
+}
+
+impl<TPixel, TIterator> Iterator for RollingPixelCache<TPixel, TIterator> 
+where
+    TPixel:     Default + Clone,
+    TIterator:  Iterator<Item=Vec<TPixel>>,
+{
+    type Item = Box<[Arc<Vec<TPixel>>]>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // Read the next line of pixels
+        let pixels = if let Some(iterator) = self.iterator.as_mut() {
+            if let Some(mut pixels) = iterator.next() {
+                if self.padding_left > 0 {
+                    pixels.splice(0..0, (0..self.padding_left).map(|_| TPixel::default()));
+                }
+
+                if self.padding_right > 0 {
+                    pixels.extend((0..self.padding_right).map(|_| TPixel::default()));
+                }
+
+                pixels
+            } else {
+                // Done with the iterator
+                self.iterator = None;
+
+                if self.remaining_lines > 0 {
+                    // Iterator has run out, so return a blank line while there are remaining lines to consider
+                    self.remaining_lines -= 1;
+                    vec![TPixel::default(); self.width]
+                } else {
+                    // No more remaining lines
+                    return None;
+                }
+            }
+        } else {
+            if self.remaining_lines > 0 {
+                // Iterator has run out, so return a blank line while there are remaining lines to consider
+                self.remaining_lines -= 1;
+                vec![TPixel::default(); self.width+self.padding_left+self.padding_right]
+            } else {
+                // No more remaining lines
+                return None;
+            }
+        };
+
+        // Add to the cache
+        self.cache.pop_front();
+        self.cache.push_back(Arc::new(pixels));
+
+        // Generate the result
+        Some(self.cache.iter().cloned().collect())
+    }
 }
 
 ///
 /// Given a function that can read a row of pixels at a time, applies a texture filter and returns an iterator 
 ///
-/// The 'read' function can assume that the only rows that will be read will be between 0..num_rows and each row will be requested at most once. Additionally, it may block
-/// in order to ensure that the rows are read in order if needed.
-///
-pub fn apply_pixel_filter<'a, TPixel, const N: usize>(width: usize, read_row: impl 'a + Send + Sync + Fn(usize) -> Vec<TPixel>, num_rows: usize, filter: &'a impl PixelFilter<Pixel=TPixel>) -> impl 'a + Iterator<Item=(usize, Vec<TPixel>)>
+pub fn apply_pixel_filter<'a, TPixel, const N: usize>(width: usize, read_row: impl 'a + Send + Sync + Iterator<Item=Vec<TPixel>>, filter: &'a impl PixelFilter<Pixel=TPixel>) -> impl 'a + Iterator<Item=Vec<TPixel>>
 where
     TPixel: 'a + Pixel<N>,
 {
-    use std::mem;
-
     // We need to add extra clear pixels above and below the 
     let (add_above, add_below) = filter.input_lines();
     let (add_left, add_right)  = filter.extra_columns();
 
     // The pixel cache stores the lines we've read from the source that we're going to supply to the filter. If we're rendering extra lines, 
     // then we allow each line to be read a certain number of times
-    let pixel_cache = Arc::new(RwLock::new(HashMap::new()));
-
-    let num_rows  = num_rows as isize;
-    let add_above = add_above as isize;
-    let add_below = add_below as isize;
-
-    (0..num_rows)
-        .map(move |row_num| {
-            // Read the rows that the filter needs to operate
-            let mut pixel_cache_read    = pixel_cache.read().unwrap();
-            let mut pixel_rows          = Vec::with_capacity((add_above + add_below + 1) as usize);
-
-            for read_row_num in (row_num-add_above)..=(row_num+add_below) {
-                match pixel_cache_read.get(&read_row_num) {
-                    None => {
-                        // Read a row from the source
-                        let mut new_row = if read_row_num < 0 || read_row_num >= num_rows {
-                            vec![TPixel::default(); width]
-                        } else {
-                            read_row(read_row_num as usize)
-                        };
-
-                        // Add the 'add_left' and 'add_right' pixels (TODO: better to ask the read_row function to do this)
-                        if add_left > 0 {
-                            new_row.splice(0..0, (0..add_left).map(|_| TPixel::default()));
-                        }
-
-                        if add_right > 0 {
-                            new_row.extend((0..add_right).map(|_| TPixel::default()));
-                        }
-
-                        // Keep the row with a reference
-                        let new_row = Arc::new(new_row);
-
-                        if add_above > 0 || add_below > 0 {
-                            // Write the row to the pixel cache, along with a counter
-                            // TODO: counter needs to account for the first and last set of rows
-                            mem::drop(pixel_cache_read);
-                            let mut pixel_cache_write = pixel_cache.write().unwrap();
-
-                            pixel_cache_write.insert(read_row_num, (Arc::clone(&new_row), AtomicIsize::new(add_above + add_below)));
-                            mem::drop(pixel_cache_write);
-
-                            pixel_cache_read = pixel_cache.read().unwrap();
-                        }
-
-                        // Add to the list of rows that we're going to process
-                        pixel_rows.push(new_row);
-                    }
-
-                    Some((pixel_row, usage_count)) => {
-                        // Add the row to the result
-                        pixel_rows.push(Arc::clone(pixel_row));
-
-                        // Decrease the usage count
-                        if usage_count.fetch_sub(1, Ordering::Acquire) <= 1 {
-                            // Free the cached row as we've read it as many times as it's going to be used
-                            mem::drop(pixel_cache_read);
-                            let mut pixel_cache_write = pixel_cache.write().unwrap();
-
-                            pixel_cache_write.remove(&read_row_num);
-                            mem::drop(pixel_cache_write);
-
-                            pixel_cache_read = pixel_cache.read().unwrap();
-                        }
-                    }
-                }
-            }
-
+    RollingPixelCache::from_iterator(read_row, width, add_above, add_below, add_left, add_right)
+        .map(move |pixel_rows| {
             // Filter the pixels
             let input_lines     = pixel_rows.iter().map(|pixels| &***pixels).collect::<Box<[_]>>();
             let mut output_line = vec![TPixel::default(); width];
             filter.filter_line(&*input_lines, &mut output_line);
 
-            (row_num as usize, output_line)
+            output_line
         })
 }
 
--- render_software/tests/filter_tests.rs
+++ render_software/tests/filter_tests.rs
@@ -1,25 +1,17 @@
 use flo_render_software::pixel::*;
 use flo_render_software::filters::*;
 
-use std::collections::{HashSet};
-
 #[test]
 pub fn alpha_blend_filter() {
     // We can apply the filter to some pixels we generate on the fly
     let filter          = AlphaBlendFilter::<F32LinearPixel, 4>::with_alpha(0.5);
-    let alpha_blended   = apply_pixel_filter(256, |y_pos| {
+    let alpha_blended   = apply_pixel_filter(256, (0..256).map(|y_pos| {
         (0..256).map(|x_pos| F32LinearPixel::from_components([0.0, 0.0, (y_pos as f32)/256.0, (x_pos as f32)/256.0])).collect()
-    }, 256, &filter);
+    }), &filter);
 
-    // Check that we receive each line once and they contain the expected set of pixels
-    let mut visited_lines = HashSet::new();
-
-    for (y_pos, pixels) in alpha_blended {
-        assert!(y_pos < 256);
-
-        // Don't allow duplicate lines
-        assert!(!visited_lines.contains(&y_pos));
-        visited_lines.insert(y_pos);
+    let mut num_visited_lines = 0;
+    for (y_pos, pixels) in alpha_blended.enumerate() {
+        num_visited_lines += 1;
 
         // Check the pixels that are generated against their expected values
         for (x_pos, pixel) in pixels.into_iter().enumerate() {
@@ -27,16 +19,16 @@ pub fn alpha_blend_filter() {
         }
     }
 
-    assert!(visited_lines.len() == 256);
+    assert!(num_visited_lines == 256);
 }
 
 #[test]
 pub fn smoke_test_horizontal_blur() {
     // We can apply the filter to some pixels we generate on the fly
     let filter  = HorizontalKernelFilter::<F32LinearPixel, 4>::with_gaussian_blur_radius(20.0);
-    let blurred = apply_pixel_filter(256, |y_pos| {
+    let blurred = apply_pixel_filter(256, (0..256).map(|y_pos| {
         (0..256).map(|x_pos| F32LinearPixel::from_components([0.0, 0.0, (y_pos as f32)/256.0, (x_pos as f32)/256.0])).collect()
-    }, 256, &filter);
+    }), &filter);
 
     let _ = blurred.collect::<Vec<_>>();
 }
@@ -45,9 +37,9 @@ pub fn smoke_test_horizontal_blur() {
 pub fn smoke_test_vertical_blur() {
     // We can apply the filter to some pixels we generate on the fly
     let filter  = VerticalKernelFilter::<F32LinearPixel, 4>::with_gaussian_blur_radius(20.0);
-    let blurred = apply_pixel_filter(256, |y_pos| {
+    let blurred = apply_pixel_filter(256, (0..256).map(|y_pos| {
         (0..256).map(|x_pos| F32LinearPixel::from_components([0.0, 0.0, (y_pos as f32)/256.0, (x_pos as f32)/256.0])).collect()
-    }, 256, &filter);
+    }), &filter);
 
     let _ = blurred.collect::<Vec<_>>();
 }
@@ -56,11 +48,11 @@ pub fn smoke_test_vertical_blur() {
 pub fn horizontal_blur_0() {
     // We can apply the filter to some pixels we generate on the fly
     let filter  = HorizontalKernelFilter::<F32LinearPixel, 4>::with_gaussian_blur_radius(0.0);
-    let blurred = apply_pixel_filter(256, |y_pos| {
+    let blurred = apply_pixel_filter(256, (0..256).map(|y_pos| {
         (0..256).map(|x_pos| F32LinearPixel::from_components([0.0, 0.0, (y_pos as f32)/256.0, (x_pos as f32)/256.0])).collect()
-    }, 256, &filter);
+    }), &filter);
 
-    for (y_pos, pixels) in blurred {
+    for (y_pos, pixels) in blurred.enumerate() {
         assert!(y_pos < 256);
 
         // Check the pixels that are generated against their expected values
@@ -75,11 +67,11 @@ pub fn horizontal_blur_0() {
 pub fn vertical_blur_0() {
     // We can apply the filter to some pixels we generate on the fly
     let filter  = VerticalKernelFilter::<F32LinearPixel, 4>::with_gaussian_blur_radius(0.0);
-    let blurred = apply_pixel_filter(256, |y_pos| {
+    let blurred = apply_pixel_filter(256, (0..256).map(|y_pos| {
         (0..256).map(|x_pos| F32LinearPixel::from_components([0.0, 0.0, (y_pos as f32)/256.0, (x_pos as f32)/256.0])).collect()
-    }, 256, &filter);
+    }), &filter);
 
-    for (y_pos, pixels) in blurred {
+    for (y_pos, pixels) in blurred.enumerate() {
         assert!(y_pos < 256);
 
         // Check the pixels that are generated against their expected values
