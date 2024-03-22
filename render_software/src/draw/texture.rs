48976a408aee6987dcc7475c7114c8d9c99e6d92
Andrew Hunter <andrew@logicalshift.co.uk>
2024-03-22 09:12:11 +0000

    Add in the 'create dynamic sprite' call

--- render_software/src/draw/texture.rs
+++ render_software/src/draw/texture.rs
@@ -100,7 +100,7 @@ where
             Free                                            => { self.texture_free(texture_id); },
             SetBytes(position, size, bytes)                 => { self.texture_set_bytes(texture_id, position, size, bytes); },
             SetFromSprite(sprite_id, bounds)                => { self.texture_set_from_sprite(texture_id, sprite_id, bounds); },
-            CreateDynamicSprite(sprite_id, bounds, size)    => { /* todo!() */ },
+            CreateDynamicSprite(sprite_id, bounds, size)    => { self.texture_create_dynamic_sprite(texture_id, sprite_id, bounds, size); },
             FillTransparency(alpha)                         => { self.texture_fill_transparency(texture_id, alpha as f64); },
             Copy(target_texture)                            => { self.texture_copy(texture_id, target_texture); },
             Filter(filter)                                  => { self.texture_filter(texture_id, filter); }
@@ -534,6 +534,16 @@ where
             // Store the new pixels
             texture.pixels = new_texture_pixels;
         }
+    }
+
+    ///
+    /// Creates a dynamic texture.
+    ///
+    /// These are rendered at the resolution of the output, and are re-rendered whenever the resolution or the sprite changes.
+    /// Filters are re-applied when re-rendering.
+    ///
+    pub fn texture_create_dynamic_sprite(&mut self, texture_id: canvas::TextureId, sprite_id: canvas::SpriteId, bounds: canvas::SpriteBounds, size: canvas::CanvasSize) {
+        // todo
     }
 }
 
