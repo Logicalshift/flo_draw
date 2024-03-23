0db54ff9b7d6d618f3493f7796d93a82c23baf05
Andrew Hunter <andrew@logicalshift.co.uk>
2024-03-23 22:22:10 +0000

    Add a filter as another test of the dynamic sprite rendering

--- render_software/examples/software_dynamic_sprite.rs
+++ render_software/examples/software_dynamic_sprite.rs
@@ -36,8 +36,10 @@ pub fn main() {
     // Draw the sprite using a dynamic texture
     let mut draw_sprite = vec![];
     draw_sprite.create_dynamic_texture(TextureId(0), SpriteId(0), 0.0, 0.0, 1000.0, 1000.0, 400.0, 400.0);
+    draw_sprite.create_dynamic_texture(TextureId(1), SpriteId(0), 0.0, 0.0, 1000.0, 1000.0, 400.0, 400.0);
+    draw_sprite.filter_texture(TextureId(1), TextureFilter::GaussianBlur(8.0));
 
-    draw_sprite.fill_texture(TextureId(0), 0.0, 100.0, 400.0, 500.0);
+    draw_sprite.fill_texture(TextureId(0), 0.0, 50.0, 400.0, 450.0);
     draw_sprite.new_path();
     draw_sprite.rect(0.0, 50.0, 400.0, 450.0);
     draw_sprite.fill();
@@ -45,6 +47,16 @@ pub fn main() {
     draw_sprite.fill_texture(TextureId(0), 600.0, 400.0, 1000.0, 800.0);
     draw_sprite.new_path();
     draw_sprite.rect(600.0, 400.0, 1000.0, 800.0);
+    draw_sprite.fill();
+
+    draw_sprite.fill_texture(TextureId(1), 600.0, 50.0, 1000.0, 450.0);
+    draw_sprite.new_path();
+    draw_sprite.rect(600.0, 50.0, 1000.0, 450.0);
+    draw_sprite.fill();
+
+    draw_sprite.fill_texture(TextureId(1), 0.0, 400.0, 400.0, 800.0);
+    draw_sprite.new_path();
+    draw_sprite.rect(0.0, 400.0, 400.0, 800.0);
     draw_sprite.fill();
 
     canvas_drawing.draw(draw_sprite.iter().cloned());
