fae8b286784bf10e2d508cb2747d4744af5a71ab
Andrew Hunter <andrew@logicalshift.co.uk>
2025-01-05 18:45:29 +0000

    Fix the display by only adding opacity (this kind of shows the problem with this approach)

--- render_software/src/scanplan/scanline_shard_intercept.rs
+++ render_software/src/scanplan/scanline_shard_intercept.rs
@@ -314,10 +314,8 @@ impl<'a> ScanlineShardInterceptState<'a> {
 
                     if is_inside {
                         existing.opacity += intercept.opacity;
-                    } else {
-                        existing.opacity -= intercept.opacity;
                     }
-
+                    
                     if !was_inside && is_inside {
                         // Need to merge with the existing blend
                         self.active_shapes[existing_idx].blend = match &self.active_shapes[existing_idx].blend {
