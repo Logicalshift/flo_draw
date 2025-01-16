967b2b2027d0a0322132299afcf4f5c45d929c86
Andrew Hunter <andrew@logicalshift.co.uk>
2025-01-16 22:23:18 +0000

    For testing purposes, always enable sub-pixel mode if there's any apexes in the range being rendered
    This isn't rendering properly at all, and is indicating some issues with the 'o's but actually produces very good looking text if you don't count the glitchiness (which seems to be more that the subpixel thing isn't working)
    
    Probably need to get some tests around this to figure out the various glitches

--- render_software/src/edgeplan/edge_plan.rs
+++ render_software/src/edgeplan/edge_plan.rs
@@ -417,7 +417,8 @@ where
                 for ((shards, output_line), y_range) in intercepts.iter().zip(output.iter_mut()).zip(y_ranges) {
                     // Find if any of the apexes lie within this y-range
                     if let Some(apex_pos) = next_apex {
-                        if *apex_pos < y_range.end {
+                        if *apex_pos < y_range.end || true {
+                            /* -- commenting this out means we always operate in supersampling mode (which should work, it's just suboptimal)
                             // Find the apexes that apply to this line
                             let mut line_apexes = vec![y_range.start];
                             let mut last_apex   = y_range.start;
@@ -438,6 +439,7 @@ where
                                     break;
                                 }
                             }
+                            */
 
                             // Instead of using the apexes as the intercept points, use fixed-width subpixels
                             let mut line_apexes = vec![y_range.start];
