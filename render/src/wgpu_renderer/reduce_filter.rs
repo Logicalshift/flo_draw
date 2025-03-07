9703abd9b0654bad0cb8b6d4fe4ec2dd1b521118
Andrew Hunter <andrew@logicalshift.co.uk>
2025-03-07 09:02:32 +0000

    Add the RENDER_ATTACHMENT usage to the textures used in the reduce filter

--- render/src/wgpu_renderer/reduce_filter.rs
+++ render/src/wgpu_renderer/reduce_filter.rs
@@ -50,7 +50,7 @@ pub (crate) fn reduce_filter(device: &wgpu::Device, encoder: &mut wgpu::CommandE
         mip_level_count:    Some(1),
         base_array_layer:   0,
         array_layer_count:  None,
-        usage:              Some(wgpu::TextureUsages::TEXTURE_BINDING),
+        usage:              Some(wgpu::TextureUsages::RENDER_ATTACHMENT | wgpu::TextureUsages::TEXTURE_BINDING),
     };
     let source_view             = source_texture.texture.create_view(&source_view_descriptor);
     let layout                  = &*reduce_pipeline.reduce_layout;
@@ -82,7 +82,7 @@ pub (crate) fn reduce_filter(device: &wgpu::Device, encoder: &mut wgpu::CommandE
             mip_level_count:    Some(1),
             base_array_layer:   0,
             array_layer_count:  None,
-            usage:              Some(wgpu::TextureUsages::TEXTURE_BINDING),
+            usage:              Some(wgpu::TextureUsages::RENDER_ATTACHMENT | wgpu::TextureUsages::TEXTURE_BINDING),
         };
         let target_view         = target_texture.texture.create_view(&view_descriptor);
         let color_attachments   = vec![
