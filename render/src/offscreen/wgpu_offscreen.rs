87fc258e8cd5b851760caa748ab9c20b7d24512e
Andrew Hunter <andrew@logicalshift.co.uk>
2025-03-07 00:12:26 +0000

    Few fields have been renamed here

--- render/src/offscreen/wgpu_offscreen.rs
+++ render/src/offscreen/wgpu_offscreen.rs
@@ -41,7 +41,7 @@ struct WgpuOffscreenRenderTarget {
 ///
 pub async fn wgpu_initialize_offscreen_rendering() -> Result<impl OffscreenRenderContext, RenderInitError> {
     // Create a new WGPU instance and adapter
-    let instance    = wgpu::Instance::new(wgpu::InstanceDescriptor { backends: wgpu::Backends::all(), dx12_shader_compiler: wgpu::Dx12Compiler::default(), ..Default::default() });
+    let instance    = wgpu::Instance::new(&wgpu::InstanceDescriptor { backends: wgpu::Backends::all(), ..Default::default() });
     let adapter     = instance.request_adapter(&wgpu::RequestAdapterOptions {
         power_preference:       wgpu::PowerPreference::default(),
         force_fallback_adapter: false,
@@ -50,9 +50,10 @@ pub async fn wgpu_initialize_offscreen_rendering() -> Result<impl OffscreenRende
 
     // Fetch the device and the queue
     let (device, queue) = adapter.request_device(&wgpu::DeviceDescriptor {
-            label:      None,
-            features:   wgpu::Features::empty(),
-            limits:     wgpu::Limits::downlevel_webgl2_defaults().using_resolution(adapter.limits())
+            label:              None,
+            required_features:  wgpu::Features::empty(),
+            required_limits:    wgpu::Limits::downlevel_webgl2_defaults().using_resolution(adapter.limits()),
+            ..Default::default()
         }, None).await.unwrap();
 
     // Result is a WGPU offscreen render context
