7344da64748675cb37151c8bc35483f12dec7b9e
Andrew Hunter <andrew@logicalshift.co.uk>
2025-03-22 10:44:25 +0000

    Clear finished intercepts before nesting a new intercept

--- render_software/src/scanplan/scanline_shard_intercept.rs
+++ render_software/src/scanplan/scanline_shard_intercept.rs
@@ -307,13 +307,13 @@ impl<'a> ScanlineShardInterceptState<'a> {
                         // Need to merge with the existing blend
                         self.active_shapes[existing_idx].blend = match &self.active_shapes[existing_idx].blend {
                             InterceptBlend::Solid   => InterceptBlend::linear_fade(intercept.lower_x, intercept.upper_x),
-                            other                   => other.nest(InterceptBlend::linear_fade(intercept.lower_x, intercept.upper_x)),
+                            other                   => clear_finished_intercepts(other, intercept.lower_x).nest(InterceptBlend::linear_fade(intercept.lower_x, intercept.upper_x)),
                         };
                     } else if !is_inside {
                         // Change the shape to fade out
                         self.active_shapes[existing_idx].blend = match &self.active_shapes[existing_idx].blend {
                             InterceptBlend::Solid   => InterceptBlend::linear_fade(intercept.upper_x, intercept.lower_x),
-                            other                   => other.nest(InterceptBlend::linear_fade(intercept.upper_x, intercept.lower_x)),
+                            other                   => clear_finished_intercepts(other, intercept.lower_x).nest(InterceptBlend::linear_fade(intercept.upper_x, intercept.lower_x)),
                         };
 
                         // If the shape matches the current z-floor, update it
