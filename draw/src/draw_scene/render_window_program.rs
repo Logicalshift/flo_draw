e4893753578c63505be69a71119c66dd93fff880
Andrew Hunter <andrew@logicalshift.co.uk>
2025-04-22 12:52:25 +0100

    Add an initialisation routine for creating the software drawing window

--- draw/src/draw_scene/render_window_program.rs
+++ draw/src/draw_scene/render_window_program.rs
@@ -4,12 +4,15 @@ use super::glutin_render_window_program::*;
 #[cfg(feature="render-wgpu")]
 use super::wgpu_render_window_program::*;
 
+#[cfg(feature="render-software")]
+use super::software_render_window_program::*;
+
 use flo_scene::*;
 
 use std::sync::*;
 
 ///
-/// Creates a render window in a scene with the specified program ID
+/// Creates a render window in a scene for OpenGL rendering with the specified program ID
 ///
 #[cfg(all(feature="render-opengl", not(feature="render-wgpu")))]
 pub fn create_render_window_sub_program(scene: &Arc<Scene>, program_id: SubProgramId, initial_size: (u64, u64)) -> Result<(), ConnectionError> {
@@ -17,7 +20,7 @@ pub fn create_render_window_sub_program(scene: &Arc<Scene>, program_id: SubProgr
 }
 
 ///
-/// Retrieves or creates a scene context for flo_draw
+/// Creates a render window in a scene for WGPU rendering, with a specified program ID
 ///
 #[cfg(all(feature="render-wgpu"))]
 pub fn create_render_window_sub_program(scene: &Arc<Scene>, program_id: SubProgramId, initial_size: (u64, u64)) -> Result<(), ConnectionError> {
@@ -25,9 +28,19 @@ pub fn create_render_window_sub_program(scene: &Arc<Scene>, program_id: SubProgr
 }
 
 ///
+/// Creates a drawing window in a scene for software rendering, with a specified program ID
+///
+/// (Note that a drawing window differs from a render window in that it takes canvas drawing instructions directly instead of render actions)
+///
+#[cfg(all(feature="render-software", not(any(feature="render-opengl", feature="render-wgpu"))))]
+pub fn create_render_window_sub_program(scene: &Arc<Scene>, program_id: SubProgramId, initial_size: (u64, u64)) -> Result<(), ConnectionError> {
+    create_software_draw_window_program(scene, program_id, initial_size)
+}
+
+///
 /// Retrieves or creates a scene context for flo_draw
 ///
-#[cfg(all(not(feature="render-wgpu"), not(feature="render-opengl")))]
+#[cfg(all(not(feature="render-wgpu"), not(feature="render-opengl"), not(feature="render-software")))]
 pub fn create_render_window_sub_program(context: &Arc<Scene>, program_id: SubProgramId, initial_size: (u64, u64)) -> Result<(), ConnectionError> {
     panic!("No default renderer was specified when flo_draw was compiled (use `render-wgpu` or `render-opengl`)")
 }
