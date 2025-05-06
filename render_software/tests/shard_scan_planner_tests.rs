911459deee10ac7feeed00545f0bee3fc806852c
Andrew Hunter <andrew@logicalshift.co.uk>
2025-05-06 21:28:50 +0100

    Add another variant of the same test that just tests a specific height known to produce issues, to help with debugging

--- render_software/tests/shard_scan_planner_tests.rs
+++ render_software/tests/shard_scan_planner_tests.rs
@@ -1661,3 +1661,71 @@ fn lower_edges_8() {
 
     assert!(failed.is_empty(), "{}/{} failed\n\nFailed={:?}", failed.len(), failed.len() + succeeded.len(), failed);
 }
+
+#[test]
+fn lower_edges_9() {
+    // This letter 'z' has an issue drawing its last line: it shimmers in and out of existence as the pixel height changes
+    use Draw::*;
+    use PathOp::*;
+
+    let letter_z = vec![
+        Path(Move(506.984, 516.866)), 
+        Path(BezierCurve(((506.984, 516.638), (506.945, 516.416)), (506.867, 516.2))), 
+        Path(BezierCurve(((506.789, 515.984), (506.68402, 515.798)), (506.552, 515.642))), 
+        Path(Line(496.67, 502.502)), 
+        Path(Line(506.642, 502.502)), 
+        Path(Line(506.642, 500.0)), 
+        Path(Line(492.90802, 500.0)), 
+        Path(Line(492.90802, 501.332)), 
+        Path(BezierCurve(((492.90802, 501.488), (492.944, 501.671)), (493.01602, 501.881))), 
+        Path(BezierCurve(((493.088, 502.091), (493.196, 502.292)), (493.34003, 502.484))), 
+        Path(Line(503.276, 515.714)), 
+        Path(Line(493.466, 515.714)), 
+        Path(Line(493.466, 518.234)), 
+        Path(Line(506.984, 518.234)), 
+        Path(Line(506.984, 516.866)), 
+        Path(ClosePath)
+    ];
+
+    // Range
+    let z_top       = 500.5;
+    let z_bottom    = 518.234;
+
+    // Renders near the center of a 1000,1000 canvas
+    let mut instructions = vec![];
+
+    instructions.canvas_height(1000.0);
+    instructions.center_region(0.0, 0.0, 1000.0, 1000.0);
+    instructions.extend(letter_z);
+    instructions.fill_color(Color::Rgba(0.0, 0.0, 0.0, 1.0));
+    instructions.fill();
+
+    // Record the locations where we do and do not find a match against the 'z'
+    let mut succeeded   = vec![];
+    let mut failed      = vec![];
+
+    let height      = 1105.0f64;
+    let transform   = ScanlineTransform::for_region(&(-1.0..1.0), height as _);
+
+    println!("\n{}", height);
+
+    // Drawing is in the range 0-1000 but we'll have 1080 pixels
+    let z_top       = (z_top/1000.0)*height;
+    let z_bottom    = (z_bottom/1000.0)*height;
+
+    // Search at the boundary of the lower part of the z, on around an actual pixel boundary
+    let pos1    = -transform.fractional_pixel_x_to_source_x((z_top+0.5).floor());
+    let pos2    = -transform.fractional_pixel_x_to_source_x((z_bottom+0.5).floor());
+
+    // Plan at the positions
+    let plan1 = plan_layer_0_line_on_drawing_with_height(instructions.clone(), pos1, height);
+
+    // Top or bottom of the z should hit both sides
+    if plan1.spans().len() > 0 {
+        succeeded.push(height);
+    } else {
+        failed.push(height);
+    }
+
+    assert!(failed.is_empty(), "{}/{} failed\n\nFailed={:?}", failed.len(), failed.len() + succeeded.len(), failed);
+}
