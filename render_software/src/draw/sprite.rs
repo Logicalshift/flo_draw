91da595878d31d734b42e3291a7ef65fec73daec
Andrew Hunter <andrew@logicalshift.co.uk>
2025-05-16 21:42:01 +0100

    Fix the order of operations when updating sprite translation matrices

--- render_software/src/draw/sprite.rs
+++ render_software/src/draw/sprite.rs
@@ -534,11 +534,11 @@ impl DrawingState {
             (Translate(x, y), SpriteTransform::ScaleTransform { translate, scale }) => { translate.0 += x as f64 * scale.0; translate.1 += y as f64 * scale.0; }
             (Scale(x, y), SpriteTransform::ScaleTransform { scale, .. })            => { scale.0 *= x as f64; scale.1 *= y as f64; }
 
-            (Rotate(theta), sprite_transform)                                       => { *sprite_transform = SpriteTransform::Matrix(sprite_transform.matrix() * canvas::Transform2D::rotate_degrees(theta)); }
-            (Transform2D(matrix), sprite_transform)                                 => { *sprite_transform = SpriteTransform::Matrix(sprite_transform.matrix() * matrix); }
+            (Rotate(theta), sprite_transform)                                       => { *sprite_transform = SpriteTransform::Matrix(canvas::Transform2D::rotate_degrees(theta) * sprite_transform.matrix()); }
+            (Transform2D(matrix), sprite_transform)                                 => { *sprite_transform = SpriteTransform::Matrix(matrix * sprite_transform.matrix()); }
         
-            (Translate(x, y), SpriteTransform::Matrix(t))                           => { *t = *t * canvas::Transform2D::translate(x, y); }
-            (Scale(x, y), SpriteTransform::Matrix(t))                               => { *t = *t * canvas::Transform2D::scale(x, y); }
+            (Translate(x, y), SpriteTransform::Matrix(t))                           => { *t = canvas::Transform2D::translate(x, y) * *t; }
+            (Scale(x, y), SpriteTransform::Matrix(t))                               => { *t = canvas::Transform2D::scale(x, y) * *t; }
         }
     }
 }
