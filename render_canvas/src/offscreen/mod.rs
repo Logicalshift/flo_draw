9c9c255364e57abeb23d9b0650d17256187f8f24
Andrew Hunter <andrew@logicalshift.co.uk>
2025-05-22 21:54:57 +0100

    Implement versions of the offscreen render target, but this time for canvas draw instructions

--- render_canvas/src/offscreen/mod.rs
+++ render_canvas/src/offscreen/mod.rs
@@ -1,3 +1,5 @@
+mod offscreen_trait;
 mod render_offscreen;
 
+pub use offscreen_trait::*;
 pub use render_offscreen::*;
---
+++ render_canvas/src/offscreen/offscreen_trait.rs
@@ -0,0 +1,30 @@
+use flo_canvas::*;
+
+///
+/// Offscreen drawing contexts can be used to create offscreen drawing targets (which can be used to render `Draw` commands and generate a `Vec<u8>` containing the RGBA
+/// data that results from rendering them)
+///
+pub trait OffscreenDrawingContext {
+    /// Drawing targets are where canvas `Draw` instructions can be sent
+    type DrawingTarget: OffscreenDrawingTarget;
+
+    ///
+    /// Creates a new drawing target for this context
+    ///
+    fn create_drawing_target(&mut self, width: usize, height: usize) -> Self::DrawingTarget;
+}
+
+///
+/// An offscreen drawing target is used to send drawing commands and generate a buffer 
+///
+pub trait OffscreenDrawingTarget : GraphicsContext {
+    ///
+    /// Sends render actions to this offscreen render target
+    ///
+    fn draw(&mut self, actions: impl IntoIterator<Item=Draw>);
+
+    ///
+    /// Consumes this render target and returns the realized pixels as a byte array
+    ///
+    fn realize(self) -> Vec<u8>;
+}
