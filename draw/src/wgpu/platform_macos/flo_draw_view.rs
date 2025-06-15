5ff4f3619766a2b2967479ea367b88005e4b54d0
Andrew Hunter <andrew@logicalshift.co.uk>
2025-06-16 00:03:36 +0100

    First pass at setting up a metal layer to render on

--- draw/src/wgpu/platform_macos/flo_draw_view.rs
+++ draw/src/wgpu/platform_macos/flo_draw_view.rs
@@ -1,7 +1,9 @@
 use objc2::*;
+use objc2::rc::*;
 
 use objc2_app_kit::{NSResponder, NSView};
-use objc2_foundation::{NSObject};
+use objc2_foundation::{NSObject, MainThreadMarker};
+use objc2_quartz_core::{CAMetalLayer};
 
 declare_class!(
     ///
@@ -26,3 +28,23 @@ declare_class!(
 
     }
 );
+
+impl FloDrawView {
+    ///
+    /// Creates a new FloDrawView
+    ///
+    pub fn new() -> Retained<Self> {
+        // Allocate the view
+        let main_thread_marker      = MainThreadMarker::new().expect("Must be on main thread");
+        let this                    = main_thread_marker.alloc().set_ivars(());
+        let this: Retained<Self>    = unsafe { msg_send_id![super(this), init] };
+
+        // Set up with a metal layer
+        this.setWantsLayer(true);
+
+        let metal_layer  = unsafe { CAMetalLayer::new() };
+        unsafe { let _: () = msg_send![&this, setLayer: &*metal_layer]; }
+
+        this
+    }
+}
