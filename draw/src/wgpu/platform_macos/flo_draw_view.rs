7d65a960a7bf604a7aa709034caebfa03f3c16dc
Andrew Hunter <andrew@logicalshift.co.uk>
2025-06-16 09:46:49 +0100

    Make the render layer a sublayer, reposition it when the view is resized

--- draw/src/wgpu/platform_macos/flo_draw_view.rs
+++ draw/src/wgpu/platform_macos/flo_draw_view.rs
@@ -2,8 +2,13 @@ use objc2::*;
 use objc2::rc::*;
 
 use objc2_app_kit::{NSResponder, NSView};
-use objc2_foundation::{NSObject, MainThreadMarker};
-use objc2_quartz_core::{CAMetalLayer};
+use objc2_foundation::{NSObject, MainThreadMarker, NSSize};
+use objc2_quartz_core::{CALayer, CAMetalLayer, CATransaction};
+
+pub struct FloDrawViewVars {
+    /// The layer that we should render on
+    metal_layer: Retained<CAMetalLayer>,
+}
 
 declare_class!(
     ///
@@ -21,11 +26,28 @@ declare_class!(
     }
 
     impl DeclaredClass for FloDrawView {
-        type Ivars = ();
+        type Ivars = FloDrawViewVars;
     }
 
     unsafe impl FloDrawView {
+        ///
+        /// Resizes this view
+        ///
+        #[method(setFrameSize:)]
+        fn set_frame_size(&self, newSize: NSSize) {
+            // Perform the normal resizing request
+            unsafe { let _: () = msg_send![super(self), setFrameSize: newSize]; }
 
+            // Resize the rendering layer within this view
+            // Disable the transaction so the resize doesn't do a silly animation
+            CATransaction::begin();
+            CATransaction::setAnimationDuration(0.0);
+            CATransaction::disableActions();
+
+            self.reposition_metal_layer();
+
+            CATransaction::commit();
+        }
     }
 );
 
@@ -34,17 +56,47 @@ impl FloDrawView {
     /// Creates a new FloDrawView
     ///
     pub fn new() -> Retained<Self> {
-        // Allocate the view
         let main_thread_marker      = MainThreadMarker::new().expect("Must be on main thread");
-        let this                    = main_thread_marker.alloc().set_ivars(());
+
+        // Allocate a layer to use as our render target
+        let metal_layer  = unsafe { CAMetalLayer::new() };
+
+        let ivars = FloDrawViewVars {
+            metal_layer: metal_layer,
+        };
+
+        // Allocate the view
+        let this                    = main_thread_marker.alloc().set_ivars(ivars);
         let this: Retained<Self>    = unsafe { msg_send_id![super(this), init] };
 
         // Set up with a metal layer
         this.setWantsLayer(true);
 
-        let metal_layer  = unsafe { CAMetalLayer::new() };
-        unsafe { let _: () = msg_send![&this, setLayer: &*metal_layer]; }
 
         this
+    }
+
+    ///
+    /// Retrieves the metal layer for this FloDrawView
+    ///
+    fn metal_layer(&self) -> Retained<CAMetalLayer> {
+        self.ivars().metal_layer.clone()
+    }
+
+    ///
+    /// Repositions the metal layer within this view
+    ///
+    fn reposition_metal_layer(&self) {
+        // Fetch the layer
+        let layer           = unsafe { self.layer() };
+        let render_layer    = self.metal_layer();
+
+        if let Some(layer) = layer {
+            // Get the parent bounds (TODO: if we want to support scrolling, the visible rect of the parent too)
+            let parent_bounds = layer.bounds();
+
+            // Set the bounds of the render layer
+            render_layer.setBounds(parent_bounds);
+        }
     }
 }
