60892cb4f286ede95669e59dfe55547f2500c15a
Andrew Hunter <andrew@logicalshift.co.uk>
2025-08-31 15:04:39 +0100

    Change the test to use the new 'square' fitting routine
    ... scale is coming out squashed

--- draw/src/draw_scene/drawing_window_program.rs
+++ draw/src/draw_scene/drawing_window_program.rs
@@ -153,7 +153,7 @@ fn handle_window_event<'a>(state: &'a mut RendererState, event: DrawEvent, send_
                 let scale           = state.scale as f32;
 
                 state.renderer.set_window_viewport(0.0..width, 0.0..height, width, height, scale);
-                state.renderer.set_canvas_viewport_exact(300.0..500.0, 300.0..500.0);
+                state.renderer.set_canvas_viewport_center(300.0..500.0, 300.0..500.0);
                 if let Some(send_drawing_actions) = send_drawing_actions { send_drawing_actions.send(DrawingWindowRequest::Redraw).await.ok(); }
 
                 vec![]
