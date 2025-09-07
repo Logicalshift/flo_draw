2b68d125d58b3fa2de13df97269badaaab9119ea
Andrew Hunter <andrew@logicalshift.co.uk>
2025-09-07 12:11:50 +0100

    Create an initial subscriber to capture any events that occur before the first 'SendEvents' request

--- draw/src/draw_scene/wgpu_render_window_program.rs
+++ draw/src/draw_scene/wgpu_render_window_program.rs
@@ -40,7 +40,13 @@ pub fn create_wgpu_render_window_program(scene: &Arc<Scene>, program_id: SubProg
             .with_requested_size(requested_size.clone())
             .with_viewport_bounds(viewport_bounds.clone());
 
-        let mut event_publisher = Publisher::new(1000);
+        let mut event_publisher     = Publisher::new(1000);
+
+        // We create an initial subscriber so that the first thing to request events gets 
+        // all of the events generated from the creation of the window. Without this, it's
+        // possible the initial 'scale' and 'size' events might not be sent to the first
+        // 'SendEvents' requestor
+        let mut initial_subscriber  = Some(event_publisher.subscribe());
 
         // Create a stream for publishing render requests
         let (render_sender, render_receiver) = mpsc::channel(5);
@@ -68,7 +74,11 @@ pub fn create_wgpu_render_window_program(scene: &Arc<Scene>, program_id: SubProg
                     }
 
                     RenderWindowRequest::SendEvents(channel_target) => {
-                        let mut subscriber = event_publisher.subscribe();
+                        let mut subscriber = if let Some(subscriber) = initial_subscriber.take() {
+                            subscriber
+                        } else {
+                            event_publisher.subscribe()
+                        };
 
                         context.send_message(SceneControl::start_program(SubProgramId::new(), move |_: InputStream<()>, context| {
                             async move {
