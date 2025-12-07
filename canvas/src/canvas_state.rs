be3d100675ab0aa4ad227e0bbec8dc93a8b67f42
Andrew Hunter <andrew@logicalshift.co.uk>
2025-12-07 17:32:56 +0000

    Add textures, gradients and fonts/text to the state

--- canvas/src/canvas_state.rs
+++ canvas/src/canvas_state.rs
@@ -1,7 +1,10 @@
 use crate::draw::*;
+use crate::font::*;
 use crate::path::*;
 use crate::color::*;
+use crate::texture::*;
 use crate::gradient::*;
+use crate::font_face::*;
 use crate::transform2d::*;
 
 use std::collections::*;
@@ -13,16 +16,16 @@ use std::sync::*;
 #[derive(Clone)]
 pub struct CanvasState {
     drawing_target:     DrawingTarget,
-    current_brush:      SharedCanvasBrush,
+    current_brush:      CanvasShared<CanvasBrush>,
     state_stack:        Vec<Arc<CanvasBrush>>,
 
     layers_and_sprites: HashMap<DrawingTarget, Vec<CanvasEntity>>,
+    textures:           HashMap<TextureId, CanvasShared<CanvasTexture>>,
+    gradients:          HashMap<GradientId, CanvasShared<CanvasGradient>>,
+    fonts:              HashMap<FontId, Arc<CanvasFontFace>>,
 
-    /*
-    textures:
-    gradients:
-    fonts:
-     */
+    current_path:       CanvasPath,
+    current_text:       Vec<CanvasTextLayout>,
 }
 
 ///
@@ -80,6 +83,8 @@ pub struct CanvasBrush {
     clip_path:          Option<CanvasPath>,
 
     sprite_transform:   SpriteTransform,
+
+    font_size:          f32,
 }
 
 ///
@@ -94,16 +99,87 @@ pub struct CanvasPath {
 /// An entity rendered on a canvas
 ///
 #[derive(Clone, Debug)]
-pub struct CanvasEntity {
-    brush:  Arc<CanvasBrush>,
-    path:   CanvasPath
+pub enum CanvasEntity {
+    FillPath {
+        brush:      Arc<CanvasBrush>,
+        texture:    Option<Arc<CanvasTexture>>,
+        gradient:   Option<Arc<CanvasGradient>>,
+        path:       CanvasPath,
+    },
+
+    StrokePath {
+        brush:      Arc<CanvasBrush>,
+        texture:    Option<Arc<CanvasTexture>>,
+        gradient:   Option<Arc<CanvasGradient>>,
+        path:       CanvasPath,
+    },
+
+    TextLayout {
+        text:       Vec<CanvasTextLayout>,
+    }
 }
 
 ///
 /// Canvas brush that can be modified or shared
 ///
 #[derive(Clone, Debug)]
-pub enum SharedCanvasBrush {
-    Modified(CanvasBrush),
-    Shared(Arc<CanvasBrush>)
+pub enum CanvasShared<T> {
+    Modified(T),
+    Shared(Arc<T>)
+}
+
+///
+/// Canvas texture definition
+///
+/// Texture data is optional here: we won't be able to reconstruct textures if this is not supplied but
+/// also won't duplicate them in the renderer (or spend time copying the bytes into multiple buffers)
+///
+#[derive(Clone, Debug)]
+pub struct CanvasTexture {
+    format:             TextureFormat,
+    size:               (f32, f32),
+    bytes:              CanvasTextureData,
+    fill_transparency:  f32,
+}
+
+///
+/// Data storage for a texture
+///
+#[derive(Clone, Debug)]
+pub enum CanvasTextureData {
+    None,
+    Modified(Vec<u8>),
+    Shared(Arc<Vec<u8>>),
+}
+
+///
+/// Canvas gradient definition
+///
+#[derive(Clone, Debug)]
+pub struct CanvasGradient {
+    stops: Vec<(f32, Color)>,
+}
+
+///
+/// Laid out text
+///
+#[derive(Clone, Debug)]
+pub struct CanvasTextLayout {
+    pos:    (f32, f32),
+    text:   Vec<CanvasTextChunk>,
+}
+
+///
+/// Block of text that is laid out
+///
+#[derive(Clone, Debug)]
+pub struct CanvasTextChunk {
+    text:       Vec<String>,
+
+    font_defn:  Arc<CanvasFontFace>,
+    font_id:    FontId,
+    brush:      Arc<CanvasBrush>,
+    texture:    Option<Arc<CanvasTexture>>,
+    gradient:   Option<Arc<CanvasGradient>>,
+    alignment:  TextAlignment,
 }
