2772849df6c0b0784f87ef10477906002e3d07c2
Andrew Hunter <andrew@logicalshift.co.uk>
2025-12-13 20:56:19 +0000

    Add 'frame' counting

--- canvas/src/canvas_state/state.rs
+++ canvas/src/canvas_state/state.rs
@@ -21,6 +21,7 @@ use std::sync::*;
 #[derive(Clone)]
 pub struct CanvasState {
     keep_textures:      bool,
+    frame_count:        usize,
     background:         Color,
 
     current_brush:      CanvasShared<CanvasBrush>,
@@ -43,6 +44,7 @@ impl Default for CanvasState {
     fn default() -> Self {
         CanvasState { 
             keep_textures:      true,
+            frame_count:        0,
             background:         Color::Rgba(1.0, 1.0, 1.0, 1.0), 
             current_brush:      CanvasShared::new(CanvasBrush::default()), 
             state_stack:        vec![], 
@@ -55,4 +57,65 @@ impl Default for CanvasState {
             current_text:       vec![],
         }
     }
+}
+
+impl CanvasState {
+    ///
+    /// Updates this state with a drawing action
+    ///
+    pub fn draw(&mut self, action: Draw) {
+        match action {
+            Draw::StartFrame                            => self.start_frame(),
+            Draw::ShowFrame                             => self.show_frame(),
+            Draw::ResetFrame                            => self.reset_frame(),
+            Draw::Path(path_op)                         => todo!(),
+            Draw::Fill                                  => todo!(),
+            Draw::Stroke                                => todo!(),
+            Draw::LineWidth(_)                          => todo!(),
+            Draw::LineWidthPixels(_)                    => todo!(),
+            Draw::LineJoin(line_join)                   => todo!(),
+            Draw::LineCap(line_cap)                     => todo!(),
+            Draw::NewDashPattern                        => todo!(),
+            Draw::DashLength(_)                         => todo!(),
+            Draw::DashOffset(_)                         => todo!(),
+            Draw::FillColor(color)                      => todo!(),
+            Draw::FillTexture(texture_id, _, _)         => todo!(),
+            Draw::FillGradient(gradient_id, _, _)       => todo!(),
+            Draw::FillTransform(transform2_d)           => todo!(),
+            Draw::StrokeColor(color)                    => todo!(),
+            Draw::WindingRule(winding_rule)             => todo!(),
+            Draw::BlendMode(blend_mode)                 => todo!(),
+            Draw::IdentityTransform                     => todo!(),
+            Draw::CanvasHeight(_)                       => todo!(),
+            Draw::CenterRegion(_, _)                    => todo!(),
+            Draw::MultiplyTransform(transform2_d)       => todo!(),
+            Draw::Unclip                                => todo!(),
+            Draw::Clip                                  => todo!(),
+            Draw::Store                                 => todo!(),
+            Draw::Restore                               => todo!(),
+            Draw::FreeStoredBuffer                      => todo!(),
+            Draw::PushState                             => todo!(),
+            Draw::PopState                              => todo!(),
+            Draw::ClearCanvas(color)                    => todo!(),
+            Draw::Layer(layer_id)                       => todo!(),
+            Draw::LayerBlend(layer_id, blend_mode)      => todo!(),
+            Draw::ClearLayer                            => todo!(),
+            Draw::ClearAllLayers                        => todo!(),
+            Draw::SwapLayers(layer_id, layer_id1)       => todo!(),
+            Draw::Sprite(sprite_id)                     => todo!(),
+            Draw::ClearSprite                           => todo!(),
+            Draw::SpriteTransform(sprite_transform)     => todo!(),
+            Draw::DrawSprite(sprite_id)                 => todo!(),
+            Draw::Texture(texture_id, texture_op)       => todo!(),
+            Draw::Font(font_id, font_op)                => todo!(),
+            Draw::BeginLineLayout(_, _, text_alignment) => todo!(),
+            Draw::DrawLaidOutText                       => todo!(),
+            Draw::DrawText(font_id, _, _, _)            => todo!(),
+            Draw::Gradient(gradient_id, gradient_op)    => todo!(),
+        }
+    }
+
+    #[inline] fn start_frame(&mut self) { self.frame_count += 1; }
+    #[inline] fn show_frame(&mut self) { self.frame_count -= 1; }
+    #[inline] fn reset_frame(&mut self) { self.frame_count = 0; }
 }
