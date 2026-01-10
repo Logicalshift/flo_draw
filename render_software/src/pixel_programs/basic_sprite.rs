5dba1b40a298e17f6b37786e9cdc409772721c06
Andrew Hunter <andrew@logicalshift.co.uk>
2026-01-10 21:15:12 +0000

    Extend the rendering range so that we get the whole sprite
    (I think this produces something slightly broken, but also the 'render' request shouldn't really be working this way)

--- render_software/src/pixel_programs/basic_sprite.rs
+++ render_software/src/pixel_programs/basic_sprite.rs
@@ -68,7 +68,7 @@ where
         // Calculate the transform for the sprite region
         let sprite_ypos         = y_pos * data.scale.1 + data.translate.1;
         let sprite_transform    = x_transform.transform(data.scale.0, data.translate.0);
-        let sprite_xrange       = sprite_transform.pixel_x_to_source_x(x_range.start)..sprite_transform.pixel_x_to_source_x(x_range.end);
+        let sprite_xrange       = (sprite_transform.pixel_x_to_source_x(x_range.start).floor())..(sprite_transform.pixel_x_to_source_x(x_range.end).ceil());
 
         // Plan the rendering for the sprite
         // TODO: we might render the same sprite multiple times on a line, in which case it would be faster to do this once and re-use it later on, maybe can exploit that 
