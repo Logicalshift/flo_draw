e5b3da34dc67b5c0b0f5eee3abe46fb87d651095
Andrew Hunter <andrew@logicalshift.co.uk>
2026-02-25 20:34:35 +0000

    Add to canvas_renderer and canvas_drawing

--- render_software/src/draw/canvas_drawing.rs
+++ render_software/src/draw/canvas_drawing.rs
@@ -169,6 +169,7 @@ where
                 ClearAllLayers                                      => { self.clear_all_layers(); },
                 SwapLayers(layer_1, layer_2)                        => { self.swap_layers(layer_1, layer_2); },
                 PlaceLayerBefore(namespace_id, layer_id)            => { self.place_layer_before(namespace_id, layer_id); }
+                SetLayerTransform(layer_transform)                  => { todo!() }
 
                 Path(path_op)                                       => { self.current_state.path_op(path_op); },
                 Fill                                                => { self.fill(); },
