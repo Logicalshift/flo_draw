20bd2445d415a646af94206f5a37936dc5637049
Andrew Hunter <andrew@logicalshift.co.uk>
2026-03-01 16:21:14 +0000

    Fix name clash

--- draw/src/platform/mod.rs
+++ draw/src/platform/mod.rs
@@ -1,7 +1,7 @@
 #[cfg(target_os="macos")] pub (crate) mod macos;
 #[cfg(target_os="macos")] pub (crate) use macos::*;
 
-#[cfg(feature="winit")] pub (crate) mod winit;
+#[cfg(feature="winit")] mod winit;
 #[cfg(feature="winit")] pub (crate) use winit::*;
 
 #[cfg(target_os="linux")] pub mod wayland;
