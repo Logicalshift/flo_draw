3ced0ab1a5a786d9ac04412c82cf22a91f790ffc
Andrew Hunter <andrew@logicalshift.co.uk>
2026-03-13 23:11:48 +0000

    Add a routine that is intended to load a system font

---
+++ canvas/src/system_fonts.rs
@@ -0,0 +1,9 @@
+use super::font_spec::*;
+use super::font_face::*;
+
+///
+/// Loads a system font (with no caching)
+///
+pub fn load_system_font(spec: &FontSpec) -> Option<CanvasFontFace> {
+    todo!()
+}
