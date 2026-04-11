462c14a04aed6af1fcdba2ba9a4c1d599dbb680a
Andrew Hunter <andrew@logicalshift.co.uk>
2026-04-11 18:39:10 +0100

    The 'shouldn't happen' case can in fact happen
    This was hiding the bug before by eliminating a transition for non-zero shapes - provided there was only one discontinuity. It'd fail for cases where there were even numbers of discontinuities though (and for cases where we're using Toggle, which is how we found the bug in the first place)

--- render_software/src/scanplan/shard.rs
+++ render_software/src/scanplan/shard.rs
@@ -172,11 +172,6 @@ fn resolve_shards(previous_line: &Vec<EdgeDescriptorIntercept>, next_line: &Vec<
             continue;
         }
 
-        if first_intercept.direction != second_intercept.direction {
-            // Shouldn't happen?
-            continue;
-        }
-
         // The first intercept is on opposite line to the second intercept, indicating that the shape crossed inbetween the two lines
         let shard = ShardIntercept {
             direction:  first_intercept.direction,
